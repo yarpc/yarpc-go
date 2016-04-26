eaa5e3799532a25372b73186f5f8500616259d42
Abhinav Gupta <abg@uber.com>
2016-04-25 18:17:41 -0700

    tchannel/outbound: Use Remote{BadRequest,Unexpected}Error

--- transport/tchannel/outbound.go
+++ transport/tchannel/outbound.go
@@ -99,6 +99,8 @@ func (o outbound) Call(ctx context.Context, req *transport.Request) (*transport.
 	}
 
 	if err := writeHeaders(format, req.Headers, call.Arg2Writer); err != nil {
+		// TODO(abg): This will wrap IO errors while writing headers as encode
+		// errors. We should fix that.
 		return nil, encoding.RequestHeadersEncodeError(req, err)
 	}
 
@@ -107,16 +109,21 @@ func (o outbound) Call(ctx context.Context, req *transport.Request) (*transport.
 	}
 
 	res := call.Response()
-
-	// TODO(abg): handle errors
-
 	headers, err := readHeaders(format, res.Arg2Reader)
 	if err != nil {
-		return nil, encoding.RequestHeadersDecodeError(req, err)
+		if err, ok := err.(tchannel.SystemError); ok {
+			return nil, fromSystemError(err)
+		}
+		// TODO(abg): This will wrap IO errors while reading headers as decode
+		// errors. We should fix that.
+		return nil, encoding.ResponseHeadersDecodeError(req, err)
 	}
 
 	resBody, err := res.Arg3Reader()
 	if err != nil {
+		if err, ok := err.(tchannel.SystemError); ok {
+			return nil, fromSystemError(err)
+		}
 		return nil, err
 	}
 
@@ -134,4 +141,15 @@ func writeBody(body io.Reader, call *tchannel.OutboundCall) error {
 	}
 
 	return w.Close()
+}
+
+func fromSystemError(err tchannel.SystemError) error {
+	switch err.Code() {
+	case tchannel.ErrCodeBadRequest:
+		return transport.RemoteBadRequestError(err.Message())
+	case tchannel.ErrCodeUnexpected:
+		return transport.RemoteUnexpectedError(err.Message())
+	default:
+		return err // TODO(abg): How to handle other error codes?
+	}
 }
