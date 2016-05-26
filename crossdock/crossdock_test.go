82ad4d197e75be8f87f615652ed4374c8e3786e7
Abhinav Gupta <abg@uber.com>
2016-05-25 17:23:46 -0700

    crossdock: Implement thriftgauntlet behavior

--- crossdock/crossdock_test.go
+++ crossdock/crossdock_test.go
@@ -94,6 +94,12 @@ func TestCrossdock(t *testing.T) {
 				"encoding": []string{"raw", "json", "thrift"},
 			},
 		},
+		{
+			name: "thriftgauntlet",
+			axes: axes{
+				"transport": []string{"http", "tchannel"},
+			},
+		},
 	}
 
 	for _, bb := range behaviors {
