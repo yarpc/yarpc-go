e9f0765adefa509d9218fbd8ef642f57483cdf79
Grayson Koonce <grayson@uber.com>
2016-06-01 17:27:17 -0700

    reuse ThriftServer when registering handlers

--- crossdock/server/tch/server.go
+++ crossdock/server/tch/server.go
@@ -64,10 +64,13 @@ func Stop() {
 // Register the different endpoints of the test subject
 func register(ch *tchannel.Channel) {
 	ch.Register(raw.Wrap(echoRawHandler{}), "echo/raw")
+
 	json.Register(ch, json.Handlers{"echo": echoJSONHandler}, onError)
-	thrift.NewServer(ch).Register(echo.NewTChanEchoServer(&echoThriftHandler{}))
-	thrift.NewServer(ch).Register(gauntlet_apache.NewTChanThriftTestServer(&thriftTestHandler{}))
-	thrift.NewServer(ch).Register(gauntlet_apache.NewTChanSecondServiceServer(&secondServiceHandler{}))
+
+	tserver := thrift.NewServer(ch)
+	tserver.Register(echo.NewTChanEchoServer(&echoThriftHandler{}))
+	tserver.Register(gauntlet_apache.NewTChanThriftTestServer(&thriftTestHandler{}))
+	tserver.Register(gauntlet_apache.NewTChanSecondServiceServer(&secondServiceHandler{}))
 }
 
 func onError(ctx context.Context, err error) {
