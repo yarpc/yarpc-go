bb10920acd71851a7b6aabcb90ad5c7a10746744
Abhinav Gupta <mail@abhinavg.net>
2016-07-20 17:45:27 -0700

    yarpctest: Support Fake ReqMeta and CallResMeta (#278)
    This adds the yarpctest package and support for building fake `ReqMeta` and
    `CallResMeta` objects for testing.
    
    Resloves #274

---
+++ yarpctest/fake_call_res_meta.go
@@ -0,0 +1,71 @@
+// Copyright (c) 2016 Uber Technologies, Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+package yarpctest
+
+import (
+	"github.com/yarpc/yarpc-go"
+
+	"golang.org/x/net/context"
+)
+
+// CallResMetaBuilder helps build fake yarpc.CallResMeta objects for testing.
+//
+// 	resMeta := NewCallResMetaBuilder(ctx).
+// 		Headers(headers).
+// 		Build()
+type CallResMetaBuilder struct {
+	singleUse
+
+	ctx     context.Context
+	headers yarpc.Headers
+}
+
+// NewCallResMetaBuilder builds a new fake ReqMeta for unit tests
+func NewCallResMetaBuilder(ctx context.Context) *CallResMetaBuilder {
+	return &CallResMetaBuilder{ctx: ctx}
+}
+
+// Headers specifies the response headers for this CallResMeta.
+func (f *CallResMetaBuilder) Headers(h yarpc.Headers) *CallResMetaBuilder {
+	f.ensureUnused(f)
+	f.headers = h
+	return f
+}
+
+// Build builds a yarpc.ReqMeta from this CallResMetaBuilder.
+//
+// The CallResMetaBuilder is not re-usable and becomes invalid after this
+// call.
+func (f *CallResMetaBuilder) Build() yarpc.CallResMeta {
+	resMeta := fakeResMeta(*f)
+	f.use(f)
+	return resMeta
+}
+
+type fakeResMeta CallResMetaBuilder
+
+func (r fakeResMeta) Context() context.Context {
+	return CallResMetaBuilder(r).ctx
+}
+
+func (r fakeResMeta) Headers() yarpc.Headers {
+	return CallResMetaBuilder(r).headers
+}
---
+++ yarpctest/fake_call_res_meta_test.go
@@ -0,0 +1,60 @@
+// Copyright (c) 2016 Uber Technologies, Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+package yarpctest
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	yarpc "github.com/yarpc/yarpc-go"
+	"golang.org/x/net/context"
+)
+
+func TestResMeta(t *testing.T) {
+	tests := []struct {
+		build func(*CallResMetaBuilder) *CallResMetaBuilder
+
+		wantHeaders yarpc.Headers
+	}{
+		{
+			build: func(r *CallResMetaBuilder) *CallResMetaBuilder {
+				return r
+			},
+			wantHeaders: yarpc.NewHeaders(),
+		},
+		{
+			build: func(r *CallResMetaBuilder) *CallResMetaBuilder {
+				return r.Headers(yarpc.NewHeaders().With("foo", "bar"))
+			},
+			wantHeaders: yarpc.NewHeaders().With("foo", "bar"),
+		},
+	}
+
+	for _, tt := range tests {
+		ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+		defer cancel()
+
+		reqMeta := tt.build(NewCallResMetaBuilder(ctx)).Build()
+		assert.Equal(t, ctx, reqMeta.Context())
+		assert.Equal(t, tt.wantHeaders, reqMeta.Headers())
+	}
+}
