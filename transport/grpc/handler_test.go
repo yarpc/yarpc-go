5f0f4c63de183cceb4cb654d0b671600f2278205
Will Hughes <wh@uber.com>
2016-09-21 13:59:01 -0700

    Add tests for the ResponseWriter and Reponse in grpc/handler.go

---
+++ transport/grpc/handler_test.go
@@ -0,0 +1,40 @@
+package grpc
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/yarpc/yarpc-go/transport"
+)
+
+func TestResponseWriter_Write(t *testing.T) {
+	strMsg := "this is a test"
+	byteMsg := []byte(strMsg)
+	var r response
+	rw := newResponseWriter(&r)
+
+	changed, err := rw.Write(byteMsg)
+
+	assert.Equal(t, len(byteMsg), changed)
+	assert.Equal(t, error(nil), err)
+	assert.Equal(t, strMsg, string(r.body))
+}
+
+func TestResponseWriter_AddHeaders(t *testing.T) {
+	headers := transport.NewHeadersWithCapacity(10)
+	var r response
+	rw := newResponseWriter(&r)
+
+	rw.AddHeaders(headers)
+
+	assert.Equal(t, headers, r.headers)
+}
+
+func TestResponseWriter_SetApplicationError(t *testing.T) {
+	var r response
+	rw := newResponseWriter(&r)
+
+	rw.SetApplicationError()
+
+	// No action on Application Error
+}
