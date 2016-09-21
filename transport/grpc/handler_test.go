25ce4a6902912717be007b81a984f80704a6a002
Will Hughes <wh@uber.com>
2016-09-21 16:00:25 -0700

    Add tests for gRPC handler "Handle" function

--- transport/grpc/handler_test.go
+++ transport/grpc/handler_test.go
@@ -3,9 +3,112 @@ package grpc
 import (
 	"testing"
 
+	"bytes"
+
+	"errors"
+
+	"github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/assert"
 	"github.com/yarpc/yarpc-go/transport"
+	"github.com/yarpc/yarpc-go/transport/transporttest"
+	"golang.org/x/net/context"
 )
+
+func TestHandler_Handle(t *testing.T) {
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
+
+	ctx := context.Background()
+	decodedMsg := []byte("test string!")
+	decoderFunc := func(v interface{}) error {
+		bs, _ := v.(*[]byte)
+		*bs = decodedMsg
+		return nil
+	}
+	expectedBody := []byte("teeeeeest response")
+
+	rpcHandler := transporttest.NewMockHandler(mockCtrl)
+	grpcHandler := handler{Handler: rpcHandler}
+
+	rpcHandler.EXPECT().Handle(
+		ctx,
+		grpcOptions,
+		transporttest.NewRequestMatcher(
+			t, &transport.Request{
+				Caller:    "hello",
+				Service:   "foo",
+				Procedure: "bar",
+				Encoding:  "raw",
+				Body:      bytes.NewReader(decodedMsg),
+			},
+		),
+		gomock.Any(),
+	).Return(nil).Do(
+		func(ctx context.Context, opts transport.Options, req *transport.Request, resw transport.ResponseWriter) {
+			resw.Write(expectedBody)
+		},
+	)
+
+	body, err := grpcHandler.Handle(nil, ctx, decoderFunc, nil)
+
+	assert.Equal(t, expectedBody, *(body.(*[]byte)))
+	assert.Equal(t, error(nil), err)
+}
+
+func TestHandler_Handle_HandlerError(t *testing.T) {
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
+
+	ctx := context.Background()
+	decodedMsg := []byte("test string!")
+	decoderFunc := func(v interface{}) error {
+		bs, _ := v.(*[]byte)
+		*bs = decodedMsg
+		return nil
+	}
+	expectedErr := errors.New("teeeest error")
+
+	rpcHandler := transporttest.NewMockHandler(mockCtrl)
+	grpcHandler := handler{Handler: rpcHandler}
+
+	rpcHandler.EXPECT().Handle(
+		ctx,
+		grpcOptions,
+		transporttest.NewRequestMatcher(
+			t, &transport.Request{
+				Caller:    "hello",
+				Service:   "foo",
+				Procedure: "bar",
+				Encoding:  "raw",
+				Body:      bytes.NewReader(decodedMsg),
+			},
+		),
+		gomock.Any(),
+	).Return(expectedErr)
+
+	_, err := grpcHandler.Handle(nil, ctx, decoderFunc, nil)
+
+	assert.Equal(t, expectedErr, err)
+}
+
+func TestHandler_Handle_DecoderError(t *testing.T) {
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
+
+	ctx := context.Background()
+	decodingError := errors.New("DecodingError")
+	decoderFunc := func(v interface{}) error {
+		return decodingError
+	}
+
+	rpcHandler := transporttest.NewMockHandler(mockCtrl)
+	grpcHandler := handler{Handler: rpcHandler}
+
+	res, err := grpcHandler.Handle(nil, ctx, decoderFunc, nil)
+
+	assert.Equal(t, nil, res)
+	assert.Equal(t, decodingError, err)
+}
 
 func TestResponseWriter_Write(t *testing.T) {
 	strMsg := "this is a test"
