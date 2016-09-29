fe7f1d7b3121c4395c803d6a8ea067df845e3e9e
Will Hughes <willhughes01@gmail.com>
2016-09-28 17:17:19 -0700

    [GRPC][Part 4] Adding a barebones GRPC inbound and outbound
    Summary: In order to get a working first version of the GRPC
    inbound/outbound I've created a barebones gRPC server "yarpc" which is
    hardcoded to support raw messages.  There is no support for encoding,
    headers, or other top-level features.  The yclient and yserver use the
    default inbound and outbounds to converse.
    
    Test Plan: Wrote tests for specific parts of the inbound and handler.
    Running the yserver and yclient shows they can talk to each other (I'll
    be adding crossdock tests once we can perform dynamic service/procedure
    routing)

---
+++ examples/grpc/yclient/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"golang.org/x/net/context"
+
+	yarpc "github.com/yarpc/yarpc-go"
+	"github.com/yarpc/yarpc-go/encoding/raw"
+	"github.com/yarpc/yarpc-go/transport"
+	"github.com/yarpc/yarpc-go/transport/grpc"
+)
+
+func main() {
+	dispatcher := yarpc.NewDispatcher(yarpc.Config{
+		Name: "hello",
+		Outbounds: transport.Outbounds{
+			"yarpc": grpc.NewOutbound("localhost:50054"),
+		},
+	})
+
+	if err := dispatcher.Start(); err != nil {
+		log.Fatal(err)
+	}
+	defer dispatcher.Stop()
+
+	client := raw.New(dispatcher.Channel("yarpc"))
+
+	yarpcReqBody := fmt.Sprintf("Request to yarpc with value %d", time.Now().Unix())
+	sendRequest(client, "yarpc", yarpcReqBody)
+}
+
+func sendRequest(client raw.Client, procedure, msgBody string) {
+	randDuration := time.Now().Unix()%100 + 1
+	randTimeout := time.Duration(randDuration) * time.Second
+	headers := yarpc.NewHeaders().With("from", "self")
+
+	fmt.Println("---Sending a request---")
+	fmt.Println("Timeout: ", randTimeout)
+	fmt.Println("Caller: hello")
+	fmt.Println("Encoding: raw")
+	fmt.Println("Service: yarpc")
+	fmt.Println("Procedure: ", procedure)
+	fmt.Println("headers: ", headers)
+	fmt.Println("Body: ", msgBody)
+
+	ctx, _ := context.WithTimeout(context.Background(), randTimeout)
+	resBody, resMeta, err := client.Call(
+		ctx,
+		yarpc.NewReqMeta().Procedure(procedure).Headers(headers),
+		[]byte(msgBody),
+	)
+	if err != nil {
+		log.Fatalf("call failed: %v", err)
+	}
+
+	fmt.Println("SUCCESS! Got response: ", string(resBody))
+	fmt.Println("With Headers: ", resMeta.Headers())
+	fmt.Println("---Finished request---")
+}
---
+++ transport/grpc/handler.go
@@ -0,0 +1,154 @@
+package grpc
+
+import (
+	"bytes"
+	"time"
+
+	"github.com/yarpc/yarpc-go/transport"
+	"github.com/yarpc/yarpc-go/transport/internal"
+
+	"strings"
+
+	"fmt"
+
+	"io"
+
+	"errors"
+
+	"net/url"
+
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+	gtransport "google.golang.org/grpc/transport"
+)
+
+var grpcOptions transport.Options
+
+type handler struct {
+	Handler transport.Handler
+	Deps    transport.Deps
+}
+
+// Handle the grpc request and convert it into a YARPC request
+// dec ('decode') will pass through the request body in raw bytes using the PassThroughCodec
+func (h handler) Handle(
+	srv interface{},
+	ctx context.Context,
+	dec func(interface{}) error,
+	interceptor grpc.UnaryServerInterceptor,
+) (interface{}, error) {
+	treq, extractErr := getTRequest(ctx, dec)
+	if extractErr != nil {
+		return nil, extractErr
+	}
+
+	// TODO handle deadlines
+	// TODO handle validation
+
+	start := time.Now()
+
+	return callHandler(h, start, ctx, treq)
+}
+
+func getTRequest(ctx context.Context, msgBodyDecoder func(interface{}) error) (*transport.Request, error) {
+	service, procedure, err := getServiceAndProcedure(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	requestBody, err := getMsgBody(msgBodyDecoder)
+	if err != nil {
+		return nil, err
+	}
+
+	treq := &transport.Request{
+		Service:   service,
+		Procedure: procedure,
+		Caller:    "yarpc",
+		Encoding:  transport.Encoding("raw"),
+		Body:      requestBody,
+	}
+
+	return treq, nil
+}
+
+func getServiceAndProcedure(ctx context.Context) (service, procedure string, err error) {
+	stream, ok := gtransport.StreamFromContext(ctx)
+	if !ok {
+		return "", "", errors.New("Could not extract stream information from context")
+	}
+
+	streamMethod := stream.Method()
+
+	return getServiceAndProcedureFromMethod(streamMethod)
+}
+
+func getServiceAndProcedureFromMethod(streamMethod string) (service, procedure string, err error) {
+	if streamMethod != "" && streamMethod[0] == '/' {
+		streamMethod = streamMethod[1:]
+	}
+	splitPos := strings.LastIndex(streamMethod, "/")
+
+	service, err = url.QueryUnescape(streamMethod[:splitPos])
+	if err != nil {
+		return "", "", fmt.Errorf("Could not parse service for request: %s, error: %v", streamMethod[:splitPos], err)
+	}
+
+	procedure, err = url.QueryUnescape(streamMethod[splitPos+1:])
+	if err != nil {
+		return "", "", fmt.Errorf("Could not parse procedure for request: %s, error: %v", streamMethod[splitPos+1:], err)
+	}
+	return
+}
+
+func getMsgBody(msgBodyDecoder func(interface{}) error) (io.Reader, error) {
+	var requestBody []byte
+	if err := msgBodyDecoder(&requestBody); err != nil {
+		return nil, err
+	}
+
+	requestBodyBuffer := bytes.NewBuffer(requestBody)
+
+	return requestBodyBuffer, nil
+}
+
+func callHandler(
+	h handler,
+	start time.Time,
+	ctx context.Context,
+	treq *transport.Request,
+) (interface{}, error) {
+	var r response
+	rw := newResponseWriter(&r)
+
+	err := internal.SafelyCallHandler(h.Handler, start, ctx, grpcOptions, treq, rw)
+
+	responseBody := r.body.Bytes()
+	return &responseBody, err
+}
+
+// The response object contains response information from the YARPC handler
+type response struct {
+	body bytes.Buffer
+}
+
+// Wrapper to control writes to the response object
+type responseWriter struct {
+	r *response
+}
+
+func newResponseWriter(r *response) responseWriter {
+	return responseWriter{r: r}
+}
+
+func (rw responseWriter) Write(s []byte) (int, error) {
+	return rw.r.body.Write(s)
+}
+
+func (rw responseWriter) AddHeaders(h transport.Headers) {
+	// TODO support Headers
+}
+
+func (responseWriter) SetApplicationError() {
+	// Nothing to do.
+}
