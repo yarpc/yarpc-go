37da496dbc1a70bd0e2638c42883a4f7629ef21d
Will Hughes <willhughes01@gmail.com>
2016-11-01 15:25:51 -0700

    Fix lint by adding comments

--- crossdock/internal/header.go
+++ crossdock/internal/header.go
@@ -2,6 +2,7 @@ package internal
 
 import "go.uber.org/yarpc"
 
+// RemoveVariableHeaderKeys removes any headers that might have been added by tracing
 func RemoveVariableHeaderKeys(headers yarpc.Headers) yarpc.Headers {
 	headers.Del("$tracing$uber-trace-id")
 	if headers.Len() == 0 {
@@ -10,6 +11,7 @@ func RemoveVariableHeaderKeys(headers yarpc.Headers) yarpc.Headers {
 	return headers
 }
 
+// RemoveVariableMapKeys removes any headers that might have been added by tracing
 func RemoveVariableMapKeys(headers map[string]string) map[string]string {
 	delete(headers, "$tracing$uber-trace-id")
 	return headers
