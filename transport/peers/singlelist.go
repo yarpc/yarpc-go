051ee77018bd4e0d1f8eb5b33283d74175bcbb18
Will Hughes <willhughes01@gmail.com>
2016-11-01 18:44:06 -0700

    [PeerList][Part 5] SinglePeerList
    Summary: This PR creates a concrete peerlist implementation that has a
    single peer implementation.

---
+++ transport/peers/singlelist.go
@@ -0,0 +1,77 @@
+package peers
+
+import (
+	"context"
+
+	"go.uber.org/yarpc/internal/errors"
+	"go.uber.org/yarpc/transport"
+
+	"github.com/uber-go/atomic"
+)
+
+type singlePeerList struct {
+	peerID  transport.PeerIdentifier
+	peer    transport.Peer
+	agent   transport.PeerAgent
+	started *atomic.Bool
+}
+
+// NewSinglePeerList creates a static PeerList with a single Peer
+func NewSinglePeerList(pi transport.PeerIdentifier, agent transport.PeerAgent) transport.PeerList {
+	return &singlePeerList{
+		peerID:  pi,
+		agent:   agent,
+		started: atomic.NewBool(false),
+	}
+}
+
+func (pl *singlePeerList) Start() error {
+	if pl.started.Swap(true) {
+		return errors.ErrOutboundAlreadyStarted("SinglePeerList")
+	}
+	peer, err := pl.agent.RetainPeer(pl.peerID, pl)
+	if err != nil {
+		pl.started.Swap(false)
+		return err
+	}
+	pl.peer = peer
+	return nil
+}
+
+func (pl *singlePeerList) Stop() error {
+	if !pl.started.Swap(false) {
+		return errors.ErrOutboundNotStarted("SinglePeerList")
+	}
+	err := pl.agent.ReleasePeer(pl.peerID, pl)
+	if err != nil {
+		return err
+	}
+
+	pl.peer = nil
+	return nil
+}
+
+func (pl *singlePeerList) ChoosePeer(context.Context, *transport.Request) (transport.Peer, error) {
+	if !pl.started.Load() {
+		return nil, errors.ErrOutboundNotStarted("peerlist was not started")
+	}
+	return pl.peer, nil
+}
+
+// NotifyAvailable when a Peer can accept requests
+func (pl *singlePeerList) NotifyAvailable(transport.Peer) error {
+	return nil // Noop
+}
+
+// NotifyConnecting when a Peer is connecting
+func (pl *singlePeerList) NotifyConnecting(transport.Peer) error {
+	return nil // Noop
+}
+
+// NotifyUnavailable when a Peer is cannot handle requests
+func (pl *singlePeerList) NotifyUnavailable(transport.Peer) error {
+	return nil // Noop
+}
+
+// NotifyPending when the number of Pending requests changes
+func (pl *singlePeerList) NotifyPending(transport.Peer) {}
