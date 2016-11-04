3ffef9d68b24721ba53e44bf959f39b65f855804
Will Hughes <willhughes01@gmail.com>
2016-11-03 17:55:59 -0700

    Tmp Start fixup, (Cleanup this diff)

--- transport/http/agent.go
+++ transport/http/agent.go
@@ -13,8 +13,14 @@ import (
 type Agent struct {
 	sync.Mutex
 
-	client *http.Client
-	peers  map[string]*peers.HostPortPeer
+	client    *http.Client
+	peerNodes map[string]*peerNode
+}
+
+// peerNode keeps track of a HostPortPeer and any subscribers referencing it
+type peerNode struct {
+	peer       *peers.HostPortPeer
+	references map[transport.PeerSubscriber]bool
 }
 
 // NewDefaultAgent creates an http agent with the default parameters
@@ -25,8 +31,8 @@ func NewDefaultAgent() *Agent {
 // NewAgent creates a new http agent for managing peers and sending requests
 func NewAgent(cfg *outboundConfig) *Agent {
 	return &Agent{
-		client: buildClient(cfg),
-		peers:  make(map[string]*peers.HostPortPeer),
+		client:    buildClient(cfg),
+		peerNodes: make(map[string]*peerNode),
 	}
 }
 
@@ -36,33 +42,38 @@ func (a *Agent) GetClient() *http.Client {
 }
 
 // RetainPeer gets or creates a Peer for the specificed PeerList
-func (a *Agent) RetainPeer(id transport.PeerIdentifier, list transport.PeerSubscriber) (transport.Peer, error) {
+func (a *Agent) RetainPeer(pid transport.PeerIdentifier, sub transport.PeerSubscriber) (transport.Peer, error) {
 	a.Mutex.Lock()
 	defer a.Mutex.Unlock()
 
-	p := a.getOrCreatePeer(id)
+	node := a.getOrCreatePeerNode(pid)
 
-	p.OnRetain(list)
-	return p, nil
+	node.references[sub] = true
+
+	return node.peer, nil
 }
 
-func (a *Agent) getOrCreatePeer(pid transport.PeerIdentifier) *peers.HostPortPeer {
-	if p, ok := a.peers[pid.Identifier()]; ok {
-		return p
+func (a *Agent) getOrCreatePeerNode(pid transport.PeerIdentifier) *peerNode {
+	if node, ok := a.peerNodes[pid.Identifier()]; ok {
+		return node
 	}
 
-	p := peers.NewPeer(pid, a)
+	peer := peers.NewPeer(pid, a, a)
+	node := &peerNode{
+		peer:       peer,
+		references: make(map[transport.PeerSubscriber]bool),
+	}
+	a.peerNodes[peer.Identifier()] = node
 
-	a.peers[p.Identifier()] = p
-	return p
+	return node
 }
 
-// ReleasePeer removes a peer from the PeerList and removes that peer from the Agent if nothing is using it
-func (a *Agent) ReleasePeer(id transport.PeerIdentifier, list transport.PeerSubscriber) error {
+// ReleasePeer releases a peer from the peersubscriber and removes that peer from the Agent if nothing is listening to it
+func (a *Agent) ReleasePeer(id transport.PeerIdentifier, sub transport.PeerSubscriber) error {
 	a.Mutex.Lock()
 	defer a.Mutex.Unlock()
 
-	p, ok := a.peers[id.Identifier()]
+	node, ok := a.peerNodes[id.Identifier()]
 	if !ok {
 		return errors.ErrAgentHasNoReferenceToPeer{
 			Agent:          a,
@@ -70,14 +81,48 @@ func (a *Agent) ReleasePeer(id transport.PeerIdentifier, list transport.PeerSubs
 		}
 	}
 
-	err := p.OnRelease(list)
-	if err != nil {
-		return err
+	_, ok = node.references[sub]
+	if !ok {
+		return errors.ErrPeerHasNoReferenceToSubscriber{
+			Peer:           node.peer,
+			PeerSubscriber: sub,
+		}
 	}
 
-	if p.References() == 0 {
-		delete(a.peers, id.Identifier())
+	delete(node.references, sub)
+
+	if len(node.references) == 0 {
+		delete(a.peerNodes, id.Identifier())
 	}
 
 	return nil
+}
+
+func (a *Agent) NotifyAvailable(peer transport.Peer) error {
+	return nil
+}
+
+func (a *Agent) NotifyConnecting(peer transport.Peer) error {
+	return nil
+}
+
+// The Peer Notifies the PeerSubscriber that it is ineligible for requests
+func (a *Agent) NotifyUnavailable(peer transport.Peer) error {
+	return nil
+}
+
+// The Peer Notifies the PeerSubscriber when its pending request count changes (maybe to 0).
+func (a *Agent) NotifyPendingUpdate(peer transport.Peer) {
+	a.Mutex.Lock()
+	defer a.Mutex.Unlock()
+
+	node, ok := a.peerNodes[peer.Identifier()]
+	if !ok {
+		// The peer has probably been released already and this is a request finishing, ignore
+		return
+	}
+
+	for sub, _ := range node.references {
+		sub.NotifyPendingUpdate(peer)
+	}
 }
