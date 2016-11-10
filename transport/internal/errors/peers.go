e71d298609a380838d148745cee8b830984e1e84
Will Hughes <willhughes01@gmail.com>
2016-11-09 22:16:20 -0800

    [PeerList][Part 3] Adding a http PeerAgent (#401)
    Summary: This diff introduces the concept of an http "PeerAgent".
    Instances of this agent have the ability to create hostportpeers for
    peerlists so they can be used in downstream calls. It controls the whole
    lifecycle of a peer, as PeerLists should call retain/release during the
    lifecycle of their peers.

---
+++ transport/internal/errors/peers.go
@@ -0,0 +1,39 @@
+package errors
+
+import (
+	"fmt"
+	"go.uber.org/yarpc/transport"
+)
+
+// ErrPeerHasNoReferenceToSubscriber is called when a Peer is expected
+// to operate on a PeerSubscriber it has no reference to
+type ErrPeerHasNoReferenceToSubscriber struct {
+	PeerIdentifier transport.PeerIdentifier
+	PeerSubscriber transport.PeerSubscriber
+}
+
+func (e ErrPeerHasNoReferenceToSubscriber) Error() string {
+	return fmt.Sprintf("peer (%v) has no reference to peer subscriber (%v)", e.PeerIdentifier, e.PeerSubscriber)
+}
+
+// ErrAgentHasNoReferenceToPeer is called when an agent is expected to
+// operate on a Peer it has no reference to
+type ErrAgentHasNoReferenceToPeer struct {
+	Agent          transport.Agent
+	PeerIdentifier transport.PeerIdentifier
+}
+
+func (e ErrAgentHasNoReferenceToPeer) Error() string {
+	return fmt.Sprintf("agent (%v) has no reference to peer (%v)", e.Agent, e.PeerIdentifier)
+}
+
+// ErrInvalidPeerType is when a specfic peer type is required, but
+// was not passed in
+type ErrInvalidPeerType struct {
+	ExpectedType   string
+	PeerIdentifier transport.PeerIdentifier
+}
+
+func (e ErrInvalidPeerType) Error() string {
+	return fmt.Sprintf("expected peer type (%s) but got peer (%v)", e.ExpectedType, e.PeerIdentifier)
+}
