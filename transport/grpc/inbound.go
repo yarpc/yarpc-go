e88459b13f3e4d3d030a4a630a0cc7de8063e4b8
Will Hughes <willhughes01@gmail.com>
2016-12-07 12:10:27 -0800

    [GRPC][UnreviewdMerge][Part 5] Adding procedure routing on inbound calls (#354)
    * [GRPC][Part 4.5] Fix Lint after golint upgrade
    
    Summary: In a recent commit to golint, there was a new rule added that
    asserts that context.Context must be the first parameter in a function
    definition.  This breaks some of GRPC's functionality since the handler
    interface they require has ctx in the second position.  As a result, we
    are fixing the cases where we have ctx in the second position, and we're
    isolating the GRPC function we provide into it's own file and ignoring
    it in our linting
    
    * Fix the context that's being used
    
    * s/handleImpl/handle/
    
    * Add comment to makefile
    
    * [GRPC][Part 5] Adding procedure routing on inbound calls
    
    Summary: In our "barebones" version of the grpc inbound we did not have
    the ability to call into any service/procedure other than "yarpc/yarpc".
    This diff configures the gRPC services to reflect whatever we're using
    in the YARPC Registry.  As a result we can perform routing to custom
    service-procedures defined in yarpc
    
    Test Plan: Updated the yclient and yserver to route to custom procedures
    and tested locally
    
    * Move serviceDesc generation into a separate function, added a comment to better explain what's going on
    
    * Cleanup after inbound.Start() changes, and a little bit of other cleanup
    
    * Fixup inbound slice creation, and passing fields by reference

--- transport/grpc/inbound.go
+++ transport/grpc/inbound.go
@@ -53,22 +53,7 @@ func (i *inbound) Start(service transport.ServiceDetail, d transport.Deps) error
 		Deps:     d,
 	}
 
-	var serviceDescs []grpc.ServiceDesc
-
-	// TODO generate serviceDescs from yarpc registration info
-	serviceDescs = append(serviceDescs, grpc.ServiceDesc{
-		ServiceName: url.QueryEscape("yarpc"),
-		HandlerType: (*passThroughService)(nil),
-		Methods: []grpc.MethodDesc{
-			{
-				MethodName: url.QueryEscape("yarpc"), // TODO: Is this what we want here?
-				Handler:    gHandler.Handle,          // grpc.methodHandler
-			},
-		},
-		Streams: []grpc.StreamDesc{},
-	})
-
-	// TODO Generate the serviceDesc from the configuration of the dispatcher name and procedure names
+	serviceDescs := getServiceDescs(gHandler)
 
 	// Register Services
 	for _, desc := range serviceDescs {
@@ -79,6 +64,54 @@ func (i *inbound) Start(service transport.ServiceDetail, d transport.Deps) error
 	go i.server.Serve(lis)
 
 	return nil
+}
+
+/*
+ServiceDescs in GRPC define how clients can interact with a server and usually have well defined schemas to match
+request methods with service functions.  For our purposes we are avoiding this check by dynamically creating the
+serviceDescs from the YARPC registered procedures.  This allows us to see what methods are available and to create an
+individual routing mechanism to each one.  All the routes will be forwarded to the same "Handler" which will do YARPC
+routing to handle methods and encodings.
+*/
+func getServiceDescs(gHandler handler) []grpc.ServiceDesc {
+	var serviceDescs []grpc.ServiceDesc
+
+	// Create separate routes for each service & procedure
+	for service, procs := range getServiceProcedures(gHandler.Registry) {
+		serviceDescs = append(serviceDescs, *createServiceDesc(gHandler, service, procs))
+	}
+
+	// If no service procedures are configured insert a default serviceProcedure
+	if len(serviceDescs) == 0 {
+		serviceDescs = append(serviceDescs, *createServiceDesc(gHandler, "yarpc", []string{"yarpc"}))
+	}
+
+	return serviceDescs
+}
+
+func getServiceProcedures(reg transport.Registry) map[string][]string {
+	// Go through the registry to find all the methods that are currently attached to inbounds
+	serviceProcedures := make(map[string][]string)
+	for _, sp := range reg.ServiceProcedures() {
+		serviceProcedures[sp.Service] = append(serviceProcedures[sp.Service], sp.Procedure)
+	}
+	return serviceProcedures
+}
+
+func createServiceDesc(gHandler handler, service string, procedures []string) *grpc.ServiceDesc {
+	methodDescs := make([]grpc.MethodDesc, len(procedures))
+	for i, proc := range procedures {
+		methodDescs[i] = grpc.MethodDesc{
+			MethodName: url.QueryEscape(proc),
+			Handler:    gHandler.Handle, // Catchall method that does custom YARPC routing
+		}
+	}
+
+	return &grpc.ServiceDesc{
+		ServiceName: url.QueryEscape(service),
+		HandlerType: (*passThroughService)(nil),
+		Methods:     methodDescs,
+	}
 }
 
 func (i *inbound) Stop() error {
