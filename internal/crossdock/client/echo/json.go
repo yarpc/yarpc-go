ce34b8b52ac69436f29d15d3985720956eed3cdf
Will Hughes <willhughes01@gmail.com>
2016-12-13 14:06:22 -0800

    [MoveBack!!][Part 1] Move /crossdock/ back to /internal/crossdock
    Summary: We moved this out of internal in order to surface this
    functionality, buuuuut, we don't actually want people to depend on this
    so we're moving this back to internal

---
+++ internal/crossdock/client/echo/json.go
@@ -0,0 +1,63 @@
+// Copyright (c) 2016 Uber Technologies, Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+package echo
+
+import (
+	"context"
+	"time"
+
+	"go.uber.org/yarpc"
+	"go.uber.org/yarpc/encoding/json"
+	disp "go.uber.org/yarpc/internal/crossdock/client/dispatcher"
+	"go.uber.org/yarpc/internal/crossdock/client/random"
+
+	"github.com/crossdock/crossdock-go"
+)
+
+// jsonEcho contains an echo request or response for the JSON echo endpoint.
+type jsonEcho struct {
+	Token string `json:"token"`
+}
+
+// JSON implements the 'json' behavior.
+func JSON(t crossdock.T) {
+	t = createEchoT("json", t)
+	fatals := crossdock.Fatals(t)
+
+	dispatcher := disp.Create(t)
+	fatals.NoError(dispatcher.Start(), "could not start Dispatcher")
+	defer dispatcher.Stop()
+
+	client := json.New(dispatcher.ClientConfig("yarpc-test"))
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	var response jsonEcho
+	token := random.String(5)
+	_, err := client.Call(
+		ctx,
+		yarpc.NewReqMeta().Procedure("echo"),
+		&jsonEcho{Token: token},
+		&response,
+	)
+	crossdock.Fatals(t).NoError(err, "call to echo failed: %v", err)
+	crossdock.Assert(t).Equal(token, response.Token, "server said: %v", response.Token)
+}
---
+++ internal/crossdock/client/oneway/thrift.go
@@ -0,0 +1,58 @@
+// Copyright (c) 2016 Uber Technologies, Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+package oneway
+
+import (
+	"context"
+
+	"go.uber.org/yarpc"
+	"go.uber.org/yarpc/internal/crossdock/thrift/oneway/yarpc/onewayclient"
+
+	"github.com/crossdock/crossdock-go"
+)
+
+// Thrift starts an http oneway run using Thrift encoding
+func Thrift(t crossdock.T, dispatcher *yarpc.Dispatcher, serverCalledBack <-chan []byte, callBackAddr string) {
+	assert := crossdock.Assert(t)
+	fatals := crossdock.Fatals(t)
+
+	client := onewayclient.New(dispatcher.ClientConfig("oneway-server"))
+	token := getRandomID()
+
+	ack, err := client.Echo(
+		context.Background(),
+		yarpc.NewReqMeta().
+			Headers(yarpc.NewHeaders().With("callBackAddr", callBackAddr)),
+		&token)
+
+	// ensure channel hasn't been filled yet
+	select {
+	case <-serverCalledBack:
+		fatals.FailNow("oneway thrift test failed", "client waited for server to fill channel")
+	default:
+	}
+
+	fatals.NoError(err, "call to Oneway::echo failed: %v", err)
+	fatals.NotNil(ack, "ack is nil")
+
+	serverToken := <-serverCalledBack
+	assert.Equal(token, string(serverToken), "Thrift token mismatch")
+}
