4daa65ba18631deb21d361dc56adfaf822fbc8b3
Will Hughes <wh@uber.com>
2016-12-14 14:55:15 -0800

    [Lifecycle][Part 2] Update Transport,Inbound,Outbound, and Chooser to use lifecycle
    Summary: Updates the start/stop interfaces so that they don't declare
    their start/stop functions directly, but, instead do it through the
    lifecycle interface

--- api/transport/inbound.go
+++ api/transport/inbound.go
@@ -25,6 +25,8 @@ package transport
 // Inbound is a transport that knows how to receive requests for procedure
 // calls.
 type Inbound interface {
+	Lifecycle
+
 	// SetRouter configures the inbound to dispatch requests through a
 	// router, typically called by a Dispatcher with its RouteTable of handled
 	// procedures.
@@ -34,22 +36,4 @@ type Inbound interface {
 	// collected for lifecycle management, typically by a Dispatcher.
 	// An inbound may submit zero or more transports.
 	Transports() []Transport
-
-	// Starts accepting new requests.
-	//
-	// The inbound must have a configured router.
-	//
-	// The function MUST return immediately, although it SHOULD block until
-	// the inbound is ready to start accepting new requests.
-	//
-	// Implementations can assume that this function is called at most once.
-	Start() error
-
-	// Stops the inbound. No new requests will be processed.
-	//
-	// This MAY block while the server drains ongoing requests.
-	Stop() error
-
-	// TODO some way for the inbound to expose the host and port it's
-	// listening on
 }
