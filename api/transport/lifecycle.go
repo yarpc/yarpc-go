10b977095b57e6aaa95a70d34c927229ef18f441
Will Hughes <wh@uber.com>
2016-12-15 10:57:12 -0800

    sentences

--- api/transport/lifecycle.go
+++ api/transport/lifecycle.go
@@ -1,13 +1,13 @@
 package transport
 
 // Lifecycle objects are used to define a common Start/Stop functionality
-// across different transport objects
+// across different transport objects.
 type Lifecycle interface {
-	// Start the lifecycle object, returns an error if it cannot be started
-	// Start MUST be idempotent
+	// Start the lifecycle object, returns an error if it cannot be started.
+	// Start MUST be idempotent.
 	Start() error
 
-	// Stop the lifecycle object, returns an error if it cannot be stopped
-	// Stop MUST be idempotent
+	// Stop the lifecycle object, returns an error if it cannot be stopped.
+	// Stop MUST be idempotent.
 	Stop() error
 }
