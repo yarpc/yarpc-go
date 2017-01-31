324ea65c1f135b2d0cf9bd48a42cfaa498c53e40
Prashant Varanasi <github@prashantv.com>
2017-01-30 16:03:40 -0800

    Add heap based PeerList (#714)

---
+++ peer/x/peerheap/heap_test.go
@@ -0,0 +1,124 @@
+package peerheap
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestPeerHeapEmpty(t *testing.T) {
+	var ph peerHeap
+	assert.Zero(t, ph.Len(), "New peer heap should be empty")
+	popAndVerifyHeap(t, &ph)
+}
+
+func TestPeerHeapOrdering(t *testing.T) {
+	p1 := &peerScore{score: 1}
+	p2 := &peerScore{score: 2}
+	p3 := &peerScore{score: 3}
+
+	// same score as p3, but always pushed after p3, so it will be returned last.
+	p4 := &peerScore{score: 3}
+
+	want := []*peerScore{p1, p2, p3, p4}
+	tests := [][]*peerScore{
+		{p1, p2, p3, p4},
+		{p3, p4, p2, p1},
+		{p3, p1, p2, p4},
+	}
+
+	for _, tt := range tests {
+		var h peerHeap
+		for _, ps := range tt {
+			h.pushPeer(ps)
+		}
+
+		popped := popAndVerifyHeap(t, &h)
+		assert.Equal(t, want, popped, "Unexpected ordering of peers")
+	}
+}
+
+func TestPeerHeapUpdate(t *testing.T) {
+	var h peerHeap
+	p1 := &peerScore{score: 1}
+	p2 := &peerScore{score: 2}
+	p3 := &peerScore{score: 3}
+
+	h.pushPeer(p3)
+	h.pushPeer(p1)
+	h.pushPeer(p2)
+
+	ps, ok := h.popPeer()
+	require.True(t, ok, "pop with non-empty heap should succeed")
+	assert.Equal(t, p1, ps, "Wrong peer")
+
+	// Now update p2's score to be higher than p3.
+	p2.score = 10
+	h.update(p2.idx)
+
+	popped := popAndVerifyHeap(t, &h)
+	assert.Equal(t, []*peerScore{p3, p2}, popped, "Unexpected order after p2 update")
+}
+
+func TestPeerHeapDelete(t *testing.T) {
+	const numPeers = 10
+
+	var h peerHeap
+	peers := make([]*peerScore, numPeers)
+	for i := range peers {
+		peers[i] = &peerScore{score: int64(i)}
+		h.pushPeer(peers[i])
+	}
+
+	// The first peer is the lowest, remove it so it swaps with the last peer.
+	h.delete(0)
+
+	// Now when we pop peers, we expect peers 1 to N.
+	want := peers[1:]
+	popped := popAndVerifyHeap(t, &h)
+	assert.Equal(t, want, popped, "Unexpected peers after delete peer 0")
+}
+
+func TestPeerHeapValidate(t *testing.T) {
+	var h peerHeap
+	h.pushPeer(&peerScore{score: 1})
+
+	for _, i := range []int{0, -1, 5} {
+		ps := &peerScore{idx: i}
+		assert.Error(t, h.validate(ps), "peer %v should not validate", ps)
+	}
+}
+
+func popAndVerifyHeap(t *testing.T, h *peerHeap) []*peerScore {
+	var popped []*peerScore
+
+	lastScore := int64(-1)
+	for h.Len() > 0 {
+		verifyIndexes(t, h)
+
+		ps, ok := h.popPeer()
+		require.True(t, ok, "pop with non-empty heap should succeed")
+		popped = append(popped, ps)
+
+		if lastScore == -1 {
+			lastScore = ps.score
+			continue
+		}
+
+		if ps.score < lastScore {
+			t.Fatalf("heap returned peer %v with lower score than %v", ps, lastScore)
+		}
+		lastScore = ps.score
+	}
+
+	_, ok := h.popPeer()
+	require.False(t, ok, "Expected no peers to be returned with empty list")
+	return popped
+}
+
+func verifyIndexes(t *testing.T, h *peerHeap) {
+	for i := range h.peers {
+		assert.Equal(t, i, h.peers[i].idx, "wrong index for peer %v", h.peers[i])
+	}
+}
---
+++ peer/x/peerheap/score.go
@@ -0,0 +1,36 @@
+package peerheap
+
+import "go.uber.org/yarpc/api/peer"
+
+// peerScore is a book-keeping object for each retained peer and
+// gets
+type peerScore struct {
+	// immutable after creation
+	peer        peer.Peer
+	id          peer.Identifier
+	list        *List
+	boundFinish func(error)
+
+	status peer.Status
+	score  int64
+	idx    int // index in the peer list.
+	last   int // snapshot of the heap's incrementing counter.
+}
+
+func (ps *peerScore) NotifyStatusChanged(_ peer.Identifier) {
+	if ps == nil || ps.list == nil {
+		// The subscriber is no longer retained by the peer list, or caller
+		// obtained a nil subscriber (happens in tests).
+		return
+	}
+	status := ps.peer.Status()
+	if ps.status == status {
+		return
+	}
+	ps.status = status
+	ps.list.peerScoreChanged(ps)
+}
+
+func (ps *peerScore) finish(error) {
+	ps.peer.EndRequest()
+}
