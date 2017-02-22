fa9eaa04ca0d677a4971cbe5a7cabc095a550000
Abhinav Gupta <mail@abhinavg.net>
2017-02-21 16:46:06 -0800

    generate.sh: Make updateLicenses.sh separate (#752)
    For cases where you want to update the license headers but don't want to
    wait for `make generate`.

--- scripts/updateLicenses.sh
+++ scripts/updateLicenses.sh
@@ -2,4 +2,17 @@
 
 set -e
 
-python "$(dirname $0)"/updateLicense.py $(go list -json $(glide nv) | jq -r '.Dir + "/" + (.GoFiles | .[])')
+# Regular expression for file paths that don't need licenses.
+#
+# We need to ignore internal/tests for licenses so that the golden test for
+# thriftrw-plugin-yarpc can verify the contents of the generated code without
+# running updateLicenses on it.
+LICENSE_FILTER="/internal/tests/"
+
+DIR="$(cd "$(dirname "${0}")/.." && pwd)"
+cd "${DIR}"
+
+python scripts/updateLicense.py $(go list -json $(glide nv) | \
+	jq -r '.Dir + "/" + (.GoFiles | .[]) | select(test("'"$LICENSE_FILTER"'") | not)')
+rm -rf internal/crossdock/thrift/gen-go/gauntlet_apache/second_service-remote # generated and not needed
+rm -rf internal/crossdock/thrift/gen-go/gauntlet_apache/thrift_test-remote # generated and not needed
