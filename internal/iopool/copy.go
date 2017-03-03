cfc4bd87c138f7f77962d13ddfaa72380b7e2545
Will Hughes <wh@uber.com>
2017-03-03 14:47:32 -0800

    Simplify the implementation, put everything in the same file

--- internal/iopool/copy.go
+++ internal/iopool/copy.go
@@ -1,12 +1,26 @@
 package iopool
 
-import "io"
+import (
+	"io"
+	"sync"
+)
+
+type buffer struct {
+	b []byte
+}
+
+var _copyBufSize = 1024 * 32
+var _pool = sync.Pool{
+	New: func() interface{} {
+		return &buffer{make([]byte, _copyBufSize)}
+	},
+}
 
 // Copy wraps the io library's CopyBuffer func with a preallocated buffer from a
 // sync.Pool we maintain.
 func Copy(dst io.Writer, src io.Reader) (int64, error) {
-	buf := get()
+	buf := _pool.Get().(*buffer)
 	written, err := io.CopyBuffer(dst, src, buf.b)
-	put(buf)
+	_pool.Put(buf)
 	return written, err
 }
--- internal/iopool/copy_test.go
+++ internal/iopool/copy_test.go
@@ -2,7 +2,6 @@ package iopool
 
 import (
 	"bytes"
-	"io"
 	"math/rand"
 	"sync"
 	"testing"
@@ -16,9 +15,6 @@ func TestBuffers(t *testing.T) {
 		wg.Add(1)
 		go func() {
 			for i := 0; i < 100; i++ {
-				buf := get()
-				assert.True(t, len(buf.b) >= _defaultCapacity, "Expected large buffer")
-
 				inputBytes := make([]byte, rand.Intn(5000)+20)
 				_, err := rand.Read(inputBytes)
 				assert.NoError(t, err, "Unexpected error from rand.Read")
@@ -27,11 +23,7 @@ func TestBuffers(t *testing.T) {
 				outputBytes := make([]byte, 0, len(inputBytes))
 				writer := bytes.NewBuffer(outputBytes)
 
-				io.CopyBuffer(writer, reader, buf.b)
-
-				assert.Equal(t, outputBytes[:20], inputBytes[:20], "Input and output from copy did not match")
-
-				put(buf)
+				Copy(writer, reader)
 			}
 			wg.Done()
 		}()
