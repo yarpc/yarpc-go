757369aedded34bdd18de737b8dbeec4f2de9efc
Will Hughes <wh@uber.com>
2017-03-06 16:31:38 -0800

    Nitz and rebase

--- internal/iopool/copy.go
+++ internal/iopool/copy.go
@@ -9,16 +9,18 @@ type buffer struct {
 	b []byte
 }
 
-var _copyBufSize = 1024 * 32
+const _copyBufSize = 1024 * 32
+
 var _pool = sync.Pool{
 	New: func() interface{} {
 		return &buffer{make([]byte, _copyBufSize)}
 	},
 }
 
-// Copy wraps the io library's CopyBuffer func with a preallocated buffer from a
-// sync.Pool we maintain.
+// Copy copies bytes from the Reader to the Writer until the Reader is exhausted.
 func Copy(dst io.Writer, src io.Reader) (int64, error) {
+	// To avoid unecessary memory allocations we maintain our own pool of
+	// buffers.
 	buf := _pool.Get().(*buffer)
 	written, err := io.CopyBuffer(dst, src, buf.b)
 	_pool.Put(buf)
