3866fd5b45356bd601494277c03d0aafc0b494a6
Will Hughes <wh@uber.com>
2017-03-06 17:19:25 -0800

    Remove limits from thrift decoder buffer pool
    Summary: One of the first use cases that moved to this buffer did not
    notice any significant change in their timings because their payloads
    were over the limit we set.
    
    Talked it over with @prashant and we agreed that we can remove the
    buffer and add it back if it becomes an issue.

--- internal/buffer/pool_test.go
+++ internal/buffer/pool_test.go
@@ -32,29 +32,3 @@ func TestBuffers(t *testing.T) {
 	}
 	wg.Wait()
 }
-
-func TestBufferWithMaxCapacity(t *testing.T) {
-	var wg sync.WaitGroup
-	for g := 0; g < 10; g++ {
-		wg.Add(1)
-		go func() {
-			for i := 0; i < 100; i++ {
-				buf := Get()
-				assert.Zero(t, buf.Len(), "Expected truncated buffer")
-				assert.True(t, buf.Cap() < _maxCapacity, "Expected buffer to not exceed the max capacity")
-
-				overCapacityByteSlice := make([]byte, _maxCapacity+10)
-				_, err := rand.Read(overCapacityByteSlice)
-				assert.NoError(t, err, "Unexpected error from rand.Read")
-				_, err = buf.Write(overCapacityByteSlice)
-				assert.NoError(t, err, "Unexpected error from buffer.Write")
-
-				assert.Equal(t, buf.Len(), len(overCapacityByteSlice), "Expected same buffer size")
-
-				Put(buf)
-			}
-			wg.Done()
-		}()
-	}
-	wg.Wait()
-}
