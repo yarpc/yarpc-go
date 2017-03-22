fff8262b7294c44bf555244002c685aa2b01b83f
Peter Edge <pedge@uber.com>
2017-03-22 14:19:45 +0100

    Start working on oneway

--- scripts/protogenerate.sh
+++ scripts/protogenerate.sh
@@ -5,9 +5,15 @@ set -euo pipefail
 DIR="$(cd "$(dirname "${0}")/.." && pwd)"
 cd "${DIR}"
 
+if echo "${GOPATH}" | grep : >/dev/null; then
+  echo "error: GOPATH can only contain one directory but is ${GOPATH}" >&2
+  exit 1
+fi
+
 protoc_with_imports() {
   protoc \
-    -I vendor/github.com/gogo/protobuf \
+    -I "${GOPATH}/src" \
+    -I vendor \
     -I vendor/github.com/gogo/protobuf/protobuf \
     -I . \
     --${1}_out=Mgoogle/protobuf/descriptor.proto=github.com/gogo/protobuf/protoc-gen-gogo/descriptor,Mgogoproto/gogo.proto=github.com/gogo/protobuf/gogoproto:. \
