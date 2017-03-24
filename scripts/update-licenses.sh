414e18b96a9954ab9e2756d534590bc93a8fea40
Peter Edge <pedge@uber.com>
2017-03-24 15:07:04 +0100

    More fixes and cleanup

--- scripts/update-licenses.sh
+++ scripts/update-licenses.sh
@@ -9,5 +9,3 @@ cd "${DIR}"
 # thriftrw-plugin-yarpc can verify the contents of the generated code without
 # running updateLicenses on it.
 update-license $(find . -name '*.go' | grep -v ^\.\/vendor | grep -v \/thriftrw-plugin-yarpc\/internal\/tests\/)
-rm -rf internal/crossdock/thrift/gen-go/gauntlet_apache/second_service-remote # generated and not needed
-rm -rf internal/crossdock/thrift/gen-go/gauntlet_apache/thrift_test-remote # generated and not needed
