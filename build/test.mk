91445651d129577d7a68cfcefc0ce4d5614ee605
Peter Edge <pedge@uber.com>
2017-03-31 16:10:41 +0200

    Try to fix travis

--- build/test.mk
+++ build/test.mk
@@ -116,7 +116,7 @@ goveralls: cover $(GOVERALLS)
 
 .PHONY: examples
 examples:
-	RUN=$(RUN) $(MAKE) -C internal/examples json-keyvalue
+	RUN=$(RUN) $(MAKE) -C internal/examples json-keyvalue-http json-keyvalue-tchannel
 
 .PHONY: __eval_packages
 __eval_packages:
