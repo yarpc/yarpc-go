6b3e1832c207e8d2b7bd75c6774dac7e304a14b2
Peter Edge <pedge@uber.com>
2017-04-02 04:16:51 +0200

    Clean up service-test

---
+++ internal/service-test/cmd.go
@@ -0,0 +1,193 @@
+// Copyright (c) 2017 Uber Technologies, Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"strings"
+	"sync"
+	"syscall"
+	"time"
+
+	"github.com/mattn/go-shellwords"
+)
+
+type cmd struct {
+	cmd           *exec.Cmd
+	sleep         time.Duration
+	output        string
+	debug         bool
+	stdout        *bytes.Buffer
+	stderr        *bytes.Buffer
+	flushedStdout bool
+	flushedStderr bool
+	killed        bool
+	lock          sync.Mutex
+}
+
+func newCmd(cmdConfig *cmdConfig, dir string, debug bool) (*cmd, error) {
+	parser := shellwords.NewParser()
+	parser.ParseEnv = true
+	args, err := parser.Parse(cmdConfig.Command)
+	if err != nil {
+		return nil, err
+	}
+	if len(args) == 0 {
+		return nil, fmt.Errorf("command evaulated to empty: %s", cmdConfig.Command)
+	}
+	execCmd := exec.Command(args[0], args[1:]...)
+	execCmd.Dir = dir
+	// https://medium.com/@felixge/killing-a-child-process-and-all-of-its-children-in-go-54079af94773
+	execCmd.SysProcAttr = &syscall.SysProcAttr{Setpgid: true}
+	cmd := &cmd{
+		execCmd,
+		time.Duration(cmdConfig.SleepMs) * time.Millisecond,
+		cmdConfig.Output,
+		debug,
+		bytes.NewBuffer(nil),
+		bytes.NewBuffer(nil),
+		false,
+		false,
+		false,
+		sync.Mutex{},
+	}
+	if cmdConfig.Input != "" {
+		execCmd.Stdin = strings.NewReader(cmdConfig.Input)
+	}
+	execCmd.Stdout = cmd.stdout
+	execCmd.Stderr = cmd.stderr
+	return cmd, nil
+}
+
+func (c *cmd) Start() error {
+	c.debugPrintf("starting")
+	if err := c.cmd.Start(); err != nil {
+		return c.wrapError("failed to start", err)
+	}
+	c.debugPrintf("started")
+	if c.sleep != 0 {
+		c.debugPrintf("sleeping")
+		<-time.After(c.sleep)
+		c.debugPrintf("done sleeping")
+	}
+	return nil
+}
+
+func (c *cmd) Wait() error {
+	c.debugPrintf("waiting")
+	if err := c.cmd.Wait(); err != nil {
+		return c.wrapError("failed", err)
+	}
+	c.debugPrintf("finished")
+	return nil
+}
+
+func (c *cmd) Validate() error {
+	if c.output == "" {
+		return nil
+	}
+	output := cleanOutput(string(c.stdout.Bytes()))
+	expectedOutput := cleanOutput(c.output)
+	if output != expectedOutput {
+		return c.wrapError("validation failed", fmt.Errorf("expected\n%s\ngot\n%s", expectedOutput, output))
+	}
+	return nil
+}
+
+func (c *cmd) Clean(suppressStdout bool) {
+	c.Kill()
+	if !suppressStdout {
+		c.FlushStdout()
+	}
+	c.FlushStderr()
+}
+
+func (c *cmd) Kill() {
+	c.lock.Lock()
+	defer c.lock.Unlock()
+	if c.killed {
+		return
+	}
+	if c.cmd.Process != nil {
+		c.debugPrintf("killing")
+		// https://medium.com/@felixge/killing-a-child-process-and-all-of-its-children-in-go-54079af94773
+		_ = syscall.Kill(-c.cmd.Process.Pid, syscall.SIGKILL)
+		c.killed = true
+	}
+}
+
+func (c *cmd) FlushStdout() {
+	c.lock.Lock()
+	defer c.lock.Unlock()
+	if c.flushedStdout {
+		return
+	}
+	if data := c.stdout.Bytes(); len(data) > 0 {
+		fmt.Print(string(data))
+	}
+	c.flushedStdout = true
+}
+
+func (c *cmd) FlushStderr() {
+	c.lock.Lock()
+	defer c.lock.Unlock()
+	if c.flushedStderr {
+		return
+	}
+	if data := c.stderr.Bytes(); len(data) > 0 {
+		fmt.Fprint(os.Stderr, string(data))
+	}
+	c.flushedStderr = true
+}
+
+func (c *cmd) String() string {
+	if len(c.cmd.Args) == 0 {
+		return c.cmd.Path
+	}
+	return strings.Join(c.cmd.Args, " ")
+}
+
+func (c *cmd) wrapError(msg string, err error) error {
+	return fmt.Errorf("%v: %s: %v", c, msg, err)
+}
+
+func (c *cmd) debugPrintf(format string, args ...interface{}) {
+	if c.debug {
+		args = append([]interface{}{c}, args...)
+		log.Printf("%v: "+format, args...)
+	}
+}
+
+func cleanOutput(output string) string {
+	output = strings.TrimSpace(output)
+	lines := strings.Split(output, "\n")
+	cleanedLines := make([]string, 0, len(lines))
+	for _, line := range lines {
+		if line != "" {
+			cleanedLines = append(cleanedLines, strings.TrimSpace(line))
+		}
+	}
+	return strings.Join(cleanedLines, "\n")
+}
--- internal/service-test/main.go
+++ internal/service-test/main.go
@@ -21,310 +21,113 @@
 package main
 
 import (
-	"bytes"
 	"errors"
 	"flag"
 	"fmt"
-	"io/ioutil"
 	"log"
 	"os"
-	"os/exec"
 	"os/signal"
 	"path/filepath"
-	"strings"
-	"sync"
-	"syscall"
 	"time"
-
-	"github.com/mattn/go-shellwords"
-	"gopkg.in/yaml.v2"
 )
 
 var (
-	flagContextDir     = flag.String("dir", "", "The relative directory to operate from, defaults to current directory")
+	flagDir            = flag.String("dir", "", "The relative directory to operate from, defaults to current directory")
 	flagConfigFilePath = flag.String("file", "service-test.yaml", "The configuration file to use relative to the context directory")
 	flagTimeout        = flag.Duration("timeout", 5*time.Second, "The time to wait until timing out")
-	flagNoVerifyOutput = flag.Bool("no-verify-output", false, "Do not verify output and just run the commands")
+	flagNoVerifyOutput = flag.Bool("no-validate-output", false, "Do not validate output and just run the commands")
 	flagDebug          = flag.Bool("debug", false, "Log debug information")
 
-	errConfigNil           = errors.New("config nil")
-	errClientCommandNotSet = errors.New("config client_command not set")
+	errSignal = errors.New("signal")
 )
-
-type config struct {
-	RequiredEnvVars     []string `json:"required_env_vars,omitempty" yaml:"required_env_vars,omitempty"`
-	ClientCommand       string   `json:"client_command,omitempty" yaml:"client_command,omitempty"`
-	ServerCommand       string   `json:"server_command,omitempty" yaml:"server_command,omitempty"`
-	SleepBeforeClientMs int      `json:"sleep_before_client_ms,omitempty" yaml:"sleep_before_client_ms,omitempty"`
-	Input               string   `json:"input,omitempty" yaml:"input,omitempty"`
-	ClientOutput        string   `json:"client_output,omitempty" yaml:"client_output,omitempty"`
-	ServerOutput        string   `json:"server_output,omitempty" yaml:"server_output,omitempty"`
-}
 
 func main() {
 	flag.Parse()
-	if err := do(*flagContextDir, *flagConfigFilePath, *flagTimeout, !(*flagNoVerifyOutput)); err != nil {
+	if err := do(
+		*flagDir,
+		*flagConfigFilePath,
+		*flagTimeout,
+		!(*flagNoVerifyOutput),
+		*flagDebug,
+	); err != nil {
 		log.Fatal(err)
 	}
 }
 
-func do(contextDir string, configFilePath string, timeout time.Duration, verifyOutput bool) (err error) {
-	configFilePath = filepath.Join(contextDir, configFilePath)
-	config, err := readConfig(configFilePath)
+func do(
+	dir string,
+	configFilePath string,
+	timeout time.Duration,
+	validateOutput bool,
+	debug bool,
+) (err error) {
+	cmds, err := newCmds(configFilePath, dir, debug)
 	if err != nil {
 		return err
 	}
-	if err := validateRequiredEnvVars(config.RequiredEnvVars); err != nil {
-		return err
-	}
-
-	clientStdout := newLockedBuffer()
-	serverStdout := newLockedBuffer()
-	stderr := newLockedBuffer()
-	clientCmd, err := getCmd(config.ClientCommand)
-	if err != nil {
-		return err
-	}
-	clientCmd.Dir = contextDir
-	clientCmd.Stdout = clientStdout
-	clientCmd.Stderr = stderr
-	var serverCmd *exec.Cmd
-	if config.ServerCommand != "" {
-		serverCmd, err = getCmd(config.ServerCommand)
-		if err != nil {
-			return err
-		}
-		serverCmd.Dir = contextDir
-		serverCmd.Stdout = serverStdout
-		serverCmd.Stderr = stderr
-	}
-	defer cleanup(clientCmd, serverCmd, clientStdout, serverStdout, stderr, verifyOutput, err)
+	defer cleanupCmds(cmds, validateOutput, err)
 	signalC := make(chan os.Signal, 1)
 	signal.Notify(signalC, os.Interrupt)
 	go func() {
 		for range signalC {
-			cleanup(clientCmd, serverCmd, clientStdout, serverStdout, stderr, verifyOutput, errors.New(""))
+			cleanupCmds(cmds, validateOutput, errSignal)
 		}
 		os.Exit(1)
 	}()
 
-	inputBuffer := bytes.NewBuffer(nil)
-	if config.Input != "" {
-		if _, err := inputBuffer.Write([]byte(config.Input)); err != nil {
-			return err
-		}
-	}
-	clientCmd.Stdin = inputBuffer
-
 	errC := make(chan error)
-	go func() {
-		if serverCmd != nil {
-			debugPrintf("Starting %s", cmdString(serverCmd))
-			if err := serverCmd.Start(); err != nil {
-				errC <- fmt.Errorf("error starting server: %v", err)
-				return
-			}
-			debugPrintf("Started %s", cmdString(serverCmd))
-			defer func() {
-				if serverCmd != nil {
-					killCmd(serverCmd)
-					_ = serverCmd.Wait()
-					debugPrintf("Finished %s", cmdString(serverCmd))
-				}
-			}()
-			// kind of weird that we can timeout too
-			// maybe add this to the timeout
-			if config.SleepBeforeClientMs != 0 {
-				sleepDuration := time.Duration(config.SleepBeforeClientMs) * time.Millisecond
-				debugPrintf("Sleeping %v before starting %s", sleepDuration, cmdString(clientCmd))
-				<-time.After(sleepDuration)
-			}
-		}
-		debugPrintf("Starting %s", cmdString(clientCmd))
-		if err := clientCmd.Start(); err != nil {
-			errC <- fmt.Errorf("error starting client: %v", err)
-			return
-		}
-		debugPrintf("Started %s, now waiting", cmdString(clientCmd))
-		if err := clientCmd.Wait(); err != nil {
-			errC <- fmt.Errorf("error on client wait: %v", err)
-			return
-		}
-		debugPrintf("Finished %s", cmdString(clientCmd))
-		errC <- nil
-	}()
+	go func() { errC <- runCmds(cmds) }()
 	select {
 	case err := <-errC:
 		if err != nil {
 			return err
 		}
 	case <-time.After(timeout):
-		return fmt.Errorf("client timed out after %v", timeout)
+		return fmt.Errorf("timed out after %v", timeout)
 	}
-	if verifyOutput {
-		if err := validateOutput(clientStdout, config.ClientOutput); err != nil {
-			return err
-		}
-		if err := validateOutput(serverStdout, config.ServerOutput); err != nil {
-			return err
-		}
+	if validateOutput {
+		return validateCmds(cmds)
 	}
 	return nil
 }
 
-func getCmd(argsString string) (*exec.Cmd, error) {
-	parser := shellwords.NewParser()
-	parser.ParseEnv = true
-	args, err := parser.Parse(argsString)
+func newCmds(configFilePath string, dir string, debug bool) ([]*cmd, error) {
+	config, err := newConfig(filepath.Join(dir, configFilePath))
 	if err != nil {
 		return nil, err
 	}
-	if len(args) == 0 {
-		return nil, fmt.Errorf("Command %s evaulated to empty", argsString)
+	return config.Cmds(dir, debug)
+}
+
+func runCmds(cmds []*cmd) error {
+	for i := 0; i < len(cmds)-1; i++ {
+		cmd := cmds[i]
+		if err := cmd.Start(); err != nil {
+			return err
+		}
+		defer func() {
+			cmd.Kill()
+			_ = cmd.Wait()
+		}()
 	}
-	cmd := exec.Command(args[0], args[1:]...)
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	// https://medium.com/@felixge/killing-a-child-process-and-all-of-its-children-in-go-54079af94773
-	cmd.SysProcAttr = &syscall.SysProcAttr{Setpgid: true}
-	return cmd, nil
+	cmd := cmds[len(cmds)-1]
+	if err := cmd.Start(); err != nil {
+		return err
+	}
+	return cmd.Wait()
 }
 
-func cleanup(
-	clientCmd *exec.Cmd,
-	serverCmd *exec.Cmd,
-	clientStdout *lockedBuffer,
-	serverStdout *lockedBuffer,
-	stderr *lockedBuffer,
-	verifyOutput bool,
-	err error,
-) {
-	cleanupCmds(clientCmd, serverCmd)
-	writeOutput(clientStdout, serverStdout, stderr, verifyOutput, err)
-}
-
-func cleanupCmds(cmds ...*exec.Cmd) {
+func validateCmds(cmds []*cmd) error {
 	for _, cmd := range cmds {
-		killCmd(cmd)
-	}
-}
-
-func killCmd(cmd *exec.Cmd) {
-	if cmd != nil && cmd.Process != nil {
-		debugPrintf("Killing %s", cmdString(cmd))
-		// https://medium.com/@felixge/killing-a-child-process-and-all-of-its-children-in-go-54079af94773
-		_ = syscall.Kill(-cmd.Process.Pid, syscall.SIGKILL)
-	}
-}
-
-func writeOutput(
-	clientStdout *lockedBuffer,
-	serverStdout *lockedBuffer,
-	stderr *lockedBuffer,
-	verifyOutput bool,
-	err error,
-) {
-	if !verifyOutput || err != nil {
-		if data := clientStdout.Bytes(); len(data) > 0 {
-			fmt.Print(string(data))
-		}
-		if data := serverStdout.Bytes(); len(data) > 0 {
-			fmt.Print(string(data))
-		}
-	}
-	if data := stderr.Bytes(); len(data) > 0 {
-		fmt.Print(string(data))
-	}
-}
-
-func validateRequiredEnvVars(requiredEnvVars []string) error {
-	for _, requiredEnvVar := range requiredEnvVars {
-		if os.Getenv(requiredEnvVar) == "" {
-			return fmt.Errorf("environment variable %s must be set", requiredEnvVar)
+		if err := cmd.Validate(); err != nil {
+			return err
 		}
 	}
 	return nil
 }
 
-func readConfig(configFilePath string) (*config, error) {
-	data, err := ioutil.ReadFile(configFilePath)
-	if err != nil {
-		return nil, err
+func cleanupCmds(cmds []*cmd, validateOutput bool, err error) {
+	for _, cmd := range cmds {
+		cmd.Clean(validateOutput && err == nil)
 	}
-	config := &config{}
-	if err := yaml.Unmarshal(data, config); err != nil {
-		return nil, err
-	}
-	if err := validateConfig(config); err != nil {
-		return nil, err
-	}
-	return config, nil
-}
-
-func validateConfig(config *config) error {
-	if config == nil {
-		return errConfigNil
-	}
-	if config.ClientCommand == "" {
-		return errClientCommandNotSet
-	}
-	return nil
-}
-
-func validateOutput(stdout *lockedBuffer, expected string) error {
-	if expected == "" {
-		return nil
-	}
-	output := cleanOutput(string(stdout.Bytes()))
-	expectedOutput := cleanOutput(expected)
-	if output != expectedOutput {
-		return fmt.Errorf("expected\n%s\ngot\n%s", expectedOutput, output)
-	}
-	return nil
-}
-
-func cleanOutput(output string) string {
-	output = strings.TrimSpace(output)
-	lines := strings.Split(output, "\n")
-	cleanedLines := make([]string, 0, len(lines))
-	for _, line := range lines {
-		if line != "" {
-			cleanedLines = append(cleanedLines, strings.TrimSpace(line))
-		}
-	}
-	return strings.Join(cleanedLines, "\n")
-}
-
-type lockedBuffer struct {
-	buffer bytes.Buffer
-	lock   sync.RWMutex
-}
-
-func newLockedBuffer() *lockedBuffer {
-	return &lockedBuffer{}
-}
-
-func (l *lockedBuffer) Write(p []byte) (int, error) {
-	l.lock.Lock()
-	defer l.lock.Unlock()
-	return l.buffer.Write(p)
-}
-
-func (l *lockedBuffer) Bytes() []byte {
-	l.lock.RLock()
-	defer l.lock.RUnlock()
-	return l.buffer.Bytes()
-}
-
-func debugPrintf(format string, args ...interface{}) {
-	if *flagDebug {
-		log.Printf(format, args...)
-	}
-}
-
-func cmdString(cmd *exec.Cmd) string {
-	if len(cmd.Args) == 0 {
-		return cmd.Path
-	}
-	return strings.Join(cmd.Args, " ")
 }
