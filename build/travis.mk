f1740dc5cbd6bbfa65890e2b02ab9c5b9fa334dc
Peter Edge <pedge@uber.com>
2017-04-02 21:41:48 +0200

    Try to fix travis

--- build/travis.mk
+++ build/travis.mk
@@ -19,14 +19,14 @@ ifeq ($(TRAVIS_BRANCH),dev)
 ifeq ($(TRAVIS_PULL_REQUEST),false)
 	mkdir -p $(DOCKER_CACHE_DIR)
 ifndef SUPPRESS_CROSSDOCK
-	docker save $(shell docker history -q $(CROSSDOCK_DOCKER_IMAGE) | grep -v '<missing>') | gzip > $(CROSSDOCK_DOCKER_CACHE_FILE)
+	PATH=$$PATH:$(BIN) docker save $(shell docker history -q $(CROSSDOCK_DOCKER_IMAGE) | grep -v '<missing>') | gzip > $(CROSSDOCK_DOCKER_CACHE_FILE)
 endif
-	docker save $(shell docker history -q $(DOCKER_IMAGE) | grep -v '<missing>') | gzip > $(DOCKER_CACHE_FILE)
+	#PATH=$$PATH:$(BIN) docker save $(shell docker history -q $(DOCKER_IMAGE) | grep -v '<missing>') | gzip > $(DOCKER_CACHE_FILE)
 endif
 endif
 
 .PHONY: travis-docker-push
 travis-docker-push: ## push crossdock docker image from travis
 ifndef SUPPRESS_CROSSDOCK
-	./scripts/travis-docker-push.sh "$(CROSSDOCK_DOCKER_IMAGE)"
+	PATH=$$PATH:$(BIN) ./scripts/travis-docker-push.sh "$(CROSSDOCK_DOCKER_IMAGE)"
 endif
