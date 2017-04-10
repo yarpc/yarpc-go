b159f50e3e60b73664467136203ff9972bd77d90
Peter Edge <pedge@uber.com>
2017-04-10 18:47:42 +0200

    Fix lint

--- build/local.mk
+++ build/local.mk
@@ -1,7 +1,7 @@
 # Paths besides auto-detected generated files that should be excluded from
 # lint results.
 LINT_EXCLUDES_EXTRAS = \
-	transport/x/grpc/handlers.go
+	transport/x/grpc/handler.go
 
 # Regex for 'go vet' rules to ignore
 GOVET_IGNORE_RULES = \
