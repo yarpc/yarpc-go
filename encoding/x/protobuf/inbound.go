ac5cd336979ae559409bce54030e61555ffbaab3
Peter Edge <pedge@uber.com>
2017-04-11 13:40:04 +0200

    Add comment about response headers

--- encoding/x/protobuf/inbound.go
+++ encoding/x/protobuf/inbound.go
@@ -81,6 +81,13 @@ func (u *unaryHandler) Handle(ctx context.Context, transportRequest *transport.R
 		}
 		responseData = protoBuffer.Bytes()
 	}
+	// We have to detect if our transport requires a raw response
+	// It is not possible to propagate this information on ctx with the current API
+	// we we attach this in the relevant transport (currently only gRPC) on the headers
+	// If we are sending a raw response back to a YARPC client, it needs to understand
+	// this is happening, so we attach the headers on the response as well
+	// Other clients (namely the existing gRPC clients outside of YARPC) understand
+	// that the response is the raw response.
 	if isRawResponse(transportRequest.Headers) {
 		responseWriter.AddHeaders(getRawResponseHeaders())
 		_, err := responseWriter.Write(responseData)
