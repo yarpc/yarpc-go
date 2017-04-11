9f8be5dbc113ba551c61aef4cec9104b84139a56
Abhinav Gupta <mail@abhinavg.net>
2017-04-10 18:02:12 -0700

    Generic string interpolation (#888)
    This implements a general string interpolation package that we intend to
    use in the configuration system to allow arbitray interpolation in YAML
    values based on environment variables.
    
    This supports variables in the form `${foo}` and `${foo:default}`
    anywhere in a string.

---
+++ internal/interpolate/types_test.go
@@ -0,0 +1,66 @@
+// Copyright (c) 2017 Uber Technologies, Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+package interpolate
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestRender(t *testing.T) {
+	tests := []struct {
+		give String
+		vars map[string]string
+
+		want    string
+		wantErr string
+	}{
+		{
+			give: String{literal("foo "), literal("bar")},
+			want: "foo bar",
+		},
+		{
+			give: String{literal("foo"), variable{Name: "bar"}},
+			vars: map[string]string{"bar": "baz"},
+			want: "foobaz",
+		},
+		{
+			give:    String{literal("foo"), variable{Name: "baz"}},
+			vars:    map[string]string{"foo": "bar"},
+			wantErr: `variable "baz" does not have a value or a default`,
+		},
+	}
+
+	for _, tt := range tests {
+		got, err := tt.give.Render(mapResolver(tt.vars))
+		if tt.wantErr != "" {
+			if assert.Error(t, err) {
+				assert.Contains(t, err.Error(), tt.wantErr)
+			}
+			continue
+		}
+
+		if assert.NoError(t, err) {
+			assert.Equal(t, tt.want, got)
+		}
+	}
+}
