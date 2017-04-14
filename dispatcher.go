ad0257050026bf7002999ad583b2f70385eebc1c
Akshay Shah <shah@uber.com>
2017-04-14 16:19:25 -0700

    Export logger field in configuration
    This PR builds upon #940 and completes more of T841919. It exposes the logging
    middleware (introduced in several earlier PRs) to users by exporting the
    `Logger` field of YARPC's configuration struct.
    
    To complete YARPC's initial request logging integration, a follow-on PR will
    alter our internal init framework to populate the logger field.

--- dispatcher.go
+++ dispatcher.go
@@ -72,9 +72,7 @@ type Config struct {
 
 	// Logger provides a logger for the dispatcher. The default logger is a
 	// no-op.
-	// TODO(shah): Export this when we're ready to deploy a branch in
-	// demo-yarpc-go.
-	logger *zap.Logger
+	Logger *zap.Logger
 }
 
 // Inbounds contains a list of inbound transports. Each inbound transport
@@ -114,8 +112,8 @@ func NewDispatcher(cfg Config) *Dispatcher {
 	}
 
 	logger := zap.NewNop()
-	if cfg.logger != nil {
-		logger = cfg.logger.Named("yarpc").With(
+	if cfg.Logger != nil {
+		logger = cfg.Logger.Named("yarpc").With(
 			zap.Namespace("yarpc"), // isolate yarpc's keys
 			zap.String("dispatcher", cfg.Name),
 		)
