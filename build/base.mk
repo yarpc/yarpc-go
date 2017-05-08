0ed249c1054846e3c978275b90e175096099b3c8
François-Xavier Bourlet <bombela@uber.com>
2017-05-08 13:18:57 -0700

    attempt to be smarter about cache location

--- build/base.mk
+++ build/base.mk
@@ -1,7 +1,11 @@
 UNAME_OS := $(shell uname -s)
 UNAME_ARCH := $(shell uname -m)
 
-XDG_CACHE_HOME ?= $(HOME)/.cache
+ifeq ($(UNAME_OS), Darwin)
+  XDG_CACHE_HOME ?= $(HOME)/Library/Caches
+else
+  XDG_CACHE_HOME ?= $(HOME)/.cache
+endif
 
 CACHE_BASE := $(XDG_CACHE_HOME)/yarpc-go
 CACHE := $(CACHE_BASE)/$(UNAME_OS)/$(UNAME_ARCH)
@@ -12,3 +16,6 @@ BIN = $(CACHE)/bin
 .PHONY: clean
 clean: ## remove installed binaries and artifacts
 	rm -rf $(CACHE_BASE)
+
+pp:
+	echo $(CACHE_BASE)
