ee3089b3c38a111a01d639c8def837bebd6dd498
Peter Edge <pedge@uber.com>
2017-05-11 18:06:39 +0200

    Set path properly in __check_docker

--- build/docker.mk
+++ build/docker.mk
@@ -82,12 +82,4 @@ shell: deps ## go into a bash shell in docker with the repository linked as a vo
 
 .PHONY: __check_docker
 __check_docker:
-	@if ! docker version >/dev/null; then \
-		echo "-----------------------------------------------------------------" >&2; \
-		echo "Most of our make commands run inside Docker but you do not appear to have" >&2; \
-		echo "the Docker daemon running. Please start the Docker daemon and try again." >&2; \
-		echo "See https://docs.docker.com/engine/installation/ to get started with Docker." >&2; \
-		echo "Alternatively, set SUPPRESS_DOCKER=1 to run the command locally against your system." >&2; \
-		echo "Docker is not running" >&2; \
-		exit 1; \
-	fi
+	@PATH=$$PATH:$(BIN) ./scripts/check-docker.sh
