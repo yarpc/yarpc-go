37cf866bdb66c264e5130c76d1f38ae41ebd6bfb
Akshay Shah <shah@uber.com>
2017-05-16 00:39:34 -0700

    Make service name validation more explicit
    While it's possible to express YARPC's service name validation with a single
    regexp, we've failed to do so despite extra-careful review from multiple smart
    cookies. This PR switches from regular expressions to a more-explicit,
    multiple-pass approach that is (IMO) much more obvious.
    
    This fixes T916721.

--- internal/servicename.go
+++ internal/servicename.go
@@ -21,27 +21,66 @@
 package internal
 
 import (
-	"fmt"
+	"errors"
 	"regexp"
 )
 
-var (
-	// Must starts with [a-z].
-	// Can contain [a-z] and 0-9] with non-consecutive dashes.
-	validNameRegex = regexp.MustCompile("^[a-z]+([a-z0-9]|[^-]-)*[^-]$")
+// We disallow UUIDs explicitly, though they're otherwise valid patterns.
+var uuidRegex = regexp.MustCompile("[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}")
 
-	// We disallow UUIDs explicitly (they would be accepted by validNameRegex alone)
-	uuidRegex = regexp.MustCompile("[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}")
-)
-
-// ValidateServiceName returns an error if the given name is not a valid
-// service name.
+// ValidateServiceName returns an error if the given servive name is invalid.
+// Valid names are at least two characters long, start with [a-z], contain only
+// [0-9a-z] and non-consecutive hyphens, and end in [0-9a-z].
 func ValidateServiceName(name string) error {
-	if !validNameRegex.MatchString(name) {
-		return fmt.Errorf("service name must begin with a letter and consist only of dash-delimited lower-case ASCII alphanumeric words")
+	if len(name) < 2 {
+		return errors.New("service name must be at least two characters long")
+	}
+	if err := checkFirstCharacter(name); err != nil {
+		return err
+	}
+	if err := checkForbiddenCharacters(name); err != nil {
+		return err
+	}
+	if err := checkHyphens(name); err != nil {
+		return err
 	}
 	if uuidRegex.MatchString(name) {
-		return fmt.Errorf("service name must not contain a UUID")
+		return errors.New("service name must not contain a UUID")
+	}
+	return nil
+}
+
+func checkHyphens(name string) error {
+	for i := 1; i < len(name); i++ {
+		if name[i-1] == '-' && name[i] == '-' {
+			return errors.New("service name must not contain consecutive hyphens")
+		}
+	}
+	if name[len(name)-1] == '-' {
+		return errors.New("service name must not end in a hyphen")
+	}
+	return nil
+}
+
+func checkFirstCharacter(name string) error {
+	if name[0] < 'a' || name[0] > 'z' {
+		return errors.New("service names must start with [a-z]")
+	}
+	return nil
+}
+
+func checkForbiddenCharacters(name string) error {
+	for _, c := range name {
+		switch {
+		case 'a' <= c && c <= 'z':
+			continue
+		case '0' <= c && c <= '9':
+			continue
+		case c == '-':
+			continue
+		default:
+			return errors.New("service name may only contain [0-9a-z] and non-consecutive hyphens")
+		}
 	}
 	return nil
 }
