6d69cfad9a71e9ff8ee9bab6d5074ac222bd52ec
Kris Kowal <kris@uber.com>
2017-05-18 17:01:44 -0700

    Support routing by encoding

--- router.go
+++ router.go
@@ -22,6 +22,7 @@ package yarpc
 
 import (
 	"context"
+	"fmt"
 	"sort"
 
 	"go.uber.org/yarpc/api/transport"
@@ -36,19 +37,27 @@ type serviceProcedure struct {
 	procedure string
 }
 
+type serviceProcedureEncoding struct {
+	service   string
+	procedure string
+	encoding  transport.Encoding
+}
+
 // MapRouter is a Router that maintains a map of the registered
 // procedures.
 type MapRouter struct {
-	defaultService string
-	entries        map[serviceProcedure]transport.Procedure
+	defaultService            string
+	serviceProcedures         map[serviceProcedure]transport.Procedure
+	serviceProcedureEncodings map[serviceProcedureEncoding]transport.Procedure
 }
 
 // NewMapRouter builds a new MapRouter that uses the given name as the
 // default service name.
 func NewMapRouter(defaultService string) MapRouter {
 	return MapRouter{
-		defaultService: defaultService,
-		entries:        make(map[serviceProcedure]transport.Procedure),
+		defaultService:            defaultService,
+		serviceProcedures:         make(map[serviceProcedure]transport.Procedure),
+		serviceProcedureEncodings: make(map[serviceProcedureEncoding]transport.Procedure),
 	}
 }
 
@@ -63,39 +72,76 @@ func (m MapRouter) Register(rs []transport.Procedure) {
 			panic("Expected procedure name not to be empty string in registration")
 		}
 
+		if r.Encoding != "" {
+			// Route to individual handlers for unique combinations of service,
+			// procedure, and encoding. This shall henceforth be the
+			// recommended way for models to register procedures.
+			spe := serviceProcedureEncoding{
+				service:   r.Service,
+				procedure: r.Name,
+				encoding:  r.Encoding,
+			}
+			m.serviceProcedureEncodings[spe] = r
+		}
+
+		// This supports wild card encodings (for backward compatibility,
+		// since type models like Thrift were not previously required to
+		// specify the encoding of every procedure)
 		sp := serviceProcedure{
 			service:   r.Service,
 			procedure: r.Name,
 		}
-		m.entries[sp] = r
+		// Protect against registering ambiguous wildcards
+		if _, ok := m.serviceProcedures[sp]; r.Encoding == "" && ok {
+			panic(fmt.Sprintf("Cannot register multiple procedures with the same name and service without specifying alternate encodings, got ambiguous procedures for (%q, %q)", sp.service, sp.procedure))
+		}
+		m.serviceProcedures[sp] = r
+
 	}
 }
 
 // Procedures returns a list procedures that
 // have been registered so far.
 func (m MapRouter) Procedures() []transport.Procedure {
-	procs := make([]transport.Procedure, 0, len(m.entries))
-	for _, v := range m.entries {
+	procs := make([]transport.Procedure, 0, len(m.serviceProcedures)+len(m.serviceProcedureEncodings))
+	for _, v := range m.serviceProcedureEncodings {
+		procs = append(procs, v)
+	}
+	for _, v := range m.serviceProcedures {
 		procs = append(procs, v)
 	}
 	sort.Sort(proceduresByServiceProcedure(procs))
 	return procs
 }
 
-// Choose retrives the HandlerSpec for the service and procedure noted on the
-// transport request, or returns an unrecognized procedure error (testable with
-// transport.IsUnrecognizedProcedureError(err)).
+// Choose retrives the HandlerSpec for the service, procedure, and encoding
+// noted on the transport request, or returns an unrecognized procedure error
+// (testable with transport.IsUnrecognizedProcedureError(err)).
 func (m MapRouter) Choose(ctx context.Context, req *transport.Request) (transport.HandlerSpec, error) {
-	service, procedure := req.Service, req.Procedure
+	service, procedure, encoding := req.Service, req.Procedure, req.Encoding
 	if service == "" {
 		service = m.defaultService
 	}
 
+	// Fully specified combinations of service, procedure, and encoding shadow
+	// and precede less specific combinations with an encoding wild card.
+	spe := serviceProcedureEncoding{
+		service:   service,
+		procedure: procedure,
+		encoding:  encoding,
+	}
+	if procedure, ok := m.serviceProcedureEncodings[spe]; ok {
+		return procedure.HandlerSpec, nil
+	}
+
+	// Fall back to the original behavior for backward compatibility: route all
+	// encodings to the same procedure, if a model specifies a handler
+	// generically.
 	sp := serviceProcedure{
 		service:   service,
 		procedure: procedure,
 	}
-	if procedure, ok := m.entries[sp]; ok {
+	if procedure, ok := m.serviceProcedures[sp]; ok {
 		return procedure.HandlerSpec, nil
 	}
 
