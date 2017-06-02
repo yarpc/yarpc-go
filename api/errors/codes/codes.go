d863a57848bc6a647f2695c1f390bf1d5d52a620
Peter Edge <pedge@uber.com>
2017-06-02 17:26:37 +0200

    Really fix capitalization

--- api/errors/codes/codes.go
+++ api/errors/codes/codes.go
@@ -22,14 +22,6 @@ package codes
 
 import "strconv"
 
-// Code represents the type of error for an RPC call.
-//
-// Sometimes multiple error codes may apply. Services should return
-// the most specific error code that applies. For example, prefer
-// `OutOfRange` over `FailedPrecondition` if both codes apply.
-// Similarly prefer `NotFound` or `AlreadyExists` over `FailedPrecondition`.
-type Code int
-
 const (
 	// Not an error; returned on success
 	//
@@ -39,7 +31,7 @@ const (
 	// The operation was cancelled, typically by the caller.
 	//
 	// HTTP Mapping: 499 Client Closed Request
-	// Google API Mapping: Code.Cancelled
+	// Google API Mapping: Code.CANCELLED
 	Cancelled Code = 1
 	// Unknown error. For example, this error may be returned when
 	// a `Status` value received from another address space belongs to
@@ -48,7 +40,7 @@ const (
 	// may be converted to this error.
 	//
 	// HTTP Mapping: 500 Internal Server Error
-	// Google API Mapping: Code.Unknown
+	// Google API Mapping: Code.UNKNOWN
 	Unknown Code = 2
 	// The client specified an invalid argument. Note that this differs
 	// from `FailedPrecondition`. `InvalidArgument` indicates arguments
@@ -56,7 +48,7 @@ const (
 	// (e.g., a malformed file name).
 	//
 	// HTTP Mapping: 400 Bad Request
-	// Google API Mapping: Code.InvalidArgument
+	// Google API Mapping: Code.INVALID_ARGUMENT
 	InvalidArgument Code = 3
 	// The deadline expired before the operation could complete. For operations
 	// that change the state of the system, this error may be returned
@@ -65,7 +57,7 @@ const (
 	// enough for the deadline to expire.
 	//
 	// HTTP Mapping: 504 Gateway Timeout
-	// Google API Mapping: Code.DeadlineExceeded
+	// Google API Mapping: Code.DEADLINE_EXCEEDED
 	DeadlineExceeded Code = 4
 	// Some requested entity (e.g., file or directory) was not found.
 	// For privacy reasons, this code *may* be returned when the client
@@ -73,13 +65,13 @@ const (
 	// discouraged.
 	//
 	// HTTP Mapping: 404 Not Found
-	// Google API Mapping: Code.NotFound
+	// Google API Mapping: Code.NOT_FOUND
 	NotFound Code = 5
 	// The entity that a client attempted to create (e.g., file or directory)
 	// already exists.
 	//
 	// HTTP Mapping: 409 Conflict
-	// Google API Mapping: Code.AlreadyExists
+	// Google API Mapping: Code.ALREADY_EXISTS
 	AlreadyExists Code = 6
 	// The caller does not have permission to execute the specified
 	// operation. `PermissionDenied` must not be used for rejections
@@ -89,13 +81,13 @@ const (
 	// instead for those errors).
 	//
 	// HTTP Mapping: 403 Forbidden
-	// Google API Mapping: Code.PermissionDenied
+	// Google API Mapping: Code.PERMISSION_DENIED
 	PermissionDenied Code = 7
 	// Some resource has been exhausted, perhaps a per-user quota, or
 	// perhaps the entire file system is out of space.
 	//
 	// HTTP Mapping: 429 Too Many Requests
-	// Google API Mapping: Code.ResourceExhausted
+	// Google API Mapping: Code.RESOURCE_EXHAUSTED
 	ResourceExhausted Code = 8
 	// The operation was rejected because the system is not in a state
 	// required for the operation's execution. For example, the directory
@@ -114,7 +106,7 @@ const (
 	//      the files are deleted from the directory.
 	//
 	// HTTP Mapping: 400 Bad Request
-	// Google API Mapping: Code.FailedPrecondition
+	// Google API Mapping: Code.FAILED_PRECONDITION
 	FailedPrecondition Code = 9
 	// The operation was aborted, typically due to a concurrency issue such as
 	// a sequencer check failure or transaction abort.
@@ -123,7 +115,7 @@ const (
 	// `Aborted`, and `Unavailable`.
 	//
 	// HTTP Mapping: 409 Conflict
-	// Google API Mapping: Code.Aborted
+	// Google API Mapping: Code.ABORTED
 	Aborted Code = 10
 	// The operation was attempted past the valid range. E.g., seeking or
 	// reading past end-of-file.
@@ -142,20 +134,20 @@ const (
 	// they are done.
 	//
 	// HTTP Mapping: 400 Bad Request
-	// Google API Mapping: Code.OutOfRange
+	// Google API Mapping: Code.OUT_OF_RANGE
 	OutOfRange Code = 11
 	// The operation is not implemented or is not supported/enabled in this
 	// service.
 	//
 	// HTTP Mapping: 501 Not Implemented
-	// Google API Mapping: Code.Unimplemented
+	// Google API Mapping: Code.UNIMPLEMENTED
 	Unimplemented Code = 12
 	// Internal errors. This means that some invariants expected by the
 	// underlying system have been broken. This error code is reserved
 	// for serious errors.
 	//
 	// HTTP Mapping: 500 Internal Server Error
-	// Google API Mapping: Code.Internal
+	// Google API Mapping: Code.INTERNAL
 	Internal Code = 13
 	// The service is currently unavailable. This is most likely a
 	// transient condition, which can be corrected by retrying with
@@ -165,70 +157,79 @@ const (
 	// `Aborted`, and `Unavailable`.
 	//
 	// HTTP Mapping: 503 Service Unavailable
-	// Google API Mapping: Code.Unavailable
+	// Google API Mapping: Code.UNAVAILABLE
 	Unavailable Code = 14
 	// Unrecoverable data loss or corruption.
 	//
 	// HTTP Mapping: 500 Internal Server Error
-	// Google API Mapping: Code.DataLoss
+	// Google API Mapping: Code.DATA_LOSS
 	DataLoss Code = 15
 	// The request does not have valid authentication credentials for the
 	// operation.
 	//
 	// HTTP Mapping: 401 Unauthorized
-	// Google API Mapping: Code.Unauthenticated
+	// Google API Mapping: Code.UNAUTHENTICATED
 	Unauthenticated Code = 16
 	// Application error. This will typically be accompianed by a user-defined
 	// error name across the wire.
 	//
 	// HTTP Mapping: 500 Internal Server Error
-	// Google API Mapping: Code.Unknown
+	// Google API Mapping: Code.UNKNOWN
 	Application Code = 17
 )
 
 var (
 	codeToString = map[int]string{
-		0:  "None",
-		1:  "Cancelled",
-		2:  "Unknown",
-		3:  "InvalidArgument",
-		4:  "DeadlineExceeded",
-		5:  "NotFound",
-		6:  "AlreadyExists",
-		7:  "PermissionDenied",
-		8:  "ResourceExhausted",
-		9:  "FailedPrecondition",
-		10: "Aborted",
-		11: "OutOfRange",
-		12: "Unimplemented",
-		13: "Internal",
-		14: "Unavailable",
-		15: "DataLoss",
-		16: "Unauthenticated",
-		17: "Application",
+		0:  "none",
+		1:  "cancelled",
+		2:  "unknown",
+		3:  "invalid-argument",
+		4:  "deadline-exceeded",
+		5:  "not-found",
+		6:  "already-exists",
+		7:  "permission-denied",
+		8:  "resource-exhausted",
+		9:  "failed-precondition",
+		10: "aborted",
+		11: "out-of-range",
+		12: "unimplemented",
+		13: "internal",
+		14: "unavailable",
+		15: "data-loss",
+		16: "unauthenticated",
+		17: "application",
 	}
 	stringToCode = map[string]int{
-		"None":                0,
-		"Cancelled":           1,
-		"Unknown":             2,
-		"InvalidArgument":    3,
-		"DeadlineExceeded":   4,
-		"NotFound":           5,
-		"AlreadyExists":      6,
-		"PermissionDenied":   7,
-		"ResourceExhausted":  8,
-		"FailedPrecondition": 9,
-		"Aborted":             10,
-		"OutOfRange":        11,
-		"Unimplemented":       12,
-		"Internal":            13,
-		"Unavailable":         14,
-		"DataLoss":           15,
-		"Unauthenticated":     16,
-		"Application":         17,
+		"none":                0,
+		"cancelled":           1,
+		"unknown":             2,
+		"invalid-argument":    3,
+		"deadline-exceeded":   4,
+		"not-found":           5,
+		"already-exists":      6,
+		"permission-denied":   7,
+		"resource-exhausted":  8,
+		"failed-precondition": 9,
+		"aborted":             10,
+		"out-of-range":        11,
+		"unimplemented":       12,
+		"internal":            13,
+		"unavailable":         14,
+		"data-loss":           15,
+		"unauthenticated":     16,
+		"application":         17,
 	}
 )
 
+// Code represents the type of error for an RPC call.
+//
+// Sometimes multiple error codes may apply. Services should return
+// the most specific error code that applies. For example, prefer
+// `OutOfRange` over `FailedPrecondition` if both codes apply.
+// Similarly prefer `NotFound` or `AlreadyExists` over `FailedPrecondition`.
+type Code int
+
+// String returns the the string representation of the Code.
 func (c Code) String() string {
 	s, ok := codeToString[int(c)]
 	if ok {
