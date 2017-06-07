2844195f00f73fc74f8950a889db2d0d947574c9
Peter Edge <pedge@uber.com>
2017-06-07 15:21:40 +0200

    Fix lint

--- api/errors/codes/codes.go
+++ api/errors/codes/codes.go
@@ -27,17 +27,19 @@ import (
 )
 
 const (
-	// Not an error; returned on success
+	// None means no error; returned on success
 	//
 	// HTTP Mapping: 200 OK
 	// Google API Mapping: Code.OK
 	None Code = 0
-	// The operation was cancelled, typically by the caller.
+
+	// Cancelled means the operation was cancelled, typically by the caller.
 	//
 	// HTTP Mapping: 499 Client Closed Request
 	// Google API Mapping: Code.CANCELLED
 	Cancelled Code = 1
-	// Unknown error. For example, this error may be returned when
+
+	// Unknown means an unknown error. For example, this error may be returned when
 	// a `Status` value received from another address space belongs to
 	// an error space that is not known in this address space. Also
 	// errors raised by APIs that do not return enough error information
@@ -46,7 +48,8 @@ const (
 	// HTTP Mapping: 500 Internal Server Error
 	// Google API Mapping: Code.UNKNOWN
 	Unknown Code = 2
-	// The client specified an invalid argument. Note that this differs
+
+	// InvalidArgument means the client specified an invalid argument. Note that this differs
 	// from `FailedPrecondition`. `InvalidArgument` indicates arguments
 	// that are problematic regardless of the state of the system
 	// (e.g., a malformed file name).
@@ -54,16 +57,18 @@ const (
 	// HTTP Mapping: 400 Bad Request
 	// Google API Mapping: Code.INVALID_ARGUMENT
 	InvalidArgument Code = 3
-	// The deadline expired before the operation could complete. For operations
-	// that change the state of the system, this error may be returned
-	// even if the operation has completed successfully. For example, a
-	// successful response from a server could have been delayed long
+
+	// DeadlineExceeded means the deadline expired before the operation could
+	// complete. For operations that change the state of the system, this error
+	// may be returned even if the operation has completed successfully. For example,
+	// a successful response from a server could have been delayed long
 	// enough for the deadline to expire.
 	//
 	// HTTP Mapping: 504 Gateway Timeout
 	// Google API Mapping: Code.DEADLINE_EXCEEDED
 	DeadlineExceeded Code = 4
-	// Some requested entity (e.g., file or directory) was not found.
+
+	// NotFound means some requested entity (e.g., file or directory) was not found.
 	// For privacy reasons, this code *may* be returned when the client
 	// does not have the access rights to the entity, though such usage is
 	// discouraged.
@@ -71,14 +76,16 @@ const (
 	// HTTP Mapping: 404 Not Found
 	// Google API Mapping: Code.NOT_FOUND
 	NotFound Code = 5
-	// The entity that a client attempted to create (e.g., file or directory)
-	// already exists.
+
+	// AlreadyExists means the entity that a client attempted to create
+	// (e.g., file or directory) already exists.
 	//
 	// HTTP Mapping: 409 Conflict
 	// Google API Mapping: Code.ALREADY_EXISTS
 	AlreadyExists Code = 6
-	// The caller does not have permission to execute the specified
-	// operation. `PermissionDenied` must not be used for rejections
+
+	// PermissionDenied means the caller does not have permission to execute
+	// the specified operation. `PermissionDenied` must not be used for rejections
 	// caused by exhausting some resource (use `ResourceExhausted`
 	// instead for those errors). `PermissionDenied` must not be
 	// used if the caller can not be identified (use `Unauthenticated`
@@ -87,14 +94,16 @@ const (
 	// HTTP Mapping: 403 Forbidden
 	// Google API Mapping: Code.PERMISSION_DENIED
 	PermissionDenied Code = 7
-	// Some resource has been exhausted, perhaps a per-user quota, or
-	// perhaps the entire file system is out of space.
+
+	// ResourceExhausted means some resource has been exhausted, perhaps a per-user
+	// quota, or perhaps the entire file system is out of space.
 	//
 	// HTTP Mapping: 429 Too Many Requests
 	// Google API Mapping: Code.RESOURCE_EXHAUSTED
 	ResourceExhausted Code = 8
-	// The operation was rejected because the system is not in a state
-	// required for the operation's execution. For example, the directory
+
+	// FailedPrecondition means the operation was rejected because the system is not
+	// in a state required for the operation's execution. For example, the directory
 	// to be deleted is non-empty, an rmdir operation is applied to
 	// a non-directory, etc.
 	//
@@ -112,8 +121,9 @@ const (
 	// HTTP Mapping: 400 Bad Request
 	// Google API Mapping: Code.FAILED_PRECONDITION
 	FailedPrecondition Code = 9
-	// The operation was aborted, typically due to a concurrency issue such as
-	// a sequencer check failure or transaction abort.
+
+	// Aborted means the operation was aborted, typically due to a concurrency issue
+	// such as a sequencer check failure or transaction abort.
 	//
 	// See the guidelines above for deciding between `FailedPrecondition`,
 	// `Aborted`, and `Unavailable`.
@@ -121,8 +131,9 @@ const (
 	// HTTP Mapping: 409 Conflict
 	// Google API Mapping: Code.ABORTED
 	Aborted Code = 10
-	// The operation was attempted past the valid range. E.g., seeking or
-	// reading past end-of-file.
+
+	// OutOfRange means the operation was attempted past the valid range.
+	// E.g., seeking or reading past end-of-file.
 	//
 	// Unlike `InvalidArgument`, this error indicates a problem that may
 	// be fixed if the system state changes. For example, a 32-bit file
@@ -140,20 +151,23 @@ const (
 	// HTTP Mapping: 400 Bad Request
 	// Google API Mapping: Code.OUT_OF_RANGE
 	OutOfRange Code = 11
-	// The operation is not implemented or is not supported/enabled in this
-	// service.
+
+	// Unimplemented means the operation is not implemented or is not
+	// supported/enabled in this service.
 	//
 	// HTTP Mapping: 501 Not Implemented
 	// Google API Mapping: Code.UNIMPLEMENTED
 	Unimplemented Code = 12
-	// Internal errors. This means that some invariants expected by the
-	// underlying system have been broken. This error code is reserved
+
+	// Internal means an internal error. This means that some invariants expected
+	// by the underlying system have been broken. This error code is reserved
 	// for serious errors.
 	//
 	// HTTP Mapping: 500 Internal Server Error
 	// Google API Mapping: Code.INTERNAL
 	Internal Code = 13
-	// The service is currently unavailable. This is most likely a
+
+	// Unavailable means the service is currently unavailable. This is most likely a
 	// transient condition, which can be corrected by retrying with
 	// a backoff.
 	//
@@ -163,19 +177,22 @@ const (
 	// HTTP Mapping: 503 Service Unavailable
 	// Google API Mapping: Code.UNAVAILABLE
 	Unavailable Code = 14
-	// Unrecoverable data loss or corruption.
+
+	// DataLoss means unrecoverable data loss or corruption.
 	//
 	// HTTP Mapping: 500 Internal Server Error
 	// Google API Mapping: Code.DATA_LOSS
 	DataLoss Code = 15
-	// The request does not have valid authentication credentials for the
-	// operation.
+
+	// Unauthenticated means the request does not have valid authentication
+	// credentials for the operation.
 	//
 	// HTTP Mapping: 401 Unauthorized
 	// Google API Mapping: Code.UNAUTHENTICATED
 	Unauthenticated Code = 16
-	// Application error. This will typically be accompianed by a user-defined
-	// error name across the wire.
+
+	// Application means there was an application error. This will typically be
+	// accompianed by a user-defined error name across the wire.
 	//
 	// HTTP Mapping: 500 Internal Server Error
 	// Google API Mapping: Code.UNKNOWN
