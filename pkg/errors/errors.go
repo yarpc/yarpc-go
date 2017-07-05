ae0a75884ee90c8d471f4c2352bf70822b95542f
Peter Edge <pedge@uber.com>
2017-07-05 15:53:44 -0400

    Godoc update

--- pkg/errors/errors.go
+++ pkg/errors/errors.go
@@ -24,12 +24,14 @@ package errors
 
 import "go.uber.org/yarpc/yarpcerrors"
 
-// WrapHandlerError is a convenience function with the following logic:
+// WrapHandlerError is a convenience function to help wrap errors returned
+// from a handler.
 //
-// - If err is nil, WrapHandlerError returns nil.
-// - If err is a YARPC error, WrapHandlerError returns err with no changes.
-// - If err is not a YARPC error, WrapHandlerError returns a new YARPC error with code.
-//   CodeUnknown and message err.Error(), along with service and procedure information.
+// If err is nil, WrapHandlerError returns nil.
+// If err is a YARPC error, WrapHandlerError returns err with no changes.
+// If err is not a YARPC error, WrapHandlerError returns a new YARPC error
+// with code yarpcerrors.CodeUnknown and message err.Error(), along with
+// service and procedure information.
 func WrapHandlerError(err error, service string, procedure string) error {
 	if err == nil {
 		return nil
