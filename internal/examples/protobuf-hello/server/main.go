70f126e84e6a8c45b640730ad71fd2ed1707c6b2
Grayson Koonce <grayson@uber.com>
2017-07-28 06:37:51 -0700

    Basic proto server

--- internal/examples/protobuf-hello/server/main.go
+++ internal/examples/protobuf-hello/server/main.go
@@ -1,7 +1,54 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"strings"
+	"syscall"
+
+	hello "go.uber.org/yarpc/internal/examples/protobuf-hello"
+	"go.uber.org/yarpc/transport/http"
+	"go.uber.org/yarpc/x/config"
+)
 
 func main() {
-	fmt.Println("Hello from server!")
+	// build a configurator with the HTTP transport registered
+	configurator := config.New()
+	configurator.MustRegisterTransport(http.TransportSpec())
+
+	// create a dispatcher for the "server" service
+	// TODO switch to yaml
+	dispatcher, err := configurator.NewDispatcherFromYAML("server", strings.NewReader(`{
+		"inbounds": {
+			"http": {
+				"address": ":8080",
+			},
+		},
+	}`))
+	if err != nil {
+		log.Panicf("Dispatcher could not be created: %v", err)
+	}
+
+	// register handler
+	procedures := hello.BuildHelloWorldYARPCProcedures(handler{})
+	dispatcher.Register(procedures)
+
+	// start service
+	dispatcher.Start()
+	defer dispatcher.Stop()
+
+	// block until SIGINT/SIGTERM
+	signals := make(chan os.Signal, 1)
+	signal.Notify(signals, syscall.SIGINT, syscall.SIGTERM)
+	<-signals
+}
+
+type handler struct{}
+
+func (handler) Hello(ctx context.Context, req *hello.HelloRequest) (*hello.HelloResponse, error) {
+	message := fmt.Sprintf("Hello %s!", req.Name)
+	return &hello.HelloResponse{Message: message}, nil
 }
