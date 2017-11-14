baddf923b6bf0cac50ae21445ee3af32ec9f9735
Will Hughes <wh@uber.com>
2017-11-14 13:46:21 -0800

    Stream 9: Add end-to-end streaming example.
    Summary: This PR adds an example of how to use grpc streaming, it's not
    entirely necessary but was very useful for quickly testing the
    grpc/proto stream apis before a lot of the test infra was finished.

---
+++ internal/examples/streaming/client/main.go
@@ -0,0 +1,88 @@
+// Copyright (c) 2017 Uber Technologies, Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+package main
+
+import (
+	"bufio"
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"go.uber.org/yarpc"
+	"go.uber.org/yarpc/internal/examples/streaming"
+	"go.uber.org/yarpc/transport/grpc"
+)
+
+func main() {
+	if err := do(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func do() error {
+	dispatcher := yarpc.NewDispatcher(yarpc.Config{
+		Name: "keyvalue-client",
+		Outbounds: yarpc.Outbounds{
+			"keyvalue": {
+				Stream: grpc.NewTransport().NewSingleOutbound("127.0.0.1:24038"),
+			},
+		},
+	})
+
+	client := streaming.NewHelloYARPCClient(dispatcher.MustOutboundConfig("keyvalue"))
+
+	if err := dispatcher.Start(); err != nil {
+		return fmt.Errorf("failed to start Dispatcher: %v", err)
+	}
+	defer dispatcher.Stop()
+
+	stream, err := client.HelloThere(context.Background(), yarpc.WithHeader("test", "testtest"))
+	if err != nil {
+		return fmt.Errorf("failed to create stream: %s", err.Error())
+	}
+
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Println("Connected and waiting for input")
+	fmt.Printf(">>> ")
+	defer func(){ fmt.Println("ending:", stream.ResponseMeta()) } ()
+	for scanner.Scan() {
+		line := scanner.Text()
+		if line == "stop" {
+			return stream.CloseSend()
+		}
+
+		fmt.Printf("sending message: %q\n", line)
+		if err := stream.Send(&streaming.HelloRequest{line}); err != nil {
+			return err
+		}
+
+		fmt.Println("waiting for response...")
+		msg, err := stream.Recv()
+		if err != nil {
+			return err
+		}
+
+		fmt.Printf("got response: %q\n", msg.Id)
+		fmt.Printf(">>> ")
+	}
+	return scanner.Err()
+}
---
+++ internal/examples/streaming/server/main.go
@@ -0,0 +1,109 @@
+// Copyright (c) 2017 Uber Technologies, Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+package main
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"log"
+	"net"
+
+	"go.uber.org/yarpc"
+	"go.uber.org/yarpc/api/transport"
+	"go.uber.org/yarpc/internal/examples/streaming"
+	"go.uber.org/yarpc/transport/grpc"
+)
+
+type handler struct {
+}
+
+func (h *handler) HelloUnary(context.Context, *streaming.HelloRequest) (*streaming.HelloResponse, error) {
+	return nil, errors.New("NOT READY")
+}
+
+func (h *handler) HelloOutStream(streaming.HelloServiceHelloOutStreamYARPCServer) (*streaming.HelloResponse, error) {
+	return nil, nil
+}
+
+func (h *handler) HelloInStream(*streaming.HelloRequest, streaming.HelloServiceHelloInStreamYARPCServer) error {
+	return nil
+}
+
+func (h *handler) HelloThere(stream streaming.HelloServiceHelloThereYARPCServer) error {
+	fmt.Println("Handling stream")
+	fmt.Println("request: ", stream.RequestMeta())
+	defer func() {
+		stream.SetResponseMeta(&transport.ResponseMeta{Headers: transport.HeadersFromMap(map[string]string{"testresp": "resssssssp"})})
+	}()
+	for {
+		msg, err := stream.Recv()
+		if err != nil {
+			fmt.Printf("Error reading from stream: %q\n", err.Error())
+			return err
+		}
+
+		if msg.Id == "exit" {
+			fmt.Println("Received 'exit' message, closing connection")
+			return nil
+		}
+		fmt.Printf("Received a message: %q\n", msg.Id)
+
+		resp := fmt.Sprintf("Got your message: %q, thanks!", msg.Id)
+		if err := stream.Send(&streaming.HelloResponse{resp}); err != nil {
+			fmt.Printf("Error sending message to stream: %q\n", err.Error())
+			return err
+		}
+		fmt.Printf("Sent response message: %q\n", resp)
+	}
+}
+
+func main() {
+	if err := do(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func do() error {
+	var inbound transport.Inbound
+	listener, err := net.Listen("tcp", "127.0.0.1:24038")
+	if err != nil {
+		return err
+	}
+	inbound = grpc.NewTransport().NewInbound(listener)
+
+	dispatcher := yarpc.NewDispatcher(yarpc.Config{
+		Name:     "keyvalue",
+		Inbounds: yarpc.Inbounds{inbound},
+	})
+
+	handler := &handler{}
+
+	dispatcher.Register(streaming.BuildHelloYARPCProcedures(handler))
+
+	fmt.Println("Starting Dispatcher")
+	if err := dispatcher.Start(); err != nil {
+		return err
+	}
+	fmt.Println("Started Dispatcher")
+
+	select {}
+}
