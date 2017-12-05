b0ceeeebd978cf5564d7e84deca9ea8ede1e1374
Will Hughes <wh@uber.com>
2017-12-05 15:51:28 -0800

    Revert "Add middleware to check if dispatcher is running when making calls and handling requests (#1375)"
    This reverts commit f9c4e61e97f9eaf9ad1dedb4873416eb2831640d.

--- transport/http/peer_test.go
+++ transport/http/peer_test.go
@@ -90,25 +90,16 @@ func TestHTTPWithRoundRobin(t *testing.T) {
 	temporary.Stop()
 	// One of these requests may fail since one of the peers has gone down but
 	// the HTTP transport will not know until a request is attempted.
-	err1 := integrationtest.Call(ctx, c)
-	err2 := integrationtest.Call(ctx, c)
-	if err1 == nil && err2 == nil {
-		t.Fatal("both calls succeeded after bringing one peer down")
-	}
-	if err1 != nil && err2 != nil {
-		t.Fatal("neither call succeeded after bringing one peer down")
-	}
+	integrationtest.Call(ctx, c)
+	integrationtest.Call(ctx, c)
 	// All subsequent should succeed since the peer should be removed on
 	// connection fail.
 	integrationtest.Blast(ctx, t, c)
 
 	// Restore the server on the temporary port.
-	// TODO the http client unfortunately reuses idle connections from the
-	// prior generation of the http server on this port, rendering this test
-	// useless.
-	// restored, _ := spec.NewServer(t, temporaryAddr)
-	// defer restored.Stop()
-	// integrationtest.Blast(ctx, t, c)
+	restored, _ := spec.NewServer(t, temporaryAddr)
+	defer restored.Stop()
+	integrationtest.Blast(ctx, t, c)
 }
 
 func TestIntegration(t *testing.T) {
