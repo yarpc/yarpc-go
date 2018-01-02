af5bd6711736c2892b291e7f47149913a72c7300
Peter Edge <pedge@uber.com>
2018-01-02 06:24:12 -0500

    Run make generate to update license headers to 2018

--- internal/examples/streaming/stream.pb.yarpc.go
+++ internal/examples/streaming/stream.pb.yarpc.go
@@ -2,7 +2,7 @@
 // source: internal/examples/streaming/stream.proto
 // DO NOT EDIT!
 
-// Copyright (c) 2017 Uber Technologies, Inc.
+// Copyright (c) 2018 Uber Technologies, Inc.
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
