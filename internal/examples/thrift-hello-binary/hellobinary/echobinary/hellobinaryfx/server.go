a1579bc9c7459317374b80975750185042caddeb
Will Hughes <wh@uber.com>
2018-01-04 18:48:05 -0800

    Regenerate everything

--- internal/examples/thrift-hello-binary/hellobinary/echobinary/hellobinaryfx/server.go
+++ internal/examples/thrift-hello-binary/hellobinary/echobinary/hellobinaryfx/server.go
@@ -25,40 +25,41 @@ package hellobinaryfx
 
 import (
 	"go.uber.org/fx"
-	"go.uber.org/yarpc"
+	"go.uber.org/yarpc/api/transport"
 	"go.uber.org/yarpc/encoding/thrift"
-	"go.uber.org/yarpc/internal/examples/thrift-hello-binary/hellobinary/echobinary/hellobinaryclient"
+	"go.uber.org/yarpc/internal/examples/thrift-hello-binary/hellobinary/echobinary/hellobinaryserver"
 )
 
-// Params defines the dependencies for the HelloBinary client.
-type Params struct {
+// ServerParams defines the dependencies for the HelloBinary server.
+type ServerParams struct {
 	fx.In
 
-	Provider yarpc.ClientConfig
+	Handler hellobinaryserver.Interface
 }
 
-// Result defines the output of the HelloBinary client module. It provides a
-// HelloBinary client to an Fx application.
-type Result struct {
+// ServerResult defines the output of HelloBinary server module. It provides the
+// procedures of a HelloBinary handler to an Fx application.
+//
+// The procedures are provided to the "yarpcfx" value group. Dig 1.2 or newer
+// must be used for this feature to work.
+type ServerResult struct {
 	fx.Out
 
-	Client hellobinaryclient.Interface
-
-	// We are using an fx.Out struct here instead of just returning a client
-	// so that we can add more values or add named versions of the client in
-	// the future without breaking any existing code.
+	Procedures []transport.Procedure `group:"yarpcfx"`
 }
 
-// Client provides a HelloBinary client to an Fx application using the given name
-// for routing.
+// Server provides procedures for HelloBinary to an Fx application. It expects a
+// hellobinaryfx.Interface to be present in the container.
 //
 // 	fx.Provide(
-// 		hellobinaryfx.Client("..."),
-// 		newHandler,
+// 		func(h *MyHelloBinaryHandler) hellobinaryserver.Interface {
+// 			return h
+// 		},
+// 		hellobinaryfx.Server(),
 // 	)
-func Client(name string, opts ...thrift.ClientOption) interface{} {
-	return func(p Params) Result {
-		client := hellobinaryclient.New(p.Provider.ClientConfig(name), opts...)
-		return Result{Client: client}
+func Server(opts ...thrift.RegisterOption) interface{} {
+	return func(p ServerParams) ServerResult {
+		procedures := hellobinaryserver.New(p.Handler, opts...)
+		return ServerResult{Procedures: procedures}
 	}
 }
