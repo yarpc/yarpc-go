b899afd61e9007ac0a288230e2057051fffa082b
Kevin Grandon <kevingrandon@yahoo.com>
2018-04-06 08:14:53 +0900

    Initial Buildkite implementation (#1378)
    This adds a BuildKite pipeline which builds two Docker images and runs
    multiple tasks in parallel.
    
    A couple things to note:
    
    - Unit tests are sharded across multiple machines.
    - AWS instances are distributed via an AWS Elastic Container Registry
      that we own.
    
    The pipeline looks like this:
    ![Pipeline Screenshot](https://user-images.githubusercontent.com/122602/35199206-e99c1f40-feaf-11e7-9a55-aedb365f29aa.png)
    
    Pros:
    
    - Great parallelism and pipeline controls.
    - Lots of knobs to turn to improve performance, and get down to ~1m
      builds.
    
    Cons:
    
    - Currently right now you need a buildkite account to see build pages.
      This is something that Buildkite is working on releasing, and if I had
      to guess would be ready in the next few months.
    
    Steps after landing:
    
    - Enable the BuildKite webhook, and update the BuildKite project to be
      this repository.
    - Run BuildKite and travis in parallel until Buildkite is in good
      enough shape to replace Travis.
    - Update Codecov token for pipeline.

---
+++ internal/shard/main.go
@@ -0,0 +1,62 @@
+// Copyright (c) 2018 Uber Technologies, Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"strconv"
+)
+
+// Shards a list of files over a distribution defined by totalShards.
+// Useful for splitting work and running tests in parallel.
+var usage = fmt.Sprintf("Usage: %s shardNum totalShards args...", os.Args[0])
+
+func main() {
+	if err := do(os.Args[1:], os.Stdout); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func do(args []string, writer io.Writer) error {
+	if len(args) < 2 {
+		return fmt.Errorf(usage)
+	}
+	shardNum, err := strconv.Atoi(args[0])
+	if err != nil {
+		return fmt.Errorf("%v\n%s", err, usage)
+	}
+	totalShards, err := strconv.Atoi(args[1])
+	if err != nil {
+		return fmt.Errorf("%v\n%s", err, usage)
+	}
+	for i := 2; i < len(args); i++ {
+		if (((i - 2) % totalShards) + 1) == shardNum {
+			if i != 2 {
+				fmt.Fprint(writer, " ")
+			}
+			fmt.Fprint(writer, args[i])
+		}
+	}
+	return nil
+}
---
+++ internal/shard/main_test.go
@@ -0,0 +1,85 @@
+// Copyright (c) 2018 Uber Technologies, Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+package main
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestDo(t *testing.T) {
+	tests := []struct {
+		Args       []string
+		WantOutput string
+		WantError  bool
+	}{
+		{
+			WantError: true,
+		},
+		{
+			Args:      []string{},
+			WantError: true,
+		},
+		{
+			Args:      []string{"1"},
+			WantError: true,
+		},
+		{
+			Args:      []string{"1", "x"},
+			WantError: true,
+		},
+		{
+			Args:      []string{"x", "1"},
+			WantError: true,
+		},
+		{
+			Args: []string{"1", "3"},
+		},
+		{
+			Args:       []string{"1", "3", "a"},
+			WantOutput: "a",
+		},
+		{
+			Args:       []string{"1", "3", "a", "b"},
+			WantOutput: "a",
+		},
+		{
+			Args:       []string{"1", "3", "a", "b", "c"},
+			WantOutput: "a",
+		},
+		{
+			Args:       []string{"1", "3", "a", "b", "c", "d"},
+			WantOutput: "a d",
+		},
+	}
+	for _, tt := range tests {
+		buffer := bytes.NewBuffer(nil)
+		err := do(tt.Args, buffer)
+		if tt.WantError {
+			assert.Error(t, err)
+		} else {
+			assert.NoError(t, err)
+		}
+		assert.Equal(t, tt.WantOutput, buffer.String())
+	}
+}
