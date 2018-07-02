17c9ad0932d439bd0651d70dd5283da7bb4c8ab2
Zachary M. Train <no1@zmt.org>
2018-07-02 13:35:54 -0700

    updating gogo/protobuf from v0.5 to v1.0.0 (#1517)
    * glide up for gogo/protobuf v1.0.0
    * make generate w/ new gogo/protobuf
    * update changelog

--- internal/examples/streaming/stream.pb.go
+++ internal/examples/streaming/stream.pb.go
@@ -40,10 +40,8 @@ import math "math"
 import strings "strings"
 import reflect "reflect"
 
-import (
-	context "golang.org/x/net/context"
-	grpc "google.golang.org/grpc"
-)
+import context "golang.org/x/net/context"
+import grpc "google.golang.org/grpc"
 
 import io "io"
 
@@ -94,10 +92,7 @@ func init() {
 }
 func (this *HelloRequest) Equal(that interface{}) bool {
 	if that == nil {
-		if this == nil {
-			return true
-		}
-		return false
+		return this == nil
 	}
 
 	that1, ok := that.(*HelloRequest)
@@ -110,10 +105,7 @@ func (this *HelloRequest) Equal(that interface{}) bool {
 		}
 	}
 	if that1 == nil {
-		if this == nil {
-			return true
-		}
-		return false
+		return this == nil
 	} else if this == nil {
 		return false
 	}
@@ -124,10 +116,7 @@ func (this *HelloRequest) Equal(that interface{}) bool {
 }
 func (this *HelloResponse) Equal(that interface{}) bool {
 	if that == nil {
-		if this == nil {
-			return true
-		}
-		return false
+		return this == nil
 	}
 
 	that1, ok := that.(*HelloResponse)
@@ -140,10 +129,7 @@ func (this *HelloResponse) Equal(that interface{}) bool {
 		}
 	}
 	if that1 == nil {
-		if this == nil {
-			return true
-		}
-		return false
+		return this == nil
 	} else if this == nil {
 		return false
 	}
