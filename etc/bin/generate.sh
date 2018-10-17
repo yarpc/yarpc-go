8498c42492a734e34c2d6de7026eedc640de0515
Vyom Shah <vyom@uber.com>
2018-10-17 13:12:47 -0700

    Add StreamEncodingHandler interface

--- etc/bin/generate.sh
+++ etc/bin/generate.sh
@@ -81,7 +81,7 @@ mockgen -destination=api/transport/transporttest/transport.go -package=transport
 
 # v2 mocks
 mockgen -destination=v2/yarpctest/mock_middleware.go -package=yarpctest go.uber.org/yarpc/v2 RouterMiddleware,UnaryInboundTransportMiddleware,UnaryOutboundTransportMiddleware,StreamInboundTransportMiddleware,StreamOutboundTransportMiddleware
-mockgen -destination=v2/yarpctest/mock_handler.go -package=yarpctest go.uber.org/yarpc/v2 UnaryTransportHandler,StreamTransportHandler,UnaryEncodingHandler
+mockgen -destination=v2/yarpctest/mock_handler.go -package=yarpctest go.uber.org/yarpc/v2 UnaryTransportHandler,StreamTransportHandler,UnaryEncodingHandler,StreamEncodingHandler
 mockgen -destination=v2/yarpctest/mock_outbound.go -package=yarpctest go.uber.org/yarpc/v2 UnaryOutbound,StreamOutbound
 mockgen -destination=v2/yarpctest/mock_stream.go -package=yarpctest go.uber.org/yarpc/v2 Stream,StreamCloser
 mockgen -destination=v2/yarpctest/mock_router.go -package=yarpctest go.uber.org/yarpc/v2 Router,RouteTable,InboundCodec
