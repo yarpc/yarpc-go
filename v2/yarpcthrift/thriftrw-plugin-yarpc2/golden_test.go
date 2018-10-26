6b318f4fc6f5b04b482f65e4ac59894852aca623
Minho Park <minho.park@uber.com>
2018-10-26 11:57:16 -0700

    [spring/thrift] plugin: Move thriftrw yarpc plugin to spring (#1604)

---
+++ v2/yarpcthrift/thriftrw-plugin-yarpc2/golden_test.go
@@ -0,0 +1,262 @@
+// Copyright (c) 2018 Uber Technologies, Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+package main
+
+import (
+	"crypto/sha1"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strconv"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"go.uber.org/atomic"
+	"go.uber.org/thriftrw/plugin"
+)
+
+// This implements a test that verifies that the code in internal/tests/ is up to
+// date.
+
+const _testPackage = "go.uber.org/yarpc/v2/yarpcthrift/thriftrw-plugin-yarpc2/internal/tests"
+
+// Thrift files for which we set --sanitize-tchannel to true.
+var tchannelSanitizeFor = map[string]struct{}{
+	"weather.thrift": {},
+}
+
+type fakePluginServer struct {
+	ln      net.Listener
+	running atomic.Bool
+
+	// Whether the next request should use --sanitize-tchannel.
+	sanitizeTChannelNext atomic.Bool
+}
+
+func newFakePluginServer(t *testing.T) *fakePluginServer {
+	ln, err := net.Listen("tcp", "127.0.0.1:0")
+	require.NoError(t, err, "failed to set up TCP server")
+
+	server := &fakePluginServer{ln: ln}
+	go server.serve(t)
+	return server
+}
+
+func (s *fakePluginServer) Addr() string {
+	return s.ln.Addr().String()
+}
+
+func (s *fakePluginServer) Stop(t *testing.T) {
+	s.running.Store(false)
+	if err := s.ln.Close(); err != nil {
+		t.Logf("failed to stop fake plugin server: %v", err)
+	}
+}
+
+func (s *fakePluginServer) SanitizeTChannel() {
+	s.sanitizeTChannelNext.Store(true)
+}
+
+func (s *fakePluginServer) serve(t *testing.T) {
+	s.running.Store(true)
+	for s.running.Load() {
+		conn, err := s.ln.Accept()
+		if err != nil {
+			if s.running.Load() {
+				t.Logf("failed to open incoming connection: %v", err)
+			}
+			break
+		}
+		s.handle(conn)
+	}
+}
+
+func (s *fakePluginServer) handle(conn net.Conn) {
+	defer conn.Close()
+
+	// The plugin expects to close both, the reader and the writer. net.Conn
+	// doesn't like Close being called multiple times so we're going to no-op
+	// one of the closes.
+	//
+	// Additionally, the plugin server writes a response for the Goodbye
+	// request on exit. As in,
+	//
+	//  plugin.Stop():
+	//    reader.Close()
+	//    writer.Write(bye)
+	//    writer.Close()
+	//
+	// We need the writer to be writeable after the reader.Close. So we'll
+	// no-op the reader.Close rather than writer.Close.
+	plugin.Main(&plugin.Plugin{
+		Name: "yarpc2",
+		ServiceGenerator: g{
+			SanitizeTChannel: s.sanitizeTChannelNext.Swap(false),
+		},
+		Reader: ioutil.NopCloser(conn),
+		Writer: conn,
+	})
+}
+
+func TestCodeIsUpToDate(t *testing.T) {
+	// ThriftRW expects to call the thriftrw-plugin-yarpc binary. We trick it
+	// into calling back into this test by setting up a fake
+	// thriftrw-plugin-yarpc exectuable which uses netcat to connect back to
+	// the TCP server controlled by this test. We serve the YARPC plugin on
+	// that TCP connection.
+	//
+	// This lets us get more accurate coverage metrics for the plugin.
+	fakePlugin := newFakePluginServer(t)
+	defer fakePlugin.Stop(t)
+	{
+		tempDir, err := ioutil.TempDir("", "current-thriftrw-plugin-yarpc2")
+		require.NoError(t, err, "failed to create temporary directory: %v", err)
+		defer os.RemoveAll(tempDir)
+
+		oldPath := os.Getenv("PATH")
+		newPath := fmt.Sprintf("%v:%v", tempDir, oldPath)
+		require.NoError(t, os.Setenv("PATH", newPath),
+			"failed to add %q to PATH: %v", tempDir, err)
+		defer os.Setenv("PATH", oldPath)
+
+		fakePluginPath := filepath.Join(tempDir, "thriftrw-plugin-yarpc2")
+		require.NoError(t,
+			ioutil.WriteFile(fakePluginPath, callback(fakePlugin.Addr()), 0777),
+			"failed to create thriftrw plugin script")
+	}
+
+	thriftRoot, err := filepath.Abs("internal/tests")
+	require.NoError(t, err, "could not resolve absolute path to internal/tests")
+
+	thriftFiles, err := filepath.Glob(thriftRoot + "/*.thrift")
+	require.NoError(t, err)
+
+	outputDir, err := ioutil.TempDir("", "golden-test")
+	require.NoError(t, err, "failed to create temporary directory")
+	defer os.RemoveAll(outputDir)
+
+	for _, thriftFile := range thriftFiles {
+		packageName := strings.TrimSuffix(filepath.Base(thriftFile), ".thrift")
+		currentPackageDir := filepath.Join("internal/tests", packageName)
+		newPackageDir := filepath.Join(outputDir, packageName)
+
+		currentHash, err := dirhash(currentPackageDir)
+		require.NoError(t, err, "could not hash %q", currentPackageDir)
+
+		_, fileName := filepath.Split(thriftFile)
+
+		// Tell the plugin whether it should --sanitize-tchannel.
+		if _, ok := tchannelSanitizeFor[fileName]; ok {
+			fakePlugin.SanitizeTChannel()
+		}
+
+		err = thriftrw(
+			"--no-recurse",
+			"--out", outputDir,
+			"--pkg-prefix", _testPackage,
+			"--thrift-root", thriftRoot,
+			"--plugin", "yarpc2",
+			thriftFile,
+		)
+		require.NoError(t, err, "failed to generate code for %q", thriftFile)
+
+		newHash, err := dirhash(newPackageDir)
+		require.NoError(t, err, "could not hash %q", newPackageDir)
+
+		assert.Equal(t, currentHash, newHash,
+			"Generated code for %q is out of date.", thriftFile)
+	}
+}
+
+// callback generates the contents of a script which connects back to the
+// given TCP server.
+func callback(addr string) []byte {
+	i := strings.LastIndexByte(addr, ':')
+	host := addr[:i]
+	port, err := strconv.ParseInt(addr[i+1:], 10, 32)
+	if err != nil {
+		panic(err)
+	}
+
+	return []byte(fmt.Sprintf(`#!/bin/bash -e
+
+nc %v %v
+`, host, port))
+}
+
+func thriftrw(args ...string) error {
+	cmd := exec.Command("thriftrw", args...)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	return cmd.Run()
+}
+
+func dirhash(dir string) (map[string]string, error) {
+	fileHashes := make(map[string]string)
+	err := filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		if info.IsDir() {
+			return nil
+		}
+
+		fileHash, err := hash(path)
+		if err != nil {
+			return fmt.Errorf("failed to hash %q: %v", path, err)
+		}
+
+		// We only care about the path relative to the directory being
+		// hashed.
+		path, err = filepath.Rel(dir, path)
+		if err != nil {
+			return err
+		}
+		if !strings.HasSuffix(path, ".nocover") {
+			fileHashes[path] = fileHash
+		}
+		return nil
+	})
+
+	return fileHashes, err
+}
+
+func hash(name string) (string, error) {
+	f, err := os.Open(name)
+	if err != nil {
+		return "", err
+	}
+	defer f.Close()
+
+	h := sha1.New()
+	if _, err := io.Copy(h, f); err != nil {
+		return "", err
+	}
+
+	return fmt.Sprintf("%x", h.Sum(nil)), nil
+}
---
+++ v2/yarpcthrift/thriftrw-plugin-yarpc2/roundtrip_test.go
@@ -0,0 +1,363 @@
+// Copyright (c) 2018 Uber Technologies, Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+package main_test
+
+import (
+	"context"
+	"fmt"
+	"net"
+	"net/url"
+	"reflect"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"go.uber.org/thriftrw/ptr"
+	"go.uber.org/yarpc/v2"
+	"go.uber.org/yarpc/v2/internal/internaltesttime"
+	"go.uber.org/yarpc/v2/yarpchttp"
+	"go.uber.org/yarpc/v2/yarpcrouter"
+	"go.uber.org/yarpc/v2/yarpcthrift"
+	"go.uber.org/yarpc/v2/yarpcthrift/thriftrw-plugin-yarpc2/internal/tests/atomic"
+	"go.uber.org/yarpc/v2/yarpcthrift/thriftrw-plugin-yarpc2/internal/tests/atomic/readonlystoreclient"
+	"go.uber.org/yarpc/v2/yarpcthrift/thriftrw-plugin-yarpc2/internal/tests/atomic/readonlystoreserver"
+	"go.uber.org/yarpc/v2/yarpcthrift/thriftrw-plugin-yarpc2/internal/tests/atomic/storeclient"
+	"go.uber.org/yarpc/v2/yarpcthrift/thriftrw-plugin-yarpc2/internal/tests/atomic/storeserver"
+	"go.uber.org/yarpc/v2/yarpcthrift/thriftrw-plugin-yarpc2/internal/tests/common/baseserviceclient"
+	"go.uber.org/yarpc/v2/yarpcthrift/thriftrw-plugin-yarpc2/internal/tests/common/baseserviceserver"
+	"go.uber.org/yarpc/v2/yarpcthrift/thriftrw-plugin-yarpc2/internal/tests/common/emptyserviceclient"
+	"go.uber.org/yarpc/v2/yarpcthrift/thriftrw-plugin-yarpc2/internal/tests/common/emptyserviceserver"
+	"go.uber.org/yarpc/v2/yarpcthrift/thriftrw-plugin-yarpc2/internal/tests/common/extendemptyclient"
+	"go.uber.org/yarpc/v2/yarpcthrift/thriftrw-plugin-yarpc2/internal/tests/common/extendemptyserver"
+	"go.uber.org/yarpc/v2/yarpcthrift/thriftrw-plugin-yarpc2/internal/tests/common/extendonlyclient"
+	"go.uber.org/yarpc/v2/yarpcthrift/thriftrw-plugin-yarpc2/internal/tests/common/extendonlyserver"
+)
+
+func TestRoundTrip(t *testing.T) {
+	tests := []struct{ enveloped, multiplexed bool }{
+		{true, true},
+		{true, false},
+		{false, true},
+		{false, false},
+	}
+
+	for _, tt := range tests {
+		name := fmt.Sprintf("enveloped(%v)/multiplexed(%v)", tt.enveloped, tt.multiplexed)
+		t.Run(name, func(t *testing.T) { testRoundTrip(t, tt.enveloped, tt.multiplexed) })
+	}
+}
+
+func testRoundTrip(t *testing.T, enveloped, multiplexed bool) {
+	var serverOpts []yarpcthrift.RegisterOption
+	if enveloped {
+		serverOpts = append(serverOpts, yarpcthrift.Enveloped)
+	}
+
+	var clientOpts []yarpcthrift.ClientOption
+	if enveloped {
+		clientOpts = append(clientOpts, yarpcthrift.Enveloped)
+	}
+	if multiplexed {
+		clientOpts = append(clientOpts, yarpcthrift.Multiplexed)
+	}
+
+	tests := []struct {
+		desc          string
+		procedures    []yarpc.TransportProcedure
+		newClientFunc interface{}
+
+		// if method is non-empty, client.method(ctx, methodArgs...) will be called
+		method     string
+		methodArgs []interface{}
+
+		wantResult interface{}
+		wantError  error
+	}{
+		{
+			desc:          "empty service",
+			procedures:    emptyserviceserver.New(struct{}{}, serverOpts...),
+			newClientFunc: emptyserviceclient.New,
+		},
+		{
+			desc:          "extend empty: hello",
+			procedures:    extendemptyserver.New(extendEmptyHandler{}, serverOpts...),
+			newClientFunc: extendemptyclient.New,
+			method:        "Hello",
+		},
+		{
+			desc:          "base: healthy",
+			procedures:    baseserviceserver.New(extendEmptyHandler{}, serverOpts...),
+			newClientFunc: baseserviceclient.New,
+			method:        "Healthy",
+			wantResult:    true,
+		},
+		{
+			desc:          "extend only: healthy",
+			procedures:    extendonlyserver.New(&storeHandler{healthy: true}, serverOpts...),
+			newClientFunc: extendonlyclient.New,
+			method:        "Healthy",
+			wantResult:    true,
+		},
+		{
+			desc:          "store: healthy",
+			procedures:    storeserver.New(&storeHandler{healthy: true}, serverOpts...),
+			newClientFunc: storeclient.New,
+			method:        "Healthy",
+			wantResult:    true,
+		},
+		{
+			desc:          "store: healthy with base client",
+			procedures:    storeserver.New(&storeHandler{healthy: true}, serverOpts...),
+			newClientFunc: baseserviceclient.New,
+			method:        "Healthy",
+			wantResult:    true,
+		},
+		{
+			desc:          "store: unhealthy",
+			procedures:    storeserver.New(&storeHandler{}, serverOpts...),
+			newClientFunc: storeclient.New,
+			method:        "Healthy",
+			wantResult:    false,
+		},
+		{
+			desc:          "store: increment",
+			procedures:    storeserver.New(&storeHandler{}, serverOpts...),
+			newClientFunc: storeclient.New,
+			method:        "Increment",
+			methodArgs:    []interface{}{ptr.String("foo"), ptr.Int64(42)},
+		},
+		{
+			desc:          "store: compare and swap",
+			procedures:    storeserver.New(&storeHandler{}, serverOpts...),
+			newClientFunc: storeclient.New,
+			method:        "CompareAndSwap",
+			methodArgs: []interface{}{
+				&atomic.CompareAndSwap{
+					Key:          "foo",
+					CurrentValue: 42,
+					NewValue:     420,
+				},
+			},
+		},
+		{
+			desc: "store: compare and swap failure",
+			procedures: storeserver.New(&storeHandler{
+				failWith: &atomic.IntegerMismatchError{
+					ExpectedValue: 42,
+					GotValue:      43,
+				},
+			}, serverOpts...),
+			newClientFunc: storeclient.New,
+			method:        "CompareAndSwap",
+			methodArgs: []interface{}{
+				&atomic.CompareAndSwap{
+					Key:          "foo",
+					CurrentValue: 42,
+					NewValue:     420,
+				},
+			},
+			wantError: &atomic.IntegerMismatchError{
+				ExpectedValue: 42,
+				GotValue:      43,
+			},
+		},
+		{
+			desc:          "store: integer with readonly client",
+			procedures:    storeserver.New(&storeHandler{integer: 42}, serverOpts...),
+			newClientFunc: readonlystoreclient.New,
+			method:        "Integer",
+			methodArgs:    []interface{}{ptr.String("foo")},
+			wantResult:    int64(42),
+		},
+		{
+			desc: "readonly store: integer error with rw client",
+			procedures: readonlystoreserver.New(&storeHandler{
+				failWith: &atomic.KeyDoesNotExist{Key: ptr.String("foo")},
+			}, serverOpts...),
+			newClientFunc: storeclient.New,
+			method:        "Integer",
+			methodArgs:    []interface{}{ptr.String("foo")},
+			wantError:     &atomic.KeyDoesNotExist{Key: ptr.String("foo")},
+		},
+		{
+			desc:          "readonly store: integer with readonly client",
+			procedures:    readonlystoreserver.New(&storeHandler{integer: 42}, serverOpts...),
+			newClientFunc: readonlystoreclient.New,
+			method:        "Integer",
+			methodArgs:    []interface{}{ptr.String("foo")},
+			wantResult:    int64(42),
+		},
+		{
+			desc:          "readonly store: integer with rw client",
+			procedures:    readonlystoreserver.New(&storeHandler{integer: 42}, serverOpts...),
+			newClientFunc: storeclient.New,
+			method:        "Integer",
+			methodArgs:    []interface{}{ptr.String("foo")},
+			wantResult:    int64(42),
+		},
+		{
+			desc: "readonly store: integer failure with rw client",
+			procedures: readonlystoreserver.New(&storeHandler{
+				failWith: &atomic.KeyDoesNotExist{Key: ptr.String("foo")},
+			}, serverOpts...),
+			newClientFunc: storeclient.New,
+			method:        "Integer",
+			methodArgs:    []interface{}{ptr.String("foo")},
+			wantError:     &atomic.KeyDoesNotExist{Key: ptr.String("foo")},
+		},
+		{
+			desc: "store: integer failure",
+			procedures: storeserver.New(&storeHandler{
+				failWith: &atomic.KeyDoesNotExist{Key: ptr.String("foo")},
+			}, serverOpts...),
+			newClientFunc: storeclient.New,
+			method:        "Integer",
+			methodArgs:    []interface{}{ptr.String("foo")},
+			wantError:     &atomic.KeyDoesNotExist{Key: ptr.String("foo")},
+		},
+	}
+
+	ctx := context.Background()
+	for _, tt := range tests {
+		t.Run(tt.desc, func(t *testing.T) {
+			router := yarpcrouter.NewMapRouter("roundtrip-server", tt.procedures)
+			listener, err := net.Listen("tcp", ":0")
+			require.NoError(t, err)
+			inbound := &yarpchttp.Inbound{
+				Listener: listener,
+				Router:   router,
+			}
+			require.NoError(t, inbound.Start(context.Background()), "failed to start server")
+			defer func() {
+				assert.NoError(t, inbound.Stop(context.Background()), "failed to stop server")
+			}()
+
+			dialer := &yarpchttp.Dialer{}
+			require.NoError(t, dialer.Start(context.Background()))
+			outbound := &yarpchttp.Outbound{
+				URL:    &url.URL{Scheme: "http", Host: listener.Addr().String()},
+				Dialer: dialer,
+			}
+			yarpcClient := yarpc.Client{
+				Caller:  "roundtrip-client",
+				Service: "roundtrip-server",
+				Unary:   outbound,
+			}
+
+			// Verify that newClientFunc was valid
+			newClientFuncType := reflect.TypeOf(tt.newClientFunc)
+			require.Equal(t, reflect.Func, newClientFuncType.Kind(),
+				"invalid test: newClientFunc must be a function")
+			require.Equal(t, 1, newClientFuncType.NumOut(),
+				"invalid test: newClientFunc must return a single result")
+
+			clientType := newClientFuncType.Out(0)
+			require.Equal(t, reflect.Interface, clientType.Kind(),
+				"invalid test: newClientFunc must return an Interface")
+
+			clientArgs := []reflect.Value{reflect.ValueOf(yarpcClient)}
+			for _, opt := range clientOpts {
+				clientArgs = append(clientArgs, reflect.ValueOf(opt))
+			}
+
+			client := reflect.ValueOf(tt.newClientFunc).
+				Call(clientArgs)[0]
+
+			if tt.method == "" {
+				return
+			}
+
+			// Equivalent to,
+			//
+			// 	... := client.$method(ctx, $methodArgs...)
+			method := client.MethodByName(tt.method)
+			assert.True(t, method.IsValid(), "Method %q not found", tt.method)
+
+			ctx, cancel := context.WithTimeout(ctx, 200*internaltesttime.Millisecond)
+			defer cancel()
+
+			args := append([]interface{}{ctx}, tt.methodArgs...)
+			returns := method.Call(values(args...))
+
+			switch len(returns) {
+			case 1: // error
+				err, _ := returns[0].Interface().(error)
+				assert.Equal(t, tt.wantError, err)
+			case 2: // (result, err)
+				result := returns[0].Interface()
+				err, _ := returns[1].Interface().(error)
+				if tt.wantError != nil {
+					assert.Equal(t, tt.wantError, err)
+				} else {
+					require.NoError(t, err, "expected success")
+					assert.Equal(t, tt.wantResult, result)
+				}
+			default:
+				t.Fatalf(
+					"impossible: %q returned %d results; only up to 2 are allowed", tt.method, len(returns))
+			}
+		})
+	}
+}
+
+func values(xs ...interface{}) []reflect.Value {
+	vs := make([]reflect.Value, len(xs))
+	for i, x := range xs {
+		vs[i] = reflect.ValueOf(x)
+	}
+	return vs
+}
+
+type storeHandler struct {
+	healthy  bool
+	failWith error
+	integer  int64
+}
+
+func (h *storeHandler) Healthy(ctx context.Context) (bool, error) {
+	return h.healthy, h.failWith
+}
+
+func (h *storeHandler) CompareAndSwap(ctx context.Context, req *atomic.CompareAndSwap) error {
+	return h.failWith
+}
+
+func (h *storeHandler) Forget(ctx context.Context, key *string) error {
+	return h.failWith
+}
+
+func (h *storeHandler) Increment(ctx context.Context, key *string, value *int64) error {
+	return h.failWith
+}
+
+func (h *storeHandler) Integer(ctx context.Context, key *string) (int64, error) {
+	return h.integer, h.failWith
+}
+
+type extendEmptyHandler struct{}
+
+func (extendEmptyHandler) Hello(ctx context.Context) error {
+	return nil
+}
+
+func (extendEmptyHandler) Healthy(ctx context.Context) (bool, error) {
+	return true, nil
+}
