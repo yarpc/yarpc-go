2519414f53ce5541caf3e637dcae60ea0239537b
yarpcbot <yarpcbot@uber.com>
2019-01-01 15:04:32 +0000

    Update generated code at 2019-01-01T15:04:32

--- internal/crossdock/crossdockpb/crossdock.pb.yarpc.go
+++ internal/crossdock/crossdockpb/crossdock.pb.yarpc.go
@@ -2,7 +2,7 @@
 // source: internal/crossdock/crossdockpb/crossdock.proto
 // DO NOT EDIT!
 
-// Copyright (c) 2018 Uber Technologies, Inc.
+// Copyright (c) 2019 Uber Technologies, Inc.
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
