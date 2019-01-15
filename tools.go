2f81b60032f2d2a404c6a62cd630c7d1c598fb35
Abhinav Gupta <abg@uber.com>
2019-01-15 10:07:19 -0800

    glide: Add tools.go and move to imports
    This moves tool dependencies to the `imports:` section of the
    glide.yaml and adds a tools.go with blank imports of these dependencies.
    
    This is needed to have all dependencies of these tools picked up
    correcly. For example, BurntSushi/toml, which is now a dependency of
    staticcheck, was not being picked up.
    
    Note:
    - kisielk/gotool was removed from the glide.yaml beacuse it wasn't being
      used directly.
    - golang/protobuf was added because prometheus/client_model doesn't
      explicitly pin to it.

---
+++ tools.go
@@ -0,0 +1,36 @@
+// Copyright (c) 2019 Uber Technologies, Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+// +build tools
+
+package yarpc
+
+import (
+	// Tools we use during development.
+	_ "github.com/gogo/protobuf/protoc-gen-gogoslick"
+	_ "github.com/golang/mock/mockgen"
+	_ "github.com/kisielk/errcheck"
+	_ "github.com/uber/tchannel-go/thrift/thrift-gen"
+	_ "go.uber.org/thriftrw"
+	_ "go.uber.org/tools/update-license"
+	_ "golang.org/x/lint/golint"
+	_ "golang.org/x/tools/cmd/stringer"
+	_ "honnef.co/go/tools/cmd/staticcheck"
+)
