8f4bb2f5fa5fd3ecd357eb538040cba566301ce7
Nathan Jordan <nathan@uber.com>
2019-03-13 14:47:26 -0700

    use stream context instead of unary outbound on grpc streaming calls

--- transport/grpc/outbound.go
+++ transport/grpc/outbound.go
@@ -302,7 +302,7 @@ func (o *Outbound) stream(
 		span.Finish()
 		return nil, err
 	}
-	stream := newClientStream(streamCtx, req, clientStream, span)
+	stream := newClientStream(clientStream.Context(), req, clientStream, span)
 	tClientStream, err := transport.NewClientStream(stream)
 	if err != nil {
 		span.Finish()
