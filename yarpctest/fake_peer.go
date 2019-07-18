3f146eb9e9dad36baa21e161e4cdadf549f6ef2d
Kris Kowal <kris@uber.com>
2019-07-18 15:15:32 -0700

    [yarpctest] Add connection simulation to fake transport

--- yarpctest/fake_peer.go
+++ yarpctest/fake_peer.go
@@ -21,12 +21,17 @@
 package yarpctest
 
 import (
+	"sync"
+
 	"go.uber.org/yarpc/api/peer"
 )
 
 // FakePeer is a fake peer with an identifier.
 type FakePeer struct {
 	id peer.Identifier
+
+	// mutable
+	lock sync.RWMutex
 	// subscribers needs to be modified under lock in FakeTransport
 	subscribers []peer.Subscriber
 	status      peer.Status
@@ -37,33 +42,87 @@ func (p *FakePeer) Identifier() string {
 	return p.id.Identifier()
 }
 
+// String returns a humane representation of the peer and its status for debugging.
+func (p *FakePeer) String() string {
+	p.lock.RLock()
+	defer p.lock.RUnlock()
+
+	return p.id.Identifier() + ":" + p.status.String()
+}
+
 // Status returns the fake peer status.
 func (p *FakePeer) Status() peer.Status {
+	p.lock.RLock()
+	defer p.lock.RUnlock()
+
 	return p.status
 }
 
 // StartRequest increments pending request count.
 func (p *FakePeer) StartRequest() {
+	p.lock.Lock()
+	defer p.lock.Unlock()
+
 	p.status.PendingRequestCount++
 }
 
 // EndRequest decrements pending request count.
 func (p *FakePeer) EndRequest() {
+	p.lock.Lock()
+	defer p.lock.Unlock()
+
 	p.status.PendingRequestCount--
 }
 
 func (p *FakePeer) simulateConnect() {
-	p.status.ConnectionStatus = peer.Available
-	p.broadcast()
+	p.simulateStatusChange(peer.Available)
 }
 
 func (p *FakePeer) simulateDisconnect() {
-	p.status.ConnectionStatus = peer.Unavailable
-	p.broadcast()
+	p.simulateStatusChange(peer.Unavailable)
 }
 
-func (p *FakePeer) broadcast() {
-	for _, sub := range p.subscribers {
+func (p *FakePeer) simulateStatusChange(status peer.ConnectionStatus) {
+	for _, sub := range p.prepareStatusChange(status) {
 		sub.NotifyStatusChanged(p.id)
 	}
+}
+
+func (p *FakePeer) prepareStatusChange(status peer.ConnectionStatus) []peer.Subscriber {
+	p.lock.Lock()
+	defer p.lock.Unlock()
+
+	p.status.ConnectionStatus = status
+	subscribers := make([]peer.Subscriber, len(p.subscribers))
+	copy(subscribers, p.subscribers)
+	return subscribers
+}
+
+func (p *FakePeer) subscribe(ps peer.Subscriber) {
+	p.lock.Lock()
+	defer p.lock.Unlock()
+
+	p.subscribers = append(p.subscribers, ps)
+}
+
+func (p *FakePeer) unsubscribe(ps peer.Subscriber) int {
+	p.lock.Lock()
+	defer p.lock.Unlock()
+
+	subscribers, count := filterSubscriber(p.subscribers, ps)
+	p.subscribers = subscribers
+	return count
+}
+
+func filterSubscriber(subs []peer.Subscriber, ps peer.Subscriber) ([]peer.Subscriber, int) {
+	res := make([]peer.Subscriber, 0, len(subs))
+	count := 0
+	for _, sub := range subs {
+		if sub != ps {
+			res = append(res, sub)
+		} else {
+			count++
+		}
+	}
+	return res, count
 }
