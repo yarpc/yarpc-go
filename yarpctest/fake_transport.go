cb32c2f41a72b986f7563f25b518f8395d60d5b8
Kris Kowal <kris@uber.com>
2019-07-29 13:34:14 -0700

    Peer list stress test (#1765)
    This set of changes introduces a stress test for peer lists, with concurrent updates, connection status changes, and workers selecting peers.
    
    The change has three parts:
    1. Add features to the fake transport such that it can emulate connection status changes and is concurrency safe.
    2. Introduce a stress test scaffold to the YARPC test library.
    3. Introduce a stress test binary that benchmarks all of the peer list implementations provided by YARPC proper.

--- yarpctest/fake_transport.go
+++ yarpctest/fake_transport.go
@@ -25,8 +25,7 @@ import (
 	"sync"
 
 	"go.uber.org/yarpc/api/peer"
-	"go.uber.org/yarpc/api/transport"
-	"go.uber.org/yarpc/pkg/lifecycletest"
+	"go.uber.org/yarpc/pkg/lifecycle"
 )
 
 // FakeTransportOption is an option for NewFakeTransport.
@@ -50,13 +49,38 @@ func InitialConnectionStatus(s peer.ConnectionStatus) FakeTransportOption {
 	}
 }
 
+// RetainErrors specifies an error for RetainPeer to return for the given
+// addresses.
+func RetainErrors(err error, addrs []string) FakeTransportOption {
+	return func(t *FakeTransport) {
+		for _, addr := range addrs {
+			t.retainErrors[addr] = err
+		}
+	}
+}
+
+// ReleaseErrors specifies an error for ReleasePeer to return for the given
+// addresses.
+func ReleaseErrors(err error, addrs []string) FakeTransportOption {
+	return func(t *FakeTransport) {
+		for _, addr := range addrs {
+			t.releaseErrors[addr] = err
+		}
+	}
+}
+
 // NewFakeTransport returns a fake transport.
 func NewFakeTransport(opts ...FakeTransportOption) *FakeTransport {
 	t := &FakeTransport{
-		Lifecycle:               lifecycletest.NewNop(),
-		initialConnectionStatus: peer.Available,
-		peers:                   make(map[string]*FakePeer),
-		mu:                      &sync.Mutex{},
+		once:                          lifecycle.NewOnce(),
+		initialConnectionStatus:       peer.Available,
+		initialPeerConnectionStatuses: make(map[string]peer.ConnectionStatus),
+
+		peers:                make(map[string]*FakePeer),
+		retainErrors:         make(map[string]error),
+		releaseErrors:        make(map[string]error),
+		pendingStatusChanges: make(chan struct{}, 1),
+		done:                 make(chan struct{}),
 	}
 	for _, opt := range opts {
 		opt(t)
@@ -66,16 +90,41 @@ func NewFakeTransport(opts ...FakeTransportOption) *FakeTransport {
 
 // FakeTransport is a fake transport.
 type FakeTransport struct {
-	transport.Lifecycle
-	nopOption               string
-	initialConnectionStatus peer.ConnectionStatus
-	peers                   map[string]*FakePeer
-	mu                      *sync.Mutex
+	nopOption                     string
+	initialConnectionStatus       peer.ConnectionStatus
+	initialPeerConnectionStatuses map[string]peer.ConnectionStatus
+	retainErrors                  map[string]error
+	releaseErrors                 map[string]error
+
+	once                 *lifecycle.Once
+	mu                   sync.RWMutex
+	peers                map[string]*FakePeer
+	changesQueue         []statusChange
+	pendingStatusChanges chan struct{}
+	done                 chan struct{}
 }
 
 // NopOption returns the configured nopOption. It's fake.
 func (t *FakeTransport) NopOption() string {
 	return t.nopOption
+}
+
+// SimulateRetainError leaves a note that any subsequent Retain for a
+// particular address should return an error.
+func (t *FakeTransport) SimulateRetainError(id peer.Identifier, err error) {
+	t.retainErrors[id.Identifier()] = err
+}
+
+// SimulateReleaseError leaves a note that any subsequent Release for a particular
+// address should return an error.
+func (t *FakeTransport) SimulateReleaseError(id peer.Identifier, err error) {
+	t.releaseErrors[id.Identifier()] = err
+}
+
+// SimulateStatusChange simulates a connection or disconnection to the peer,
+// marking the peer connection status and notifying all subscribers.
+func (t *FakeTransport) SimulateStatusChange(id peer.Identifier, status peer.ConnectionStatus) {
+	t.Peer(id).simulateStatusChange(status)
 }
 
 // SimulateConnect simulates a connection to the peer, marking the peer as
@@ -111,37 +160,123 @@ func (t *FakeTransport) Peer(id peer.Identifier) *FakePeer {
 
 // RetainPeer returns a fake peer.
 func (t *FakeTransport) RetainPeer(id peer.Identifier, ps peer.Subscriber) (peer.Peer, error) {
+	if err := t.retainErrors[id.Identifier()]; err != nil {
+		return nil, err
+	}
 	peer := t.Peer(id)
-	t.mu.Lock()
-	defer t.mu.Unlock()
-	peer.subscribers = append(peer.subscribers, ps)
+	peer.subscribe(ps)
+	t.enqueue(statusChange{
+		Peer:   peer,
+		Status: t.getInitialStatus(id.Identifier()),
+	})
+
+	t.scheduleFlush()
 	return peer, nil
+}
+
+func (t *FakeTransport) getInitialStatus(addr string) peer.ConnectionStatus {
+	if status, ok := t.initialPeerConnectionStatuses[addr]; ok {
+		return status
+	}
+	return t.initialConnectionStatus
 }
 
 // ReleasePeer does nothing.
 func (t *FakeTransport) ReleasePeer(id peer.Identifier, ps peer.Subscriber) error {
 	peer := t.Peer(id)
+
 	t.mu.Lock()
 	defer t.mu.Unlock()
-	if subscribers, count := filterSubscriber(peer.subscribers, ps); count == 0 {
+
+	if err := t.releaseErrors[id.Identifier()]; err != nil {
+		return err
+	}
+
+	if count := peer.unsubscribe(ps); count == 0 {
 		return fmt.Errorf("no such subscriber")
 	} else if count > 1 {
 		return fmt.Errorf("extra subscribers: %d", count-1)
-	} else {
-		peer.subscribers = subscribers
 	}
+
+	t.scheduleFlush()
 	return nil
 }
 
-func filterSubscriber(subs []peer.Subscriber, ps peer.Subscriber) ([]peer.Subscriber, int) {
-	res := make([]peer.Subscriber, 0, len(subs))
-	count := 0
-	for _, sub := range subs {
-		if sub != ps {
-			res = append(res, sub)
-		} else {
-			count++
+// Start spins up a goroutine to asynchronously flush status change notifications.
+//
+// If you do not start a fake dialer, you must call Flush explicitly.
+func (t *FakeTransport) Start() error {
+	return t.once.Start(func() error {
+		go t.monitor()
+		return nil
+	})
+}
+
+// Stop shuts down the fake dialer, allowing its status change notification
+// loop to exit.
+func (t *FakeTransport) Stop() error {
+	return t.once.Stop(func() error {
+		close(t.done)
+		return nil
+	})
+}
+
+// IsRunning returns whether the fake transport is running.
+func (t *FakeTransport) IsRunning() bool {
+	return t.once.IsRunning()
+}
+
+func (t *FakeTransport) scheduleFlush() {
+	select {
+	case t.pendingStatusChanges <- struct{}{}:
+	default:
+	}
+}
+
+func (t *FakeTransport) monitor() {
+Loop:
+	for {
+		select {
+		case <-t.done:
+			break Loop
+		case <-t.pendingStatusChanges:
+			t.Flush()
 		}
 	}
-	return res, count
+}
+
+type statusChange struct {
+	Peer   *FakePeer
+	Status peer.ConnectionStatus
+}
+
+// Flush effects all queued status changes from retaining or releasing peers.
+//
+// Calling RetainPeer and ReleasePeer schedules a peer status change and its
+// notifications.
+// Concrete dialer implementations dispatch these notifications from a
+// goroutine and subscribers may obtain a lock on the peer status.
+// For testability, the fake transport queues these changes and calling Flush
+// dispatches the notifications synchronously, but still off the RetainPeer and
+// ReleasePeer stacks.
+func (t *FakeTransport) Flush() {
+	for _, change := range t.dequeue() {
+		change.Peer.simulateStatusChange(change.Status)
+	}
+}
+
+func (t *FakeTransport) enqueue(change statusChange) {
+	t.mu.Lock()
+	defer t.mu.Unlock()
+
+	t.changesQueue = append(t.changesQueue, change)
+}
+
+func (t *FakeTransport) dequeue() []statusChange {
+	t.mu.Lock()
+	defer t.mu.Unlock()
+
+	queue := t.changesQueue
+	t.changesQueue = make([]statusChange, 0)
+	return queue
 }
---
+++ yarpctest/stress_test.go
@@ -0,0 +1,113 @@
+// Copyright (c) 2019 Uber Technologies, Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+package yarpctest
+
+import (
+	"context"
+	"fmt"
+	"sync"
+	"testing"
+	"time"
+
+	"go.uber.org/yarpc/api/peer"
+	"go.uber.org/yarpc/api/transport"
+)
+
+func TestStress(t *testing.T) {
+	test := ListStressTest{
+		Workers:  1,
+		Duration: time.Second,
+		Timeout:  10 * time.Millisecond,
+		New: func(t peer.Transport) peer.ChooserList {
+			return newMRUList(t)
+		},
+	}
+	report := test.Run(t)
+	report.Log(t)
+	test.Log(t)
+}
+
+var _ peer.ChooserList = (*mruList)(nil)
+
+type mruList struct {
+	transport peer.Transport
+	peer      peer.Peer
+	mu        sync.Mutex
+}
+
+func newMRUList(t peer.Transport) *mruList {
+	return &mruList{transport: t}
+}
+
+func (l *mruList) Start() error {
+	return nil
+}
+
+func (l *mruList) Stop() error {
+	return nil
+}
+
+func (l *mruList) IsRunning() bool {
+	return true
+}
+
+func (l *mruList) Choose(context.Context, *transport.Request) (peer peer.Peer, onFinish func(error), err error) {
+	l.mu.Lock()
+	defer l.mu.Unlock()
+
+	if l.peer == nil {
+		return nil, nil, fmt.Errorf("no peer available")
+	}
+
+	return l.peer, func(error) {}, nil
+}
+
+func (l *mruList) Update(updates peer.ListUpdates) error {
+	l.mu.Lock()
+	defer l.mu.Unlock()
+
+	for _, pid := range updates.Additions {
+		l.update(pid)
+	}
+
+	if (len(updates.Additions)+len(updates.Removals))%2 == 0 {
+		return fmt.Errorf("can you handle this")
+	}
+	if len(updates.Additions) == 0 {
+		return fmt.Errorf("parting is such sweet sorrow")
+	}
+	return nil
+}
+
+func (l *mruList) update(id peer.Identifier) {
+	if peer, err := l.transport.RetainPeer(id, nopSub); err == nil {
+		if l.peer != nil {
+			_ = l.transport.ReleasePeer(id, nopSub)
+		}
+		l.peer = peer
+	}
+}
+
+type _nopSub struct{}
+
+func (_nopSub) NotifyStatusChanged(peer.Identifier) {}
+
+var nopSub = _nopSub{}
