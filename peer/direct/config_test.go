8744936e95ae157b6ad26ab5137954e2a71818d5
Alex Peats-Bond <apeatsbond@uber.com>
2019-09-18 11:02:34 -0700

    Introduce direct peer chooser (#1789)
    This introduces a new peer chooser, `direct`, that returns the peer
    identified by `transport.Request#ShardKey`. This supports directly
    addressable peers across transports.
    
    The `direct` chooser is not a `peer.List` as it only holds onto peers
    for as long as it takes for a call to succeed. Therefore, it has no
    `Update` method.
    
    The configuration structs are added for forwards
    compatibility. Service owners config can register the
    `yarpcconfig.PeerChooserSpec` and configure the peer chooser as
    follows:
    
     ```
     outbounds:
       destination-service:
         grpc:
    	direct: {}
     ```

---
+++ peer/direct/config_test.go
@@ -0,0 +1,56 @@
+// Copyright (c) 2019 Uber Technologies, Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+package direct
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"go.uber.org/yarpc/yarpcconfig"
+	"go.uber.org/yarpc/yarpctest"
+)
+
+type attrs map[string]interface{}
+
+func TestConfig(t *testing.T) {
+	const theirService = "their-service"
+
+	cfg := yarpcconfig.New()
+	assert.NotPanics(t, func() {
+		cfg.MustRegisterPeerChooser(Spec())
+		cfg.MustRegisterTransport(yarpctest.FakeTransportSpec())
+	})
+
+	config, err := cfg.LoadConfig("our-service", attrs{
+		"outbounds": attrs{
+			theirService: attrs{
+				"fake-transport": attrs{
+					name: attrs{},
+				},
+			},
+		},
+	})
+	require.NoError(t, err)
+	require.NotNil(t, config.Outbounds)
+	require.NotNil(t, config.Outbounds[theirService])
+	require.NotNil(t, config.Outbounds[theirService].Unary)
+}
