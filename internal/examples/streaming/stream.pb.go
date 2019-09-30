82b2f398063befeab531ee3dfc90007ef688d7ef
yarpcbot <yarpcbot@uber.com>
2019-09-30 15:05:27 +0000

    Update generated code at 2019-09-30T15:05:27

--- internal/examples/streaming/stream.pb.go
+++ internal/examples/streaming/stream.pb.go
@@ -28,8 +28,11 @@ import (
 	fmt "fmt"
 	proto "github.com/gogo/protobuf/proto"
 	grpc "google.golang.org/grpc"
+	codes "google.golang.org/grpc/codes"
+	status "google.golang.org/grpc/status"
 	io "io"
 	math "math"
+	math_bits "math/bits"
 	reflect "reflect"
 	strings "strings"
 )
@@ -43,7 +46,7 @@ var _ = math.Inf
 // is compatible with the proto package it is being compiled against.
 // A compilation error at this line likely means your copy of the
 // proto package needs to be updated.
-const _ = proto.GoGoProtoPackageIsVersion2 // please upgrade the proto package
+const _ = proto.GoGoProtoPackageIsVersion3 // please upgrade the proto package
 
 type HelloRequest struct {
 	Id string `protobuf:"bytes,1,opt,name=id,proto3" json:"id,omitempty"`
@@ -62,7 +65,7 @@ func (m *HelloRequest) XXX_Marshal(b []byte, deterministic bool) ([]byte, error)
 		return xxx_messageInfo_HelloRequest.Marshal(b, m, deterministic)
 	} else {
 		b = b[:cap(b)]
-		n, err := m.MarshalTo(b)
+		n, err := m.MarshalToSizedBuffer(b)
 		if err != nil {
 			return nil, err
 		}
@@ -105,7 +108,7 @@ func (m *HelloResponse) XXX_Marshal(b []byte, deterministic bool) ([]byte, error
 		return xxx_messageInfo_HelloResponse.Marshal(b, m, deterministic)
 	} else {
 		b = b[:cap(b)]
-		n, err := m.MarshalTo(b)
+		n, err := m.MarshalToSizedBuffer(b)
 		if err != nil {
 			return nil, err
 		}
@@ -379,6 +382,23 @@ type HelloServer interface {
 	HelloInStream(*HelloRequest, Hello_HelloInStreamServer) error
 }
 
+// UnimplementedHelloServer can be embedded to have forward compatible implementations.
+type UnimplementedHelloServer struct {
+}
+
+func (*UnimplementedHelloServer) HelloUnary(ctx context.Context, req *HelloRequest) (*HelloResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method HelloUnary not implemented")
+}
+func (*UnimplementedHelloServer) HelloThere(srv Hello_HelloThereServer) error {
+	return status.Errorf(codes.Unimplemented, "method HelloThere not implemented")
+}
+func (*UnimplementedHelloServer) HelloOutStream(srv Hello_HelloOutStreamServer) error {
+	return status.Errorf(codes.Unimplemented, "method HelloOutStream not implemented")
+}
+func (*UnimplementedHelloServer) HelloInStream(req *HelloRequest, srv Hello_HelloInStreamServer) error {
+	return status.Errorf(codes.Unimplemented, "method HelloInStream not implemented")
+}
+
 func RegisterHelloServer(s *grpc.Server, srv HelloServer) {
 	s.RegisterService(&_Hello_serviceDesc, srv)
 }
@@ -507,7 +527,7 @@ var _Hello_serviceDesc = grpc.ServiceDesc{
 func (m *HelloRequest) Marshal() (dAtA []byte, err error) {
 	size := m.Size()
 	dAtA = make([]byte, size)
-	n, err := m.MarshalTo(dAtA)
+	n, err := m.MarshalToSizedBuffer(dAtA[:size])
 	if err != nil {
 		return nil, err
 	}
@@ -515,23 +535,29 @@ func (m *HelloRequest) Marshal() (dAtA []byte, err error) {
 }
 
 func (m *HelloRequest) MarshalTo(dAtA []byte) (int, error) {
-	var i int
+	size := m.Size()
+	return m.MarshalToSizedBuffer(dAtA[:size])
+}
+
+func (m *HelloRequest) MarshalToSizedBuffer(dAtA []byte) (int, error) {
+	i := len(dAtA)
 	_ = i
 	var l int
 	_ = l
 	if len(m.Id) > 0 {
-		dAtA[i] = 0xa
-		i++
+		i -= len(m.Id)
+		copy(dAtA[i:], m.Id)
 		i = encodeVarintStream(dAtA, i, uint64(len(m.Id)))
-		i += copy(dAtA[i:], m.Id)
+		i--
+		dAtA[i] = 0xa
 	}
-	return i, nil
+	return len(dAtA) - i, nil
 }
 
 func (m *HelloResponse) Marshal() (dAtA []byte, err error) {
 	size := m.Size()
 	dAtA = make([]byte, size)
-	n, err := m.MarshalTo(dAtA)
+	n, err := m.MarshalToSizedBuffer(dAtA[:size])
 	if err != nil {
 		return nil, err
 	}
@@ -539,27 +565,35 @@ func (m *HelloResponse) Marshal() (dAtA []byte, err error) {
 }
 
 func (m *HelloResponse) MarshalTo(dAtA []byte) (int, error) {
-	var i int
+	size := m.Size()
+	return m.MarshalToSizedBuffer(dAtA[:size])
+}
+
+func (m *HelloResponse) MarshalToSizedBuffer(dAtA []byte) (int, error) {
+	i := len(dAtA)
 	_ = i
 	var l int
 	_ = l
 	if len(m.Id) > 0 {
-		dAtA[i] = 0xa
-		i++
+		i -= len(m.Id)
+		copy(dAtA[i:], m.Id)
 		i = encodeVarintStream(dAtA, i, uint64(len(m.Id)))
-		i += copy(dAtA[i:], m.Id)
+		i--
+		dAtA[i] = 0xa
 	}
-	return i, nil
+	return len(dAtA) - i, nil
 }
 
 func encodeVarintStream(dAtA []byte, offset int, v uint64) int {
+	offset -= sovStream(v)
+	base := offset
 	for v >= 1<<7 {
 		dAtA[offset] = uint8(v&0x7f | 0x80)
 		v >>= 7
 		offset++
 	}
 	dAtA[offset] = uint8(v)
-	return offset + 1
+	return base
 }
 func (m *HelloRequest) Size() (n int) {
 	if m == nil {
@@ -588,14 +622,7 @@ func (m *HelloResponse) Size() (n int) {
 }
 
 func sovStream(x uint64) (n int) {
-	for {
-		n++
-		x >>= 7
-		if x == 0 {
-			break
-		}
-	}
-	return n
+	return (math_bits.Len64(x|1) + 6) / 7
 }
 func sozStream(x uint64) (n int) {
 	return sovStream(uint64((x << 1) ^ uint64((int64(x) >> 63))))
