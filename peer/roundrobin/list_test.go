788a9f8ec3800216a59b7a7654cf3cd65060e774
Kris Kowal <kris@uber.com>
2020-01-07 14:45:44 -0800

    Reduce the public interface of peer lists (#1866)
    This change removes the publicly embedded concrete list implementation
    from every peer list and instead exposes only the methods of peer.List
    and peer.Chooser.
    
    This is technically a breaking change.
    There is potential for compatibility fallout with users that expected
    additional methods.
    However, we estimate that all these uses are bugs that should be fixed.

--- peer/roundrobin/list_test.go
+++ peer/roundrobin/list_test.go
@@ -913,21 +913,21 @@ func TestRoundRobinList(t *testing.T) {
 			}
 			ApplyPeerListActions(t, pl, tt.peerListActions, deps)
 
-			assert.Equal(t, len(tt.expectedAvailablePeers), pl.NumAvailable(), "invalid available peerlist size")
+			assert.Equal(t, len(tt.expectedAvailablePeers), pl.list.NumAvailable(), "invalid available peerlist size")
 			for _, expectedRingPeer := range tt.expectedAvailablePeers {
-				ok := pl.Available(hostport.PeerIdentifier(expectedRingPeer))
+				ok := pl.list.Available(hostport.PeerIdentifier(expectedRingPeer))
 				assert.True(t, ok, fmt.Sprintf("expected peer: %s was not in available peerlist", expectedRingPeer))
 			}
 
-			assert.Equal(t, len(tt.expectedUnavailablePeers), pl.NumUnavailable(), "invalid unavailable peerlist size")
+			assert.Equal(t, len(tt.expectedUnavailablePeers), pl.list.NumUnavailable(), "invalid unavailable peerlist size")
 			for _, expectedUnavailablePeer := range tt.expectedUnavailablePeers {
-				ok := !pl.Available(hostport.PeerIdentifier(expectedUnavailablePeer))
+				ok := !pl.list.Available(hostport.PeerIdentifier(expectedUnavailablePeer))
 				assert.True(t, ok, fmt.Sprintf("expected peer: %s was not in unavailable peerlist", expectedUnavailablePeer))
 			}
 
-			assert.Equal(t, len(tt.expectedUninitializedPeers), pl.NumUninitialized(), "invalid uninitialized peerlist size")
+			assert.Equal(t, len(tt.expectedUninitializedPeers), pl.list.NumUninitialized(), "invalid uninitialized peerlist size")
 			for _, expectedUninitializedPeer := range tt.expectedUninitializedPeers {
-				ok := pl.Uninitialized(hostport.PeerIdentifier(expectedUninitializedPeer))
+				ok := pl.list.Uninitialized(hostport.PeerIdentifier(expectedUninitializedPeer))
 				assert.True(t, ok, fmt.Sprintf("expected peer: %s was not in uninitialized peerlist", expectedUninitializedPeer))
 			}
 
