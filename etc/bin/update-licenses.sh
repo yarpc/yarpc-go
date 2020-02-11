eff9d575ba3a4cdb56e8890cd9bc9419c472ecf1
Kris Kowal <kris@uber.com>
2020-02-11 09:56:23 -0800

    Fix update-licenses for go modules

--- etc/bin/update-licenses.sh
+++ etc/bin/update-licenses.sh
@@ -5,9 +5,12 @@ set -e
 DIR="$(cd "$(dirname "${0}")/../.." && pwd)"
 cd "${DIR}"
 
+export GOBIN=$DIR/etc/bin
+go install go.uber.org/tools/update-license
+
 # We need to ignore internal/tests for licenses so that the golden test for
 # thriftrw-plugin-yarpc can verify the contents of the generated code without
 # running updateLicenses on it.
-update-license $(find . -name '*.go' \
+$GOBIN/update-license $(find . -name '*.go' \
 	| grep -v '^\./vendor' \
 	| grep -v '/thriftrw-plugin-yarpc/internal/tests/')
