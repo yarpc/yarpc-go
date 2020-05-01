b28c95321ff8a68c59670e95a436397d6fa54dfe
Alex Peats-Bond <apeatsbond@uber.com>
2020-05-01 16:04:12 -0400

    gRPC: Add streaming test with out context deadline
    This adds an integration test to verify that streams may be created
    without context deadlines. Users should use cancelable contexts
    instead of timeouts for long-lived streams.
    
    Before the last commit, this test fails with:
    
    ```
       Error:      	Received unexpected error:
                   	code:invalid-argument message:"round-robin" peer list can't wait for peer without a context deadline
    Test:       	TestStreamingWithNoCtxDeadline
    Messages:   	could not create client stream
    ```

---
+++ transport/grpc/external_integration_test.go
@@ -0,0 +1,118 @@
+// Copyright (c) 2020 Uber Technologies, Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+package grpc_test
+
+import (
+	"context"
+	"net"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"go.uber.org/yarpc"
+	"go.uber.org/yarpc/api/peer"
+	"go.uber.org/yarpc/api/transport"
+	"go.uber.org/yarpc/internal/prototest/example"
+	"go.uber.org/yarpc/internal/prototest/examplepb"
+	"go.uber.org/yarpc/peer/hostport"
+	"go.uber.org/yarpc/peer/roundrobin"
+	"go.uber.org/yarpc/transport/grpc"
+)
+
+func TestStreamingWithNoCtxDeadline(t *testing.T) {
+	// This test ensures that we can use gRPC streaming without a context deadline
+	// set. For long-lived streams, it should be unnecesary for users to set a
+	// deadline; instead they should use context.WithCancel to cancel the stream.
+
+	const serviceName = "service-name"
+
+	// init YARPC transport / inbound / outbound
+	grpcTransport := grpc.NewTransport()
+	peerList := roundrobin.New(grpcTransport)
+
+	listener, err := net.Listen("tcp", "127.0.0.1:0")
+	require.NoError(t, err, "could not start listener")
+	inbound := grpcTransport.NewInbound(listener)
+
+	dispatcher := yarpc.NewDispatcher(yarpc.Config{
+		Name:     serviceName,
+		Inbounds: yarpc.Inbounds{inbound},
+		Outbounds: yarpc.Outbounds{
+			serviceName: {
+				ServiceName: serviceName,
+				Stream:      grpcTransport.NewOutbound(peerList),
+			},
+		},
+	})
+	dispatcher.Register(
+		examplepb.BuildFooYARPCProcedures(
+			example.NewFooYARPCServer(transport.NewHeaders())))
+
+	require.NoError(t, dispatcher.Start(), "could not start dispatcher")
+	defer func() { assert.NoError(t, dispatcher.Stop(), "could not stop dispatcher") }()
+
+	// add streaming peer so we can call ourself
+	err = peerList.Update(peer.ListUpdates{Additions: []peer.Identifier{
+		hostport.PeerIdentifier(listener.Addr().String()),
+	}})
+	require.NoError(t, err, "could not add peer to peer list")
+
+	waitForPeerAvailable(t, peerList, time.Second)
+
+	// init streaming client
+	client := examplepb.NewFooYARPCClient(dispatcher.ClientConfig(serviceName))
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	streamClient, err := client.EchoBoth(ctx)
+	require.NoError(t, err, "could not create client stream")
+
+	// veryify we can send a request
+	err = streamClient.Send(&examplepb.EchoBothRequest{
+		Message:      "test message!",
+		NumResponses: 0,
+	})
+	require.NoError(t, err, "could not send message")
+	assert.NoError(t, streamClient.CloseSend(), "could not close stream")
+}
+
+// waitForPeerAvailable ensures that the peer becomes available before
+// proceeding, and that we do not wait forever.
+func waitForPeerAvailable(t *testing.T, peerList *roundrobin.List, wait time.Duration) {
+	peerAvailable := make(chan struct{})
+	go func() {
+		for {
+			if peerList.Peers()[0].Status().ConnectionStatus == peer.Available {
+				close(peerAvailable)
+				return
+			}
+			time.Sleep(10 * time.Millisecond)
+		}
+	}()
+
+	select {
+	case <-time.After(wait):
+		t.Fatal("failed waiting to connect to peer")
+	case <-peerAvailable:
+		return
+	}
+}
