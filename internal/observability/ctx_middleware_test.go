c615aeb4f3079da457e17283571e044bd49a160d
Alex Peats-Bond <apeatsbond@uber.com>
2020-05-14 22:54:13 -0400

    observability: Add log for dropped responses
    Now that we are potentially overriding handler responses with context
    errors, we need to let users know somehow. This change adds a
    `dropped` log field into the existing metrics, showing the underlying
    success, error or application error that was dropped.

--- internal/observability/ctx_middleware_test.go
+++ internal/observability/ctx_middleware_test.go
@@ -27,11 +27,13 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"go.uber.org/yarpc/api/transport"
 	"go.uber.org/yarpc/api/transport/transporttest"
 	"go.uber.org/yarpc/yarpcerrors"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
+	"go.uber.org/zap/zaptest/observer"
 )
 
 func TestContextMiddleware(t *testing.T) {
@@ -40,10 +42,10 @@ func TestContextMiddleware(t *testing.T) {
 		ctxCancelledMsg        = `call to procedure "my-procedure" of service "my-service" from caller "my-caller" was canceled`
 	)
 
+	core, logs := observer.New(zapcore.DebugLevel)
 	infoLevel := zapcore.InfoLevel
-
 	mw := NewMiddleware(Config{
-		Logger:           zap.NewNop(),
+		Logger:           zap.New(core),
 		ContextExtractor: NewNopContextExtractor(),
 		Levels: LevelsConfig{
 			Default: DirectionalLevelsConfig{
@@ -57,22 +59,32 @@ func TestContextMiddleware(t *testing.T) {
 	tests := []struct {
 		name       string
 		handlerErr error
+		appErr     bool
 		ctx        func() context.Context
 
 		wantDeadlineExceeded bool
 		wantCtxCancelled     bool
 	}{
 		{
-			name: "no-op with no handler err",
+			name: "no-op/handler success",
 			ctx:  func() context.Context { return context.Background() },
 		},
 		{
-			name:       "no-op with handler err",
+			name:       "no-op/handler err",
 			handlerErr: errors.New("an err"),
 			ctx:        func() context.Context { return context.Background() },
 		},
 		{
-			name:       "ctx deadline exceeded error",
+			name: "deadline exceeded/handler success",
+			ctx: func() context.Context {
+				ctx, cancel := context.WithTimeout(context.Background(), -1)
+				cancel()
+				return ctx
+			},
+			wantDeadlineExceeded: true,
+		},
+		{
+			name:       "deadline exceeded/handler err",
 			handlerErr: fmt.Errorf("my custom error"),
 			ctx: func() context.Context {
 				ctx, cancel := context.WithTimeout(context.Background(), -1)
@@ -82,13 +94,42 @@ func TestContextMiddleware(t *testing.T) {
 			wantDeadlineExceeded: true,
 		},
 		{
-			name:       "ctx cancelled error",
+			name: "deadline exceeded/app err",
+			ctx: func() context.Context {
+				ctx, cancel := context.WithTimeout(context.Background(), -1)
+				cancel()
+				return ctx
+			},
+			appErr:               true,
+			wantDeadlineExceeded: true,
+		},
+		{
+			name: "cancelled error/handler success",
+			ctx: func() context.Context {
+				ctx, cancel := context.WithCancel(context.Background())
+				cancel()
+				return ctx
+			},
+			wantCtxCancelled: true,
+		},
+		{
+			name:       "cancelled error/handler err",
 			handlerErr: fmt.Errorf("my custom error"),
 			ctx: func() context.Context {
 				ctx, cancel := context.WithCancel(context.Background())
 				cancel()
 				return ctx
 			},
+			wantCtxCancelled: true,
+		},
+		{
+			name: "cancelled error/app err",
+			ctx: func() context.Context {
+				ctx, cancel := context.WithCancel(context.Background())
+				cancel()
+				return ctx
+			},
+			appErr:           true,
 			wantCtxCancelled: true,
 		},
 	}
@@ -99,9 +140,33 @@ func TestContextMiddleware(t *testing.T) {
 		Caller:    "my-caller",
 	}
 
+	expectLogField := func(appErr bool, err error) *zap.Field {
+		dropMsg := _droppedSuccessLog
+		if err == nil && appErr {
+			dropMsg = _droppedAppErrLog
+		} else if err != nil {
+			dropMsg = fmt.Sprintf(_droppedErrLogFmt, err)
+		}
+		log := zap.String(_dropped, dropMsg)
+		return &log
+	}
+
+	getDropLogField := func(t *testing.T) *zap.Field {
+		entries := logs.TakeAll()
+		require.Equal(t, 1, len(entries), "unexpected number of logs written: %v", entries)
+		for _, f := range entries[0].Context {
+			if f.Key == _dropped {
+				return &f
+			}
+		}
+		return nil
+	}
+
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			handler := &testHandler{err: tt.handlerErr}
+			defer logs.TakeAll() // throw away logs for next run
+
+			handler := &testHandler{err: tt.handlerErr, appErr: tt.appErr}
 			err := mw.Handle(tt.ctx(), req, &transporttest.FakeResponseWriter{}, handler)
 
 			if tt.wantDeadlineExceeded {
@@ -109,6 +174,8 @@ func TestContextMiddleware(t *testing.T) {
 					err,
 					yarpcerrors.DeadlineExceededErrorf(ctxDeadlineExceededMsg).Error(),
 					"expected deadline exceeded error override")
+
+				assert.Equal(t, expectLogField(tt.appErr, tt.handlerErr), getDropLogField(t), "unexpected log")
 				return
 			}
 
@@ -117,18 +184,25 @@ func TestContextMiddleware(t *testing.T) {
 					err,
 					yarpcerrors.CancelledErrorf(ctxCancelledMsg).Error(),
 					"expected cancelled yarpcerror code")
+
+				assert.Equal(t, expectLogField(tt.appErr, tt.handlerErr), getDropLogField(t), "unexpected log")
 				return
 			}
 
 			assert.Equal(t, tt.handlerErr, err, "unexpected error")
+			assert.Nil(t, getDropLogField(t), "unexpectedly saw 'dropped' log field")
 		})
 	}
 }
 
 type testHandler struct {
-	err error
+	err    error
+	appErr bool
 }
 
 func (h *testHandler) Handle(ctx context.Context, req *transport.Request, resw transport.ResponseWriter) error {
+	if h.appErr {
+		resw.SetApplicationError()
+	}
 	return h.err
 }
