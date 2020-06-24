f3bcc04768748fab11707d07ce56159a388ff951
Alex Peats-Bond <apeatsbond@uber.com>
2020-06-23 20:18:03 -0400

    Assert on error components instead of complete type

--- transport/grpc/integration_test.go
+++ transport/grpc/integration_test.go
@@ -129,7 +129,12 @@ func TestYARPCErrorWithDetails(t *testing.T) {
 	te.do(t, func(t *testing.T, e *testEnv) {
 		e.KeyValueYARPCServer.SetNextError(protobuf.NewError(yarpcerrors.CodeNotFound, "hello world", protobuf.WithErrorDetails(&examplepb.SetValueResponse{})))
 		err := e.SetValueYARPC(context.Background(), "foo", "bar")
-		assert.Equal(t, protobuf.NewError(yarpcerrors.CodeNotFound, "hello world", protobuf.WithErrorDetails(&examplepb.SetValueResponse{})), err)
+
+		assert.Equal(t, yarpcerrors.CodeNotFound, yarpcerrors.FromError(err).Code())
+		assert.Equal(t, "hello world", yarpcerrors.FromError(err).Message())
+		deets := protobuf.GetErrorDetails(err)
+		require.Len(t, deets, 1, "unexpected")
+		assert.IsType(t, deets[0], &examplepb.SetValueResponse{})
 	})
 }
 
@@ -170,9 +175,11 @@ func TestGRPCErrorWithDetails(t *testing.T) {
 		e.KeyValueYARPCServer.SetNextError(protobuf.NewError(yarpcerrors.CodeNotFound, "hello world", protobuf.WithErrorDetails(&examplepb.SetValueResponse{})))
 		err := e.SetValueGRPC(context.Background(), "foo", "bar")
 		st := status.Convert(err)
-		assert.Equal(t, st.Code(), codes.NotFound)
-		assert.Equal(t, st.Message(), "hello world")
-		assert.Equal(t, st.Details(), []interface{}{&examplepb.SetValueResponse{}})
+		assert.Equal(t, codes.NotFound, st.Code())
+		assert.Equal(t, "hello world", st.Message())
+
+		require.Len(t, st.Details(), 1)
+		assert.IsType(t, &examplepb.SetValueResponse{}, st.Details()[0])
 	})
 }
 
