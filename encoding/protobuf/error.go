aca0468cfe7e61d064119a8e7e5ed3ff281c74ba
Alexandre Wilhelm <awilhelm@uber.com>
2021-06-10 12:45:15 -0700

    Fixed: linter

--- encoding/protobuf/error.go
+++ encoding/protobuf/error.go
@@ -154,7 +154,7 @@ func createStatusWithDetail(pberr *pberror, encoding transport.Encoding, codec *
 	st := status.New(grpcerrorcodes.YARPCCodeToGRPCCode[pberr.code], pberr.message)
 	// Here we check that status.New has returned a valid error.
 	if st.Err() == nil {
-		return nil, errors.New(fmt.Sprintf("no status error for error with code %d", pberr.code))
+		return nil, fmt.Errorf("no status error for error with code %d", pberr.code)
 	}
 	pst := st.Proto()
 	pst.Details = pberr.details
