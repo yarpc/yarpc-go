2fc4fb301e3998c4b284c9a3d08218967894cfe2
Ronak Jain <ronakj@uber.com>
2022-10-07 01:00:15 +0530

    [6/n] tls: integrate gRPC outbound tls (#2172)

--- yarpcconfig/builder.go
+++ yarpcconfig/builder.go
@@ -266,7 +266,7 @@ func (b *builder) AddUnaryOutbound(
 	}
 
 	b.needTransport(spec)
-	cv, err := spec.UnaryOutbound.Decode(attrs, config.InterpolateWith(b.kit.resolver))
+	cv, err := spec.UnaryOutbound.Decode(attrs, config.InterpolateWith(b.kit.resolver), mapdecode.DecodeHook(tlsModeDecodeHook))
 	if err != nil {
 		return fmt.Errorf("failed to decode unary outbound configuration: %v", err)
 	}
@@ -312,7 +312,7 @@ func (b *builder) AddStreamOutbound(
 	}
 
 	b.needTransport(spec)
-	cv, err := spec.StreamOutbound.Decode(attrs, config.InterpolateWith(b.kit.resolver))
+	cv, err := spec.StreamOutbound.Decode(attrs, config.InterpolateWith(b.kit.resolver), mapdecode.DecodeHook(tlsModeDecodeHook))
 	if err != nil {
 		return fmt.Errorf("failed to decode stream outbound configuration: %v", err)
 	}
