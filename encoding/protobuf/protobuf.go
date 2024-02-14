07b432dbda537c4fbdeeaef13f401fd790400e3b
Justin Hwang <justinhwang@uber.com>
2024-02-14 11:08:38 -0500

    encoding/protobuf: Update BuildProcedures slice capacity hint

--- encoding/protobuf/protobuf.go
+++ encoding/protobuf/protobuf.go
@@ -76,7 +76,7 @@ type BuildProceduresStreamHandlerParams struct {
 
 // BuildProcedures builds the transport.Procedures.
 func BuildProcedures(params BuildProceduresParams) []transport.Procedure {
-	procedures := make([]transport.Procedure, 0, 2*(len(params.UnaryHandlerParams)+len(params.OnewayHandlerParams)))
+	procedures := make([]transport.Procedure, 0, 2*(len(params.UnaryHandlerParams)+len(params.OnewayHandlerParams)+len(params.StreamHandlerParams)))
 	for _, unaryHandlerParams := range params.UnaryHandlerParams {
 		procedures = append(
 			procedures,
