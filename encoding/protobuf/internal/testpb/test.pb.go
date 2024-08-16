96831bc40a051d11e6512f8f1bc18b7d368a9dd6
Vitalii Levitskii <vitalii@uber.com>
2024-08-16 13:15:49 +0200

    Bump dependencies versions to align with internal monorepo

--- encoding/protobuf/internal/testpb/test.pb.go
+++ encoding/protobuf/internal/testpb/test.pb.go
@@ -435,10 +435,7 @@ func (m *TestMessage) Unmarshal(dAtA []byte) error {
 			if err != nil {
 				return err
 			}
-			if skippy < 0 {
-				return ErrInvalidLengthTest
-			}
-			if (iNdEx + skippy) < 0 {
+			if (skippy < 0) || (iNdEx+skippy) < 0 {
 				return ErrInvalidLengthTest
 			}
 			if (iNdEx + skippy) > l {
