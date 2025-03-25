93c3de6d82b006b5fb22f903f6066ae714a0566c
yingz <yingz@uber.com>
2025-03-25 00:26:10 +0000

    gRPC reflection server

---
+++ x/reflection/grpcreflectionv1alpha/reflection.pb.yarpc.custom.go
@@ -0,0 +1,18 @@
+package grpcreflectionv1alpha
+
+// YARPCReflectionFileDescriptors returns an array of encoded filedescriptor
+// for yaprc to use. Normally the filedescriptors are accessed through the
+// reflection.ServerMeta that is injected into the container. The injection is
+// happens using New{}YARPCProcedures to automatically inject for all services
+// using the fx pattern.
+// This will not work for the reflection service due to a chicken and egg
+// problem: we need a server to access the reflection.Meta and to create a
+// server we need access to all the reflection.Meta (including our own).
+// We could use throwaway instantiation of the reflection service to access
+// its meta, but this would require using the interface{} response of
+// New{}YARPCProcedures meant for fx. Instead of being type unsafe, here we
+// augment the generated code to get compile time safe access to the required
+// filedescriptor
+//
+// After regeneration with the yarpc plugin, update this reference
+var YARPCReflectionFileDescriptors = yarpcFileDescriptorClosure42a8ac412db3cb03
---
+++ x/reflection/server_test.go
@@ -0,0 +1,240 @@
+package reflection
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"go.uber.org/yarpc"
+	"go.uber.org/yarpc/encoding/protobuf/reflection"
+	rpb "go.uber.org/yarpc/x/reflection/grpcreflectionv1alpha"
+)
+
+func TestNewServers(t *testing.T) {
+	tests := []struct {
+		name         string
+		args         []reflection.ServerMeta
+		wantServices []string
+		wantErr      string
+	}{
+		{
+			name:         "none",
+			args:         []reflection.ServerMeta{},
+			wantServices: []string{"grpc.reflection.v1alpha.ServerReflection"},
+		},
+		{
+			name: "invalid fd",
+			args: []reflection.ServerMeta{
+				{ServiceName: "yaha", FileDescriptors: [][]byte{{0x00}}},
+			},
+			wantErr: "bad gzipped descriptor",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			transports, terr := NewServer(tt.args)
+			server, err := newServerFromMetas(tt.args)
+			if tt.wantErr == "" {
+				assert.NoError(t, err)
+				assert.NotNil(t, transports)
+
+				assert.NoError(t, terr)
+				for _, s := range tt.wantServices {
+					assert.Contains(t, server.serviceNames, s)
+					fd, e := server.fileDescriptorIndex.descriptorForSymbol(s)
+					assert.NoError(t, e)
+					assert.NotNil(t, fd)
+				}
+			} else {
+				assert.Error(t, terr)
+				assert.Nil(t, transports)
+
+				require.Error(t, err)
+				assert.Contains(t, err.Error(), tt.wantErr)
+			}
+		})
+	}
+}
+
+type errorClient struct {
+	sendErr      error
+	sendRequest  *rpb.ServerReflectionRequest
+	errOnReceive error
+}
+
+func (m *errorClient) Context() context.Context {
+	return context.Background()
+}
+
+func (m *errorClient) Recv(...yarpc.StreamOption) (*rpb.ServerReflectionRequest, error) {
+	return m.sendRequest, m.sendErr
+}
+
+func (m *errorClient) Send(response *rpb.ServerReflectionResponse, options ...yarpc.StreamOption) error {
+	return m.errOnReceive
+}
+
+func TestErrors(t *testing.T) {
+	s := &server{}
+	t.Run("request nil", func(t *testing.T) {
+		err := s.ServerReflectionInfo(&errorClient{
+			sendErr: io.EOF,
+		})
+		require.NoError(t, err)
+	})
+	t.Run("send error", func(t *testing.T) {
+		errToSend := fmt.Errorf("send this")
+		err := s.ServerReflectionInfo(&errorClient{
+			sendErr: errToSend,
+		})
+		require.EqualError(t, err, errToSend.Error())
+	})
+	t.Run("invalid request", func(t *testing.T) {
+		err := s.ServerReflectionInfo(&errorClient{
+			sendRequest: &rpb.ServerReflectionRequest{},
+		})
+		require.EqualError(t, err, "rpc error: code = InvalidArgument desc = invalid MessageRequest: <nil>")
+	})
+	t.Run("simulate receive error", func(t *testing.T) {
+		errOnReceive := fmt.Errorf("rec this")
+		err := s.ServerReflectionInfo(&errorClient{
+			sendRequest:  &rpb.ServerReflectionRequest{MessageRequest: &rpb.ServerReflectionRequest_ListServices{ListServices: "yes"}},
+			errOnReceive: errOnReceive,
+		})
+		require.EqualError(t, err, errOnReceive.Error())
+	})
+}
+
+type mockClient struct {
+	requests  []*rpb.ServerReflectionRequest
+	responses []*rpb.ServerReflectionResponse
+}
+
+func (m *mockClient) Context() context.Context {
+	return context.Background()
+}
+
+func (m *mockClient) Recv(...yarpc.StreamOption) (*rpb.ServerReflectionRequest, error) {
+	if len(m.requests) == 0 {
+		return nil, io.EOF
+	}
+	r := m.requests[0]
+	m.requests = m.requests[1:]
+	return r, nil
+}
+
+func (m *mockClient) Send(response *rpb.ServerReflectionResponse, options ...yarpc.StreamOption) error {
+	response.OriginalRequest = nil
+	m.responses = append(m.responses, response)
+	return nil
+}
+
+func filedescriptorResponseWithContent(bytes []byte) *rpb.ServerReflectionResponse {
+	return &rpb.ServerReflectionResponse{
+		MessageResponse: &rpb.ServerReflectionResponse_FileDescriptorResponse{
+			FileDescriptorResponse: &rpb.FileDescriptorResponse{FileDescriptorProto: [][]byte{bytes}},
+		},
+	}
+}
+
+func extensionsResponse(basename string, extensions ...int32) *rpb.ServerReflectionResponse {
+	return &rpb.ServerReflectionResponse{
+		MessageResponse: &rpb.ServerReflectionResponse_AllExtensionNumbersResponse{
+			AllExtensionNumbersResponse: &rpb.ExtensionNumberResponse{
+				BaseTypeName:    basename,
+				ExtensionNumber: extensions,
+			},
+		},
+	}
+}
+
+func TestRequestResponse(t *testing.T) {
+	mockIndex := &descriptorIndex{
+		descriptorsByFile:   map[string][]byte{"file.proto": {0xff, 0xf0}},
+		descriptorsBySymbol: map[string][]byte{"some.symbol": {0xff}},
+		descriptorsByExtensionAndNumber: map[string]map[int32][]byte{
+			"some.symbol": {
+				42: {0x1f},
+				21: {0x1f},
+			},
+		},
+	}
+	s := &server{
+		serviceNames:        []string{"test"},
+		fileDescriptorIndex: mockIndex,
+	}
+	tests := []struct {
+		name              string
+		requests          []*rpb.ServerReflectionRequest
+		expectedResponses []*rpb.ServerReflectionResponse
+	}{
+		{
+			name: "list services",
+			requests: []*rpb.ServerReflectionRequest{
+				{MessageRequest: &rpb.ServerReflectionRequest_ListServices{ListServices: "yes"}},
+			},
+			expectedResponses: []*rpb.ServerReflectionResponse{
+				{MessageResponse: &rpb.ServerReflectionResponse_ListServicesResponse{ListServicesResponse: &rpb.ListServiceResponse{Service: []*rpb.ServiceResponse{{Name: "test"}}}}},
+			},
+		}, {
+			name: "by filename",
+			requests: []*rpb.ServerReflectionRequest{
+				{MessageRequest: &rpb.ServerReflectionRequest_FileByFilename{FileByFilename: "file.proto"}},
+				{MessageRequest: &rpb.ServerReflectionRequest_FileByFilename{FileByFilename: "error"}},
+			},
+			expectedResponses: []*rpb.ServerReflectionResponse{
+				filedescriptorResponseWithContent(mockIndex.descriptorsByFile["file.proto"]),
+				{MessageResponse: notFoundResponse(`could not find descriptor for file "error"`)},
+			},
+		}, {
+			name: "by symbol",
+			requests: []*rpb.ServerReflectionRequest{
+				{MessageRequest: &rpb.ServerReflectionRequest_FileContainingSymbol{FileContainingSymbol: "some.symbol"}},
+				{MessageRequest: &rpb.ServerReflectionRequest_FileContainingSymbol{FileContainingSymbol: "error"}},
+			},
+			expectedResponses: []*rpb.ServerReflectionResponse{
+				filedescriptorResponseWithContent(mockIndex.descriptorsBySymbol["some.symbol"]),
+				{MessageResponse: notFoundResponse(`could not find descriptor for symbol "error"`)},
+			},
+		}, {
+			name: "by extension",
+			requests: []*rpb.ServerReflectionRequest{
+				{MessageRequest: &rpb.ServerReflectionRequest_FileContainingExtension{FileContainingExtension: &rpb.ExtensionRequest{ContainingType: "some.symbol", ExtensionNumber: 42}}},
+				{MessageRequest: &rpb.ServerReflectionRequest_FileContainingExtension{FileContainingExtension: &rpb.ExtensionRequest{ContainingType: "some.symbol", ExtensionNumber: 21}}},
+				{MessageRequest: &rpb.ServerReflectionRequest_FileContainingExtension{FileContainingExtension: &rpb.ExtensionRequest{ContainingType: "some.symbol", ExtensionNumber: 0}}},
+				{MessageRequest: &rpb.ServerReflectionRequest_FileContainingExtension{FileContainingExtension: &rpb.ExtensionRequest{ContainingType: "error", ExtensionNumber: -1}}},
+			},
+			expectedResponses: []*rpb.ServerReflectionResponse{
+				filedescriptorResponseWithContent(mockIndex.descriptorsByExtensionAndNumber["some.symbol"][42]),
+				filedescriptorResponseWithContent(mockIndex.descriptorsByExtensionAndNumber["some.symbol"][21]),
+				{MessageResponse: notFoundResponse(`could not find extension number 0 on type "some.symbol"`)},
+				{MessageResponse: notFoundResponse(`could not find extension type "error"`)},
+			},
+		}, {
+			name: "by extension",
+			requests: []*rpb.ServerReflectionRequest{
+				{MessageRequest: &rpb.ServerReflectionRequest_AllExtensionNumbersOfType{AllExtensionNumbersOfType: "some.symbol"}},
+				{MessageRequest: &rpb.ServerReflectionRequest_AllExtensionNumbersOfType{AllExtensionNumbersOfType: "error"}},
+			},
+			expectedResponses: []*rpb.ServerReflectionResponse{
+				extensionsResponse("some.symbol", 21, 42),
+				{MessageResponse: notFoundResponse(`could not find extension type "error"`)},
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			mockClient := &mockClient{requests: tt.requests}
+			err := s.ServerReflectionInfo(mockClient)
+			assert.NoError(t, err)
+			for i, expected := range tt.expectedResponses {
+				require.NotNil(t, mockClient.responses[i])
+				assert.Equal(t, expected, mockClient.responses[i])
+			}
+		})
+	}
+}
