0a29ef14e62223f9c3032007e4530ba65072b275
Kexiong Liu <kexiong.liu@uber.com>
2025-04-29 15:57:24 -0400

    saving

--- transport/http/header_test.go
+++ transport/http/header_test.go
@@ -1,19 +1,18 @@
-// header_test.go
 // Copyright (c) 2025 Uber Technologies, Inc.
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
 // in the Software without restriction, including without limitation the rights
 // to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software are
+// copies of the Software, and to permit persons to whom the Software is
 // furnished to do so, subject to the following conditions:
 //
 // The above copyright notice and this permission notice shall be included in
 // all copies or substantial portions of the Software.
 //
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF NONINFRINGEMENT,
-// FITNESS FOR A PARTICULAR PURPOSE AND MERCHANTABILITY. IN NO EVENT SHALL THE
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
 // AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
@@ -29,7 +28,7 @@ import (
 	"go.uber.org/yarpc/api/transport"
 )
 
-func TestHeaderMapper_ToAndFromHTTPHeaders(t *testing.T) {
+func TestHTTPHeaders(t *testing.T) {
 	tests := []struct {
 		name         string
 		transHeaders transport.Headers
@@ -78,3 +77,6 @@ func TestHeaderMapper_ToAndFromHTTPHeaders(t *testing.T) {
 		assert.Equal(t, tt.transHeaders.Items(), gotTrans.Items(), "%s: FromHTTPHeaders", tt.name)
 	}
 }
+
+// TODO(abg): Test handling of duplicate HTTP headers when
+// https://github.com/yarpc/yarpc/issues/21 is resolved.
