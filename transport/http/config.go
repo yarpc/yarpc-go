b739f41be3a9c142c8bee419536cc638516692a1
Pulkit Bhatia <pulkit4tech@gmail.com>
2025-05-20 13:42:41 +0200

    Chore: remove useHTTP2 config support for http outbound (#2389)

--- transport/http/config.go
+++ transport/http/config.go
@@ -284,12 +284,6 @@ type OutboundConfig struct {
 	//      spiffe-ids:
 	//        - destination-id
 	TLS OutboundTLSConfig `config:"tls"`
-	// UseHTTP2 configure to send http2 requests.
-	//
-	// http:
-	// 		url: "http://localhost:8080/yarpc"
-	// 		useHTTP2: true
-	UseHTTP2 bool `config:"useHTTP2"`
 }
 
 // OutboundTLSConfig configures TLS for the HTTP outbound.
@@ -340,10 +334,6 @@ func (ts *transportSpec) buildOutbound(oc *OutboundConfig, t transport.Transport
 		return nil, err
 	}
 	opts = append(option, opts...)
-
-	if oc.UseHTTP2 {
-		opts = append(opts, UseHTTP2())
-	}
 
 	// Special case where the URL implies the single peer.
 	if oc.Empty() {
