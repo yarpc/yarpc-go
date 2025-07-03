365633df1b720852a869c3390f9b03248271f9a3
sakshi4016 <sakshi.gupta@uber.com>
2025-07-03 14:17:50 +0200

    resolve lint error

--- internal/metricstagdecorators/metricstagdecorators.go
+++ internal/metricstagdecorators/metricstagdecorators.go
@@ -1,5 +1,7 @@
 package metricstagdecorators
 
+// DecoratorProperties is used for passing request-specific properties
+// to metrics decorators.
 type DecoratorProperties struct{}
 
 // MetricsTagsDecorators is used for adding custom tags to YARPC metrics.
