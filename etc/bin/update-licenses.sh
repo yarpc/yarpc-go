5472b4ec2c98da046059c413c490b083e5b411b0
Mark Dragnev <mdragnev@uber.com>
2026-06-23 01:48:36 +0200

    Update license script to ignore random_pkg generated files

--- etc/bin/update-licenses.sh
+++ etc/bin/update-licenses.sh
@@ -8,10 +8,13 @@ cd "${DIR}"
 export GOBIN=$DIR/etc/bin
 go install go.uber.org/tools/update-license
 
-# We need to ignore internal/tests for licenses so that the golden test for
-# thriftrw-plugin-yarpc can verify the contents of the generated code without
-# running updateLicenses on it.
+# We need to ignore internal/tests and internal/random_pkg for licenses so
+# that the golden test for thriftrw-plugin-yarpc can verify the contents of
+# the generated code without running updateLicenses on it. random_pkg sits
+# alongside tests/ on purpose so it can be imported by tests/WITHSERVICES
+# fixtures.
 $GOBIN/update-license $(find . -name '*.go' \
 	| grep -v '^\./vendor' \
 	| grep -v '/thriftrw-plugin-yarpc/internal/tests/' \
+	| grep -v '/thriftrw-plugin-yarpc/internal/random_pkg/' \
 	| grep -v -e '.pb.go$' -e '.pb.yarpc.go$')
